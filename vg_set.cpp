d1813881e45bb39214dac8d9d10d1f214a94f744
Erik Garrison <erik.garrison@gmail.com>
2015-02-02 10:41:14 +0000

    remove intermediate "kmer" files
    It is about 10% faster to write the buffers directly into the db, provided the
    db is set up for bulk loading.

--- vg_set.cpp
+++ vg_set.cpp
@@ -77,28 +77,22 @@ void VGset::index_kmers(Index& index, int kmer_size, int edge_max, int stride) {
 
         // these are indexed by thread
         vector<vector<KmerMatch> > buffer;
-        vector<int> written_buffer_count;
         for (int i = 0; i < thread_count; ++i) {
             buffer.emplace_back();
-            written_buffer_count.push_back(0);
         }
         // how many kmer entries to hold onto
         uint64_t buffer_max_size = 1000000;
 
-        auto write_buffer = [&written_buffer_count, &index](int tid, vector<KmerMatch>& buf) {
-            stringstream file_name;
-            file_name << index.name << "/" << tid << "." << written_buffer_count[tid]++;
-            ofstream out(file_name.str());
-            function<KmerMatch(uint64_t)> write_kmer =
-                [&buf](uint64_t i) {
-                return buf.at(i);
+        auto write_buffer = [&index](int tid, vector<KmerMatch>& buf) {
+            rocksdb::WriteBatch batch;
+            function<void(KmerMatch&)> keep_kmer = [&index, &batch](KmerMatch& k) {
+                index.batch_kmer(k.sequence(), k.node_id(), k.position(), batch);
             };
-            stream::write(out, buf.size(), write_kmer);
-            out.close();
+            std::for_each(buf.begin(), buf.end(), keep_kmer);
+            rocksdb::Status s = index.db->Write(rocksdb::WriteOptions(), &batch);
         };
 
-        auto cache_kmer = [&buffer, &written_buffer_count,
-                           &buffer_max_size, &write_buffer,
+        auto cache_kmer = [&buffer, &buffer_max_size, &write_buffer,
                            this](string& kmer, Node* n, int p) {
             if (allATGC(kmer)) {
                 int tid = omp_get_thread_num();
@@ -113,7 +107,7 @@ void VGset::index_kmers(Index& index, int kmer_size, int edge_max, int stride) {
             }
         };
 
-        g->create_progress("caching kmers of " + g->name, buffer.size());
+        g->create_progress("indexing kmers of " + g->name, buffer.size());
         g->for_each_kmer_parallel(kmer_size, edge_max, cache_kmer, stride);
         g->destroy_progress();
 
@@ -127,33 +121,7 @@ void VGset::index_kmers(Index& index, int kmer_size, int edge_max, int stride) {
         }
         buffer.clear();
         g->destroy_progress();
-
-        int total_buffers = 0;
-        for (int count : written_buffer_count) {
-            total_buffers += count;
-        }
-
-        g->create_progress("indexing kmers " + g->name, total_buffers);
-        int written_buffers = 0;
-#pragma omp parallel for schedule(dynamic)
-        for (int tid = 0; tid < written_buffer_count.size(); ++tid) {
-            int count = written_buffer_count[tid];
-            for (int i = 0; i < count; ++ i) {
-                stringstream file_name;
-                file_name << index.name << "/" << tid << "." << i;
-                ifstream in(file_name.str());
-                rocksdb::WriteBatch batch;
-                function<void(KmerMatch&)> keep_kmer = [&index, &batch, this](KmerMatch& k) {
-                    index.batch_kmer(k.sequence(), k.node_id(), k.position(), batch);
-                };
-                stream::for_each(in, keep_kmer);
-                rocksdb::Status s = index.db->Write(rocksdb::WriteOptions(), &batch);
-                int r = system((string("rm ") + file_name.str()).c_str());
-                g->update_progress(++written_buffers);
-            }
-        }
         index.remember_kmer_size(kmer_size);
-        g->destroy_progress();
 
     });
 
