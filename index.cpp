8b178ab54f8477c2630953efbc07061fa10370ba
Erik Garrison <erik.garrison@gmail.com>
2015-02-03 10:34:33 +0000

    use level style compaction
    This seems to be recommended for large (>100G) databases. Also, it looks to be
    a tiny bit quicker on bulk load, maybe a few percent. It's hard to tell on a 1
    megabase graph though.

--- index.cpp
+++ index.cpp
@@ -17,9 +17,8 @@ void Index::reset_options(void) {
     end_sep = '\xff';
     write_options = rocksdb::WriteOptions();
     options.create_if_missing = true;
-    //options.compression = rocksdb::kZlibCompression;
     options.compression = rocksdb::kSnappyCompression;
-    options.compaction_style = rocksdb::kCompactionStyleUniversal;
+    options.compaction_style = rocksdb::kCompactionStyleLevel;
     int threads = 1;
 #pragma omp parallel
     {
