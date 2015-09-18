5f18340d330c40fdb0e53bc7ef25903a5e71b1bb
Jerven Bolleman <me@jerven.eu>
2015-09-18 11:17:41 +0900

    Use correct IRI for predicates in the RDF namespace

--- vgtordf.sh
+++ vgtordf.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 ((
-jq " .path[]|{(\"\(.name)\"):{\"rdf:type\":[{\"value\":\"Path\",\"type\":\"uri\"}]}} " $1|sed '$d';
+jq " .path[]|{(\"\(.name)\"):{\"http://www.w3.org/1999/02/22-rdf-syntax-ns#type\":[{\"value\":\"Path\",\"type\":\"uri\"}]}} " $1|sed '$d';
 jq ".edge[]|{(\"\(.from | tostring)\"):{\"before\":[ {\"value\":(\"\(.to |tostring)\"), \"type\" : \"uri\"}]}}" $1 | sed -e "s_^\}_,_"| sed -e 's_^[\{]__'|sed '1d;$d';
-jq ".node[]|{(\"\(.id | tostring)\"): {\"rdf:value\":[ {\"value\":.sequence, \"type\" : \"literal\"} ] }}" $1|sed -e "s_^\}_,_"| sed -e 's_^[\{]__'|sed '1d;$d';
+jq ".node[]|{(\"\(.id | tostring)\"): {\"http://www.w3.org/1999/02/22-rdf-syntax-ns#value\":[ {\"value\":.sequence, \"type\" : \"literal\"} ] }}" $1|sed -e "s_^\}_,_"| sed -e 's_^[\{]__'|sed '1d;$d';
 echo ',';
-jq  "  .path[].mapping | keys[] as \$k | { (\$k | tostring):{\"node\":[ {\"value\":(.[\$k].position.node_id | tostring) , \"type\":\"uri\"} ], \"step\" : [{\"value\":(\$k | tostring), \"type\":\"literal\"} ]} }" $1 |sed -e "s_^\}_,_"| sed -e 's_^[\{]__';)|sed '$d';echo '}')
+jq  "  .path[] as \$p|\$p.mapping 
+| keys[] as \$k 
+| { (\$k | tostring):{\"node\":[ {\"value\":(.[\$k].position.node_id | tostring) 
+, \"type\":\"uri\"} ]
+, \"step\" : 
+	[{\"value\":(\$k | tostring)
+	, \"type\":\"literal\"}]
+,\"path\":
+	[{\"value\":(\$p.name),
+	\"type\":\"uri\" } ]}} 
+" $1 |sed -e "s_^\}_,_"| sed -e 's_^[\{]__';)|sed '$d';echo '}')
