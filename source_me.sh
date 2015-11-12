999071e1367a27c5348c743cae3cda89d8a182b6
Eric T Dawson <eric.t.dawson@gmail.com>
2015-11-12 21:27:42 +0000

    Added bin dir to PATH in source_me, updated XG to build on Travis

--- source_me.sh
+++ source_me.sh
@@ -4,3 +4,4 @@ export LD_INCLUDE_PATH=$LD_INCLUDE_PATH:`pwd`/lib
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:`pwd`/include
 export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:`pwd`/include
 export INCLUDE_PATH=$INCLUDE_PATH:`pwd`/include
+export PATH=$PATH:`pwd`/bin
