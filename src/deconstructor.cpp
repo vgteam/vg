9f0da7ffe671d9d4bdf8a670abb7e5758457eb56
Eric T Dawson <eric.t.dawson@gmail.com>
2015-11-17 15:46:59 +0000

    Updated submodules and added more to deconstructor

--- src/deconstructor.cpp
+++ src/deconstructor.cpp
@@ -1,13 +1,51 @@
 #include "deconstructor.hpp"
 
-Path relative_mapping(Path& p1, Path& p2){
-  VG graph;
-  Path ret;
+using namespace std;
 
-}
+namespace vg {
+    Deconstructor::Deconstructor(){
+        this.index = nullptr;
+    }
 
-Path relative_mapping(Path& p1, string p2){
-  VG graph;
-  Path ret;
+    Deconstructor::Deconstructor(Index i) index(i){
+
+    }
+
+    Deconstructor::~Deconstructor(){
+        clear()
+    }
+
+    void clear(){
+        
+    }
+
+    /**
+     * Set the deconstructor's index, be it a rocksdb-backed disk index
+     * or an XG + gcsa index
+     */
+    void Deconstructor::set_index(Index index, bool isXG){
+        if (isXG){
+        }
+        else{
+        }
+    }
+
+    /**
+     * This one is still a bit unsettled in implementation, but the gist
+     * is to:
+     * 1. Open the index
+     * 2. 
+     */
+    Path Deconstructor::relative_mapping(Path& p1, Path& p2){
+        VG graph;
+        Path ret;
+        
+        this.index.open_read_only(db_name)
+    }
+
+    Path Deconstructor::relative_mapping(Path& p1, string p2){
+        VG graph;
+        Path ret;
+    }
 
 }
--- src/deconstructor.hpp
+++ src/deconstructor.hpp
@@ -2,47 +2,50 @@
 #define DECON_HPP
 #include <vector>
 #include <string>
+#include <iostream>
 #include <unordered_map>
 #include "Variant.h"
 #include "index.hpp"
 #include "path.hpp"
 #include "vg.hpp"
-#include <set>
 #include "vg.pb.h"
+namespace vg{
+    using namespace std;
+    class Deconstructor{
+        public:
 
-class Deconstructor{
-public:
+            Deconstructor();
+            ~Deconstructor();
+            void clear();
+            void set_index(Index ind, bool isXG);
 
-  Deconstructor();
-  void set_index(Index ind);
+            /**
+             * Project a mapping onto another mapping.
+             */
+            Path relative_mapping(Path& p1, Path& p2);
 
-  /**
-  * Project a mapping onto another mapping.
-  */
-  Path relative_mapping(Path& p1, Path& p2);
+            /**
+             * Convenience function.
+             * Project a mapping with name 'p2' onto p1.
+             * Return the projection as a map.
+             */
+            Path relative_mapping(Path& p1, string p2);
 
-  /**
-  * Convenience function.
-  * Project a mapping with name 'p2' onto p1.
-  * Return the projection as a map.
-  */
-  Path relative_mapping(Path& p1, string p2);
+            /**
+             * Build a vcf record from two paths, with the
+             * second path argument taken as the reference.
+             */
+            Variant mapping_to_variant(Path variant, Path ref);
 
-  /**
-  * Build a vcf record from two paths, with the
-  * second path argument taken as the reference.
-  */
-  Variant mapping_to_variant(Path variant, Path ref);
-  
-  /**
-  * Turn a vector of variants into a proper VCF.
-  */
-  VariantCallFile write_variants(string filename, vector<Variant> variants);
-private:
-  // TODO Should probably be able to handle XG or VG indices
-  Index index;
+            /**
+             * Turn a vector of variants into a proper VCF.
+             */
+            VariantCallFile write_variants(string filename, vector<Variant> variants);
+        private:
+            // TODO Should probably be able to handle XG or VG indices
+            Index index;
 
+    }
 }
-
 
 #endif
