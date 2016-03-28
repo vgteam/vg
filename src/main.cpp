317823f84f37abe0256039a3004e63e94a243806
Erik Garrison <erik.garrison@gmail.com>
2016-03-28 20:48:21 +0100

    vg index without threads

--- src/main.cpp
+++ src/main.cpp
@@ -3792,8 +3792,6 @@ int main_index(int argc, char** argv) {
         // store the graphs
         VGset graphs(file_names);
         // Turn into an XG index, except for the alt paths which we pull out and load into RAM instead.
-        //xg::XG index = graphs.to_xg(store_threads); //TODO graphs.to_xg(is_alt, alt_paths);
-        //xg::XG index = graphs.to_xg(); //TODO
         //xg::XG index = graphs.to_xg(is_alt, alt_paths);
         xg::XG index = graphs.to_xg(store_threads); //TODO graphs.to_xg(is_alt, alt_paths);
 
