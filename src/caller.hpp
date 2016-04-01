2067e60cd0fca972825d7cba76f4c5889d495a38
Glenn Hickey <glenn.hickey@gmail.com>
2016-04-01 11:10:58 -0400

    Start manual merge of variant calling updates from bakeoff branch

--- src/caller.hpp
+++ src/caller.hpp
@@ -7,6 +7,7 @@
 #include <cmath>
 #include <limits>
 #include <unordered_set>
+#include <tuple>
 #include "vg.pb.h"
 #include "vg.hpp"
 #include "hash_map.hpp"
@@ -17,10 +18,59 @@ namespace vg {
 
 using namespace std;
 
+// We need to break apart nodes but remember where they came from to update edges.
+// Wrap all this up in this class.  For a position in the input graph, we can have
+// up to three nodes in the augmented graph (Ref, Alt1, Alt2), so we map to node
+// triplets (Entry struct below).  Note we never *call* all three nodes due to
+// diploid assumption, but the augmented graph stores everything. 
+struct NodeDivider {
+    // up to three fragments per position in augmented graph (basically a Node 3-tuple,
+    // avoiding aweful C++ tuple syntax)
+    enum EntryCat {Ref = 0, Alt1, Alt2, Last};
+    struct Entry { Entry(Node* r = 0, char cn_r = (char)0,
+                         Node* a1 = 0, char cn_a1 = (char)0,
+                         Node* a2 = 0, char cn_a2 = (char)0) : ref(r), alt1(a1), alt2(a2),
+                                                               cn_ref(cn_r), cn_alt1(cn_a1), cn_alt2(cn_a2){}
+        Node* ref; Node* alt1; Node* alt2;
+        char cn_ref; char cn_alt1; char cn_alt2;
+        Node*& operator[](int i) {
+            assert(i >= 0 && i <= 2);
+            return i == EntryCat::Ref ? ref : (i == EntryCat::Alt1 ? alt1 : alt2);
+        }
+        char& cn(int i) {
+            assert(i >= 0 && i <= 2);
+            return i == EntryCat::Ref ? cn_ref : (i == EntryCat::Alt1 ? cn_alt1 : cn_alt2);
+        }
+    };
+    // offset in original graph node -> up to 3 nodes in call graph
+    typedef map<int, Entry> NodeMap;
+    // Node id in original graph to map above
+    typedef hash_map<int, NodeMap> NodeHash;
+    NodeHash index;
+    int64_t* _max_id;
+    // map given node to offset i of node with id in original graph
+    // this function can never handle overlaps (and should only be called before break_end)
+    void add_fragment(const Node* orig_node, int offset, Node* subnode, EntryCat cat, int cn);
+    // break node if necessary so that we can attach edge at specified side
+    // this function wil return NULL if there's no node covering the given location
+    Entry break_end(const Node* orig_node, VG* graph, int offset, bool left_side);
+    // assuming input node is fully covered, list of nodes that correspond to it in call graph
+    // if node not in structure at all, just return input (assumption uncalled nodes kept as is)
+    list<Mapping> map_node(int64_t node_id, int64_t start_offset, int64_t length, bool reverse);
+    // erase everything (but don't free any Node pointers, they belong to the graph)
+    void clear();
+};
+ostream& operator<<(ostream& os, const NodeDivider::NodeMap& nm);
+ostream& operator<<(ostream& os, NodeDivider::Entry entry);
+
 // Super simple variant caller, for now written to get bakeoff evaluation bootstrapped.
 // Idea: Idependently process Pileup records, using simple model to make calls that
-//       take into account read errors with diploid assumption.  
-// Output stored as a graph representing the aligned sample.  
+//       take into account read errors with diploid assumption.  Edges and node positions
+//       are called independently for now.  
+// Outputs either a sample graph (only called nodes and edges) or augmented graph
+// (include uncalled nodes and edges too).
+// The augmented graph (leave_uncalled), with optional text annotation output (text_calls)
+// is required to convert calls to VCF. 
 class Caller {
 public:
 
@@ -34,12 +84,14 @@ public:
     static const int Default_max_depth;
     // minimum number of reads that support snp required to call it
     static const int Default_min_support;
-    // minimum pct of pileup that's not indels to call snp
+    // same as min_support, but as fraction of total depth
     static const double Default_min_frac;
     // minimum likelihood to call a snp
     static const double Default_min_likelihood;
     // use this score when pileup is missing quality
     static const char Default_default_quality;
+    // use to balance alignments to forward and reverse strand
+    static const double Default_max_strand_bias;
     
     Caller(VG* graph,
            double het_prior = Default_het_prior,
@@ -49,7 +101,9 @@ public:
            double min_frac = Default_min_frac,
            double min_likelihood = Default_min_likelihood, 
            bool leave_uncalled = false,
-           int default_quality = Default_default_quality);
+           int default_quality = Default_default_quality,
+           double max_strand_bias = Default_max_strand_bias,
+           ostream* text_calls = NULL);
     ~Caller();
     void clear();
 
@@ -57,22 +111,37 @@ public:
     VG* _graph;
     // output called graph
     VG _call_graph;
+    // optional text file of calls
+    ostream* _text_calls;
 
     // buffer for base calls for each position in the node
     // . = reference
     // - = missing
-    typedef pair<char, char> Genotype;
+    typedef pair<string, string> Genotype;
     vector<Genotype> _node_calls;
+    vector<double> _node_likelihoods;
+    // separate structure for isnertion calls since they
+    // don't really have reference coordinates (instead happen just to
+    // right of offset).  
+    vector<Genotype> _insert_calls;
+    vector<double> _insert_likelihoods;
     // buffer for current node;
     const Node* _node;
     // max id in call_graph
     int64_t _max_id;
-    // node id map to convert edges
-    typedef pair<int64_t, int64_t> NodePair;
-    typedef hash_map<int64_t, NodePair> NodeMap;
-    NodeMap _start_node_map;
-    NodeMap _end_node_map;
+    // link called nodes back to the original graph. needed
+    // to figure out where edges go
+    NodeDivider _node_divider;
     unordered_set<int64_t> _visited_nodes;
+    unordered_set<pair<NodeSide, NodeSide> > _called_edges;
+    // deletes can don't necessarily need to be in incident to node ends
+    // so we throw in an offset into the mix. 
+    typedef pair<NodeSide, int> NodeOffSide;
+    // map a call category to an edge
+    typedef unordered_map<pair<NodeOffSide, NodeOffSide>, char> EdgeHash;
+    EdgeHash _augmented_edges;
+    // keep track of inserted nodes for tsv output
+    vector<pair<Node*, int> > _inserted_nodes;
 
     // used to favour homozygous genotype (r from MAQ paper)
     double _het_log_prior;
@@ -94,6 +163,8 @@ public:
     bool _leave_uncalled;
     // if we don't have a mapping quality for a read position, use this
     char _default_quality;
+    // min deviation from .5 in proportion of negative strand reads
+    double _max_strand_bias;
 
     // write the call graph
     void write_call_graph(ostream& out, bool json);
@@ -101,51 +172,52 @@ public:
     // call every position in the node pileup
     void call_node_pileup(const NodePileup& pileup);
 
+    // call an edge.  remembering it in a table for the whole graph
+    void call_edge_pileup(const EdgePileup& pileup);
+
     // fill in edges in the call graph (those that are incident to 2 call nodes)
     // and add uncalled nodes (optionally)
     void update_call_graph();
+
+    // map paths from input graph into called graph
+    void map_paths();
+    // make sure mapped paths generate same strings as input paths
+    void verify_path(const Path& in_path, const list<Mapping>& call_path);
     
     // call position at given base
-    void call_base_pileup(const NodePileup& np, int64_t offset);
+    // if insertion flag set to true, call insertion between base and next base
+    void call_base_pileup(const NodePileup& np, int64_t offset, bool insertions);
     
     // Find the top-two bases in a pileup, along with their counts
+    // Last param toggles whether we consider only inserts or everything else
+    // (do not compare all at once since inserts do not have reference coordinates)
     void compute_top_frequencies(const BasePileup& bp,
-                                 const vector<pair<int, int> >& base_offsets,
-                                 char& top_base, int& top_count,
-                                 char& second_base, int& second_count);
+                                 const vector<pair<int64_t, int64_t> >& base_offsets,
+                                 string& top_base, int& top_count, int& top_rev_count,
+                                 string& second_base, int& second_count, int& second_rev_count,
+                                 int& total_count, bool inserts);
     
     // compute genotype for a position with maximum prob
-    Genotype mp_snp_genotype(const BasePileup& bp,
-                             const vector<pair<int, int> >& base_offsets,
-                             char top_base, char second_base);
+    double mp_snp_genotype(const BasePileup& bp,
+                           const vector<pair<int64_t, int64_t> >& base_offsets,
+                           const string& top_base, const string& second_base,
+                           Genotype& mp_genotype);
     // compute likelihood of a genotype samtools-style
     double genotype_log_likelihood(const BasePileup& pb,
-                                   const vector<pair<int, int> >& base_offsets,
-                                   double g, char first, char second);
+                                   const vector<pair<int64_t, int64_t> >& base_offsets,
+                                   double g, const string& first, const string& second);
 
     // write graph structure corresponding to all the calls for the current
     // node.  
     void create_node_calls(const NodePileup& np);
 
-    // make a path corresponding to a snp in the call grpah
-    void create_snp_path(int64_t snp_node, bool secondary_snp);
+    void create_augmented_edge(Node* node1, int from_offset, bool left_side1, bool aug1,
+                               Node* node2, int to_offset, bool left_side2, bool aug2, char cat);
 
-    // convert nucleotide base into integer
-    static int nidx(char c) {
-        c = ::toupper(c);
-        switch (c) {
-        case 'A': return 0;
-        case 'C': return 1;
-        case 'G': return 2;
-        case 'T': return 3;
-        }
-        return 4;
-    }
-
-    // and back
-    static char idxn(int i) {
-        return "ACGTN"[i];
-    }
+    // write calling info to tsv to help with VCF conversion
+    void write_node_tsv(Node* node, char call, char cn);
+    void write_edge_tsv(Edge* edge, char call, char cn = '.');
+    void write_nd_tsv();
 
     // log function that tries to avoid 0s
     static double safe_log(double v) {
@@ -161,27 +233,22 @@ public:
     }
 
     // call missing
-    bool missing_call(const Genotype& g) {
-        return g.first == '-' && g.second == '.';
+    static bool missing_call(const Genotype& g) {
+        return g.first == "-" &&  g.second == "-";
     }
 
     // call is reference
-    bool ref_call(const Genotype& g) {
-        if (g.first == '.') {
-            return g.second == '.' || g.second == '-';
-        } else if (g.first == '-') {
-            return g.second == '.';
-        }
-        return false;
+    static bool ref_call(const Genotype& g) {
+        return g.first == "." && (g.second == "." || g.second == "-");
     }
 
     // call is snp
-    bool snp_call(const Genotype& g) {
+    static bool snp_call(const Genotype& g) {
         return !missing_call(g) && !ref_call(g);
     }
 
     // classify call as 0: missing 1: reference 2: snp
-    int call_cat(const Genotype&g) {
+    static int call_cat(const Genotype&g) {
         if (missing_call(g)) {
             return 0;
         } else if (ref_call(g)) {
@@ -191,6 +258,7 @@ public:
     }
 };
 
+ostream& operator<<(ostream& os, const Caller::NodeOffSide& no);
 
 
 }
