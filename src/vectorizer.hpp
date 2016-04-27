686520d37b8d2fce79e07d2af6d24e72b5717d00
Eric T Dawson <eric.t.dawson@gmail.com>
2016-04-27 11:48:46 +0100

    Appeasing git for merge

--- src/vectorizer.hpp
+++ src/vectorizer.hpp
@@ -61,9 +61,9 @@ class Vectorizer{
     // names. This allows things like generating simulated data with true cluster as the name.
     vector<bit_vector> my_vectors;
     vector<string> my_names;
-    output_tabbed = false;
-    output_names = false;
-    output_wabbit = false;
+    bool output_tabbed = false;
+    bool output_names = false;
+    //bool output_wabbit = false;
     unordered_map<string, int> wabbit_map;
 
 };
