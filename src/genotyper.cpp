50edc101d44c3b67bf87856fc2e8bbf62bbb5bf1
Adam Novak <anovak@soe.ucsc.edu>
2016-06-13 15:52:24 +0900

    Add a genotyper.cpp to put widgets in

---
+++ src/genotyper.cpp
@@ -0,0 +1,1 @@
+#include "genotyper.hpp"
---
+++ src/genotyper.hpp
@@ -0,0 +1,27 @@
+#ifndef VG_GENOTYPER_H
+#define VG_GENOTYPER_H
+// genotyper.hpp: defines the Genotyper, which is used to genotype from a graph
+// and a collection of indexed alignments.
+
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+#include <limits>
+#include <unordered_set>
+#include <tuple>
+#include "vg.pb.h"
+#include "vg.hpp"
+#include "hash_map.hpp"
+#include "utility.hpp"
+
+namespace vg {
+
+using namespace std;
+
+
+
+}
+
+
+#endif
