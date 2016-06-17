a6768621a0b67d02dc702e392ce7aa00fc6b6aec
Glenn Hickey <glenn.hickey@gmail.com>
2016-06-17 10:54:04 +0900

    move bash autocomplete out of source_me

--- source_me.sh
+++ source_me.sh
@@ -11,21 +11,5 @@ export CXX=$(which g++)
 # add bash autocompletion
 if test -n "$BASH_VERSION"
 then
-	 _vg_complete()
-	 {
-		  local cur prev
-		  COMPREPLY=()
-		  cur="${COMP_WORDS[COMP_CWORD]}"
-		  opts="construct deconstruct view vectorize index find paths align map stats join ids concat kmers sim mod surject msga pileup call genotype compare scrub circularize validate version"
-
-		  if [ $COMP_CWORD -eq 1 ]
-		  then
-				COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-		  else 
-				local files=(*)
-				[[ -e ${files[0]} ]] && COMPREPLY=( "${files[@]##*/}" )
-		  fi
-		  return 0
-	 }
-	 complete -o nospace -F _vg_complete vg
+	 . autocomp.bash
 fi
