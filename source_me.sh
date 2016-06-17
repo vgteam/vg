4f5fdc279ea6c2c8870f90a390065159ce6083ff
Glenn Hickey <glenn.hickey@gmail.com>
2016-06-17 09:19:49 +0900

    fix bash autocompletion to use filenames for all but 1st argument

--- source_me.sh
+++ source_me.sh
@@ -18,7 +18,13 @@ then
 		  cur="${COMP_WORDS[COMP_CWORD]}"
 		  opts="construct deconstruct view vectorize index find paths align map stats join ids concat kmers sim mod surject msga pileup call genotype compare scrub circularize validate version"
 
-		  COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+		  if [ $COMP_CWORD -eq 1 ]
+		  then
+				COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+		  else 
+				local files=(*)
+				[[ -e ${files[0]} ]] && COMPREPLY=( "${files[@]##*/}" )
+		  fi
 		  return 0
 	 }
 	 complete -o nospace -F _vg_complete vg
