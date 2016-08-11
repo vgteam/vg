039792c16fb9b89b8e471e01301121edb66bd08d
Adam Novak <anovak@soe.ucsc.edu>
2016-08-11 14:26:16 -0700

    Add a spot for filter unit tests

---
+++ src/unittest/readfilter.cpp
@@ -0,0 +1,17 @@
+/**
+ * unittest/readfilter.cpp: test cases for the read filtering/transforming logic
+ */
+
+#include "catch.hpp"
+#include "readfilter.hpp"
+
+namespace vg {
+namespace unittest {
+
+using namespace std;
+
+
+
+
+}
+}
