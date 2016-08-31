6b278d27f3f8b8d1e7ede8d8a344d2525bd2fe41
Adam Novak <anovak@soe.ucsc.edu>
2016-08-31 15:27:52 -0700

    Clean up extra nodes added during alignment
    They could confuse softclip handling by making you think you had a node
    already from the index that you didn't, and by giving that node the
    sequence "N" and a completely incorrect set of edges.

--- src/vg.cpp
+++ src/vg.cpp
@@ -7486,6 +7486,11 @@ Alignment VG::align(const Alignment& alignment,
         sort();
         // run the alignment
         do_align(this->graph);
+        
+        // Clean up the node we added. This is important because this graph will
+        // later be extended with more material for softclip handling, and we
+        // might need that node ID.
+        destroy_node(root);
 
     } else {
         map<id_t, pair<id_t, bool> > unfold_trans;
@@ -7543,6 +7548,11 @@ Alignment VG::align(const Alignment& alignment,
                 return get_node(node_id)->sequence().size();
             });
         //check_aln(*this, aln);
+        
+        // Clean up the node we added. This is important because this graph will
+        // later be extended with more material for softclip handling, and we
+        // might need that node ID.
+        destroy_node(root);
 
     }
 
