ab0095e2a7132024913731f5b54871a22f13eee1
Adam Novak <anovak@soe.ucsc.edu>
2016-11-03 16:23:48 -0700

    Make VCF coordinates inclusive like they should be

--- src/subcommand/construct.cpp
+++ src/subcommand/construct.cpp
@@ -16,7 +16,7 @@ void help_construct(char** argv) {
          << "    -v, --vcf FILE        input VCF" << endl
          << "    -r, --reference FILE  input FASTA reference" << endl
          << "    -a, --alt-paths       save paths for alts of variants by variant ID" << endl
-         << "    -R, --region REGION   specify a particular chromosome or 1-based region" << endl
+         << "    -R, --region REGION   specify a particular chromosome or 1-based inclusive region" << endl
          << "    -C, --region-is-chrom don't attempt to parse the region (use when the reference" << endl
          << "                          sequence name could be inadvertently parsed as a region)" << endl
          << "    -z, --region-size N   variants per region to parallelize" << endl
@@ -143,8 +143,8 @@ int main_construct(int argc, char** argv) {
                 // These are 0-based, so if both are nonzero we got a real set of coordinates
                 cerr << "Restricting to " << seq_name << " from " << start_pos << " to " << stop_pos << endl;
                 constructor.allowed_vcf_names.insert(seq_name);
-                // Make sure to correct the coordinates to 0-based
-                constructor.allowed_vcf_regions[seq_name] = make_pair(start_pos - 1, stop_pos - 1);
+                // Make sure to correct the coordinates to 0-based exclusive-end, from 1-based inclusive-end
+                constructor.allowed_vcf_regions[seq_name] = make_pair(start_pos - 1, stop_pos);
             } else if (start_pos == 0 && stop_pos == 0) {
                 // We just got a name
                 cerr << "Restricting to " << seq_name << " from 1 to end" << endl;
