baab5558f27327d8cfc2514a373fba10b746d67e
Erik Garrison <erik.garrison@gmail.com>
2016-12-08 19:45:37 +0100

    improve benchmark script

--- test/bench/compare.sh
+++ test/bench/compare.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-if [ ! $# -eq 7 ];
+if [ ! $# -eq 6 ];
 then
-    echo "usage: " $0 " [prefix] [fq1] [fq2] [linear_xg] [linear_gcsa] [pan_xg] [pan_gcsa]"
+    echo "usage: " $0 " [prefix] '[input spec]' [linear_xg] [linear_gcsa] [pan_xg] [pan_gcsa]"
     exit
 fi
 
 prefix=$1
-fq1=$2
-fq2=$3
-linear_xg=$4
-linear_gcsa=$5
-pan_xg=$6
-pan_gcsa=$7
+input=$2
+linear_xg=$3
+linear_gcsa=$4
+pan_xg=$5
+pan_gcsa=$6
 
 # take the pan and ref indexes
 # map a set of reads against both
 
 mkdir -p $prefix
 
-echo mapping against linear graph
-vg map -x $linear_xg -g $linear_gcsa -f $fq1 -f $fq2 -u 1 -L 10 >$prefix/linear.gam
+echo mapping $prefix against linear graph
+vg map -x $linear_xg -g $linear_gcsa $input -u 1 -L 10 -a >$prefix/linear.gam
 
-echo mapping against pan graph
-vg map -x $pan_xg -g $pan_gcsa -f $fq1 -f $fq2 -u 1 -L 10 >$prefix/pan.gam
+echo mapping $prefix against pan graph
+vg map -x $pan_xg -g $pan_gcsa $input -u 1 -L 10 -a >$prefix/pan.gam
 
 # join the results (identity, score, mapping quality) on read name
 echo comparing results
-( echo 'name\tref.identity\tref.score\tref.mapq\tpan.identity\tpan.score\tpan.mapq'
+( echo group name ref.identity ref.score ref.mapq pan.identity pan.score pan.mapq
   join -j 1 \
-       <(vg view -a $prefix/linear.gam | jq '[.name, .identity, .score, .mapping_quality] | @sh' | sed 's/"//g' | sed s/null/0/ | sort) \
-       <(vg view -a $prefix/pan.gam | jq '[.name, .identity, .score, .mapping_quality] | @sh' | sed 's/"//g' | sed s/null/0/ | sort)
-) | gzip >$prefix/vs.tsv.gz
+       <(vg view -a $prefix/linear.gam | jq '[.name, .identity, .score, .mapping_quality] | @sh' | sed 's/"//g' | sed "s/'//g" | sed 's/ null/ 0/g' | sort) \
+       <(vg view -a $prefix/pan.gam | jq '[.name, .identity, .score, .mapping_quality] | @sh' | sed 's/"//g' | sed "s/'//g" | sed 's/ null/ 0/g' | sort) \
+      | while read line; do echo $prefix $line; done ) | tr ' ' '\t' | gzip >$prefix/vs.tsv.gz
