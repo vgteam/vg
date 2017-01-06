202d63a01f0917b4b9955586a16ed763b124e4df
Erik Garrison <erik.garrison@gmail.com>
2017-01-06 18:49:53 +0100

    describe how the pos_compare.py script is used

--- scripts/pos_compare.py
+++ scripts/pos_compare.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 
+### positional comparison script useful for tagging alignments with their true position
+## for use with results of this form
+# vg map -iG x.paired.sim -x x.xg -g x.gcsa \
+#     | vg annotate -x x.xg -p -a - \
+#     | vg view -a - \
+#     | jq -c  '[.name, .refpos[0].name, .refpos[0].offset, if .mapping_quality == null then 0 else .mapping_quality end ] | @sh'  \
+#     | sed 's/"//g' | sed "s/'//g" | sed s/null/0/g \
+#     | sort >x.pos
+## then merged and passed through this script
+# join x.true_pos x.pos | ./pos_compare.py
+## the output is a table of aln_name, correct (0/1), aln_mapq
+## the same result can be examined for a SAM-capable aligner like bwa mem:
+# bwa mem x.fasta x.fq.gz | cut -f 1,3,4,5 | grep -v ^@ | sort >x.bwa_pos
+
 import sys
 
 threshold = 100
