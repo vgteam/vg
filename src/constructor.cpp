6aeedb88d1633da93de25c58683f7b42bf520017
Eric Dawson <eric.t.dawson@gmail.com>
2017-01-24 17:05:35 +0000

    Remove extra cerr from constructor

--- src/constructor.cpp
+++ src/constructor.cpp
@@ -457,7 +457,6 @@ namespace vg {
                     // Name the variant and place it in the order that we'll
                     // actually construct nodes in (see utility.hpp)
                     string variant_name = make_variant_id(*variant);
-                    cerr << *variant << endl;
                     if (variants_by_name.count(variant_name)) {
                         // Some VCFs may include multiple variants at the same
                         // position with the same ref and alt. We will only take the
