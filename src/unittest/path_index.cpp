ec7b117780c5320c946e51fa6b0eba6a80b1bf7b
Adam Novak <anovak@soe.ucsc.edu>
2017-01-27 10:43:41 -0800

    Exercise reverse strand support for trnaslation from path

--- src/unittest/path_index.cpp
+++ src/unittest/path_index.cpp
@@ -227,6 +227,68 @@ TEST_CASE("PathIndex translation can create reverse strand mappings", "[pathinde
     }
     
 }
+
+TEST_CASE("PathIndex translation can handle translations articulated for the reverse strand", "[pathindex]") {
+    
+    // Load the graph
+    Graph graph;
+    json2pb(graph, path_index_graph_1.c_str(), path_index_graph_1.size());
+    
+    // Make it into a VG
+    VG to_index;
+    to_index.extend(graph);
+    
+    // Make a PathIndex
+    PathIndex index(to_index, "cool", true);
+    
+    SECTION("Before translation, node 4 is at positions 2-4") {
+        REQUIRE(index.at_position(2).node == 4);
+        REQUIRE(index.at_position(3).node == 4);
+        REQUIRE(index.at_position(4).node == 4);
+    }
+    
+    // Make a Translation to change node 4 (GGG) to nodes 1337 (G) and 1338 (GG)
+    // But do it in reverse on both paths
+    Translation t;
+    auto* from_mapping = t.mutable_from()->add_mapping();
+    from_mapping->mutable_position()->set_node_id(4);
+    from_mapping->mutable_position()->set_is_reverse(true);
+    auto* from_edit = from_mapping->add_edit();
+    from_edit->set_from_length(3);
+    from_edit->set_to_length(3);
+    
+    auto* to_mapping_1 = t.mutable_to()->add_mapping();
+    to_mapping_1->mutable_position()->set_node_id(1338);
+    to_mapping_1->mutable_position()->set_is_reverse(true);
+    auto* to_edit_1 = to_mapping_1->add_edit();
+    to_edit_1->set_from_length(2);
+    to_edit_1->set_to_length(2);
+    
+    auto* to_mapping_2 = t.mutable_to()->add_mapping();
+    to_mapping_2->mutable_position()->set_node_id(1337);
+    to_mapping_2->mutable_position()->set_is_reverse(true);
+    auto* to_edit_2 = to_mapping_2->add_edit();
+    to_edit_2->set_from_length(1);
+    to_edit_2->set_to_length(1);
+    
+    // Apply it
+    index.apply_translation(t);
+    
+    // Everything should come out forward
+    
+    SECTION("After translation, node 1337 is at position 2 forward") {
+        REQUIRE(index.at_position(2).node == 1337);
+        REQUIRE(index.at_position(2).is_end == false);
+    }
+    
+    SECTION("After translation, node 1338 is at positions 3-4 forward") {
+        REQUIRE(index.at_position(3).node == 1338);
+        REQUIRE(index.at_position(3).is_end == false);
+        REQUIRE(index.at_position(4).node == 1338);
+        REQUIRE(index.at_position(4).is_end == false);
+    }
+    
+}
    
 }
 }
