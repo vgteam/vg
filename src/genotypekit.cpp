ef076bd764c1dfb8a3669b814f0e905456e9bcf2
Jordan Eizenga <joeizeng@gmail.com>
2017-01-31 12:04:57 -0800

    fix bug in exhaustive snarl traversal finder

--- src/genotypekit.cpp
+++ src/genotypekit.cpp
@@ -226,7 +226,11 @@ vector<SnarlTraversal> ExhaustiveTraversalFinder::find_traversals(const Snarl& s
             stack.push_back(to_rev_node_traversal(child_site->start(), graph));
         }
         else {
-            // add all of the node traversals we can reach through valid walks
+            // make a visit out of the node traversal
+            visit.set_node_id(node_traversal.node->id());
+            visit.set_backward(node_traversal.backward);
+            
+            // add all of the node traversals we can reach through valid walks to stack
             stack_up_valid_walks(node_traversal, stack);
         }
         
