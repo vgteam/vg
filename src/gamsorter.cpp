67723729d3e96b287514aec31ddbbb77b165ed81
Eric T Dawson <eric.t.dawson@utexas.edu>
2017-02-16 16:43:03 +0000

    Finish CLI for sort, though it is definitely still buggy

--- src/gamsorter.cpp
+++ src/gamsorter.cpp
@@ -10,6 +10,10 @@ using namespace vg;
 /**
 vector<Alignment> GAMSorter::merge(vector<vector<Alignment> > a){
     vector<Alignment> ret;
+    vector<int> a_tracker(a.size());
+    
+
+
     return ret;
 }
 
@@ -23,78 +27,92 @@ void GAMSorter::merge(vector<string> sorted_tmp_filenames){
 
 **/
 
-struct custom_pos_sort_key{
-      bool operator() (const Position lhs, const Position rhs){
-        if (lhs.node_id() == rhs.node_id()){
-          return lhs.offset() < rhs.offset();
+struct custom_pos_sort_key
+{
+    bool operator()(const Position lhs, const Position rhs)
+    {
+        if (lhs.node_id() == rhs.node_id())
+        {
+            return lhs.offset() < rhs.offset();
         }
-        else{
-          return lhs.node_id() < rhs.node_id();
+        else
+        {
+            return lhs.node_id() < rhs.node_id();
         }
-      }
-    } possortkey;
+    }
+} possortkey;
 
-    struct custom_aln_sort_key{
-      bool operator() (const Alignment a_one, const Alignment a_two){
+struct custom_aln_sort_key
+{
+    bool operator()(const Alignment a_one, const Alignment a_two)
+    {
         Position x_front = a_one.path().mapping(0).position();
-        Position x_back = a_one.path().mapping( a_one.path().mapping_size() - 1 ).position();
+        Position x_back = a_one.path().mapping(a_one.path().mapping_size() - 1).position();
 
         Position lhs;
         Position rhs;
-        if (x_front.node_id() < x_back.node_id()){
-          lhs = x_front;
+        if (x_front.node_id() < x_back.node_id())
+        {
+            lhs = x_front;
         }
-        else{
-          lhs = x_back;
+        else
+        {
+            lhs = x_back;
         }
         Position y_front = a_two.path().mapping(0).position();
-        Position y_back = a_two.path().mapping( a_two.path().mapping_size() - 1 ).position();
-        if (y_front.node_id() < y_back.node_id()){
-          rhs = y_front;
+        Position y_back = a_two.path().mapping(a_two.path().mapping_size() - 1).position();
+        if (y_front.node_id() < y_back.node_id())
+        {
+            rhs = y_front;
         }
-        else{
-          rhs = y_back;
+        else
+        {
+            rhs = y_back;
         }
 
-        if (lhs.node_id() == rhs.node_id()){
-          return lhs.offset() < rhs.offset();
+        if (lhs.node_id() == rhs.node_id())
+        {
+            return lhs.offset() < rhs.offset();
         }
-        else{
-          return lhs.node_id() < rhs.node_id();
+        else
+        {
+            return lhs.node_id() < rhs.node_id();
         }
-      }
-    } alnsortkey;
+    }
+} alnsortkey;
 
-
-void GAMSorter::sort(vector<Alignment>& alns){
+void GAMSorter::sort(vector<Alignment> &alns)
+{
     std::sort(alns.begin(), alns.end(), alnsortkey);
 }
 
-void GAMSorter::paired_sort(string gamfile){
-    
+void GAMSorter::paired_sort(string gamfile)
+{
+
     std::unordered_map<string, Alignment> pair_seconds;
     vector<Alignment> firsts;
     firsts.reserve(100000);
-    std::function<void(Alignment&, Alignment&)> sort_the_gam = [&](Alignment& alpha, Alignment& beta){
+    std::function<void(Alignment &, Alignment &)> sort_the_gam = [&](Alignment &alpha, Alignment &beta) {
         bool first_min = min_aln_first(alpha, beta);
-        if (!alpha.fragment_next().name().empty()){
-            if (first_min){
-            firsts.push_back( alpha);
-            pair_seconds[ beta.name()] = beta;
+        if (!alpha.fragment_next().name().empty())
+        {
+            if (first_min)
+            {
+                firsts.push_back(alpha);
+                pair_seconds[beta.name()] = beta;
+            }
+            else
+            {
+                firsts.push_back(beta);
+                pair_seconds[alpha.name()] = alpha;
+            }
         }
-        else{
-            firsts.push_back( beta );
-            pair_seconds[ alpha.name()] = alpha;
-        }
-        }
-        else{
+        else
+        {
             firsts.push_back(alpha);
         }
 
-        
-        
     };
-    
 
     ifstream gammy;
     gammy.open(gamfile);
@@ -102,16 +120,18 @@ void GAMSorter::paired_sort(string gamfile){
 
     sort(firsts);
     vector<Alignment> ret;
-    for (auto x : firsts){
+    for (auto x : firsts)
+    {
         // write first read.
-        ret.push_back( x );
+        ret.push_back(x);
         // grab second read and write it.
         if (!x.fragment_next().name().empty())
-        ret.push_back( pair_seconds[x.fragment_next().name()] );
+            ret.push_back(pair_seconds[x.fragment_next().name()]);
     }
 }
 
-void GAMSorter::write_temp(vector<Alignment>& alns){
+void GAMSorter::write_temp(vector<Alignment> &alns)
+{
     string t_name = "tmp_sort_" + tmp_filenames.size();
     tmp_filenames.push_back(t_name);
     ofstream t_file;
@@ -119,11 +139,12 @@ void GAMSorter::write_temp(vector<Alignment>& alns){
     stream::write_buffered(t_file, alns, 1000);
 }
 
-void GAMSorter::dumb_sort(string gamfile){
+void GAMSorter::dumb_sort(string gamfile)
+{
     std::vector<Alignment> buf;
     buf.reserve(1000000);
 
-    std::function<void(Alignment&)> presort = [&](Alignment& aln){
+    std::function<void(Alignment &)> presort = [&](Alignment &aln) {
 
         buf.push_back(aln);
     };
@@ -133,12 +154,12 @@ void GAMSorter::dumb_sort(string gamfile){
 
     std::sort(buf.begin(), buf.end(), alnsortkey);
 
-    std::function<void(uint64_t)> x_buf_func = [&](uint64_t){
+    std::function<void(uint64_t)> x_buf_func = [&](uint64_t) {
 
     };
 
     ofstream outfi;
-    outfi.open(gamfile + "sorted.gam");
+    outfi.open(gamfile + ".sorted.gam");
     stream::write_buffered(outfi, buf, buf.size());
 }
 
@@ -154,13 +175,12 @@ void GAMSorter::dumb_sort(string gamfile){
 //                 write_temp(buffer);
 //                 buffer.clear();
 //         }
-        
+
 //     };
 
 //     ifstream gammy;
 //     gammy.open(gamfile);
 //     stream::for_each_interleaved_pair_parallel(gammy, firstpass_bufsort);
-
 
 //     vector<Alignment> file_tops(tmp_files.size());
 //     std::function<int(vector<Alignment>)> min_index = [&](vector<Alignment> alns){
@@ -185,7 +205,7 @@ void GAMSorter::dumb_sort(string gamfile){
 //         }
 //         if (buf.size() >= max_buf_size){
 //             stream::write_buffered(ofi, buf, 1000);
-//         }    
+//         }
 //         };
 
 //     /**
@@ -208,73 +228,83 @@ void GAMSorter::dumb_sort(string gamfile){
 //         handle_lowest_tmpfile(min_ind, outfi, obuf, file_tops);
 //     }
 
-    
-
 // }
 
-void GAMSorter::write_index(string gamfile, string outfile, bool isSorted ){
+void GAMSorter::write_index(string gamfile, string outfile, bool isSorted)
+{
 
     map<int64_t, vector<string>> node_to_alns;
-    
-    std::function<void(Alignment&)> node_grabber = [&](Alignment& aln){
+
+    std::function<void(Alignment &)> node_grabber = [&](Alignment &aln) {
         vector<int64_t> ret;
         Path p = aln.path();
-        for (int i = 0; i < p.mapping_size(); i++){
+        for (int i = 0; i < p.mapping_size(); i++)
+        {
             Mapping m = p.mapping(i);
-            ret.push_back( m.position().node_id() );
-            
+            ret.push_back(m.position().node_id());
         }
-        for (auto x : ret){
+        for (auto x : ret)
+        {
             node_to_alns[x].push_back(aln.name());
         }
     };
-
-
 }
 
-bool GAMSorter::min_aln_first(Alignment& a, Alignment& b){
+bool GAMSorter::min_aln_first(Alignment &a, Alignment &b)
+{
 
-    if (less_than( get_min_position(a), get_min_position(b))){
+    if (less_than(get_min_position(a), get_min_position(b)))
+    {
         return true;
     }
-    else{
+    else
+    {
         return false;
     }
 }
 
-Position GAMSorter::get_min_position(Alignment a){
+Position GAMSorter::get_min_position(Alignment a)
+{
     return get_min_position(a.path());
 }
 
-Position GAMSorter::get_min_position(Path pat){
+Position GAMSorter::get_min_position(Path pat)
+{
     Position p = pat.mapping(0).position();
     Position p_prime = pat.mapping(pat.mapping_size() - 1).position();
     return less_than(p, p_prime) ? p : p_prime;
 }
 
-bool GAMSorter::equal_to(Position a, Position b){
+bool GAMSorter::equal_to(Position a, Position b)
+{
     if (a.node_id() == b.node_id() &&
-        a.offset() == b.offset()){
-            return true;
-        }
+        a.offset() == b.offset())
+    {
+        return true;
+    }
     return false;
 }
 
-bool GAMSorter::less_than(Position a, Position b){
-    if (a.node_id() > b.node_id()){
+bool GAMSorter::less_than(Position a, Position b)
+{
+    if (a.node_id() > b.node_id())
+    {
         return false;
     }
-    else{
+    else
+    {
         return (a.node_id() == b.node_id()) ? (a.offset() < b.offset()) : true;
     }
-
 }
 
-bool GAMSorter::greater_than(Position a, Position b){
-    if (a.node_id() < b.node_id()){
+bool GAMSorter::greater_than(Position a, Position b)
+{
+    if (a.node_id() < b.node_id())
+    {
         return false;
     }
-    else{
+    else
+    {
         return (a.node_id() == b.node_id()) ? (a.offset() > b.offset()) : true;
     }
 }
--- src/subcommand/gamsort_main.cpp
+++ src/subcommand/gamsort_main.cpp
@@ -1,7 +1,8 @@
-#include "GAMSorter.hpp"
-#include "stream.hpp"
 #include <getopt.h>
 #include "subcommand.hpp"
+#include "index.hpp"
+#include "GAMSorter.hpp"
+#include "stream.hpp"
 
 /**
 * GAM sort main
@@ -15,18 +16,24 @@ void help_gamsort(char **argv)
     cerr << "gamsort: sort a GAM file (or index it) without Rocksdb" << endl
          << "Usage: " << argv[1] << " [Options] gamfile" << endl
          << "Options:" << endl
-         << "  -i / --index <INDEX>    produce a node-to-alignment index"
-         << "  -d / --dumb-sort      use naive sorting algorithm (no tmp files, faster for small GAMs)"
+         << "  -p / --paired           Index a paired-end GAM." << endl
+         << "  -s / --sorted           Input GAM is already sorted." << endl
+         << "  -i / --index <INDEX>    produce a node-to-alignment index" << endl
+         << "  -d / --dumb-sort        use naive sorting algorithm (no tmp files, faster for small GAMs)" << endl
+         << "  -r / --rocks            Just use the old RocksDB-style indexing scheme for sorting." << endl
+         << "  -a / --aln-index        Create the old RocksDB-style node-to-alignment index." << endl
          << endl;
 }
 
 int main_gamsort(int argc, char **argv)
 {
     string gamfile;
-    string index_file;
+    bool do_index = false;
     bool dumb_sort = false;
     bool is_paired = false;
+    bool is_sorted = false;
     bool just_use_rocks = false;
+    bool do_aln_index = false;
     int c;
     optind = 2; // force optind past command positional argument
     while (true)
@@ -36,10 +43,12 @@ int main_gamsort(int argc, char **argv)
                 {"index", required_argument, 0, 'i'},
                 {"dumb-sort", no_argument, 0, 'd'},
                 {"paired", no_argument, 0, 'p'},
-                {"rocks", no_argument, 0, 'R'},
+                {"rocks", no_argument, 0, 'r'},
+                {"aln-index", no_argument, 0, 'a'},
+                {"is-sorted", no_argument, 0, 's'},
                 {0, 0, 0, 0}};
         int option_index = 0;
-        c = getopt_long(argc, argv, "i:dhR",
+        c = getopt_long(argc, argv, "i:dhraps",
                         long_options, &option_index);
 
         // Detect the end of the options.
@@ -49,13 +58,22 @@ int main_gamsort(int argc, char **argv)
         switch (c)
         {
         case 'i':
-            index_file = optarg;
+            do_index = true;
             break;
         case 'd':
             dumb_sort = true;
             break;
-        case 'R':
+        case 's':
+            is_sorted = true;
+            break;
+        case 'r':
             just_use_rocks = true;
+            break;
+        case 'a':
+            do_aln_index = true;
+            break;
+        case 'p':
+            is_paired = true;
             break;
         case 'h':
         case '?':
@@ -65,6 +83,11 @@ int main_gamsort(int argc, char **argv)
         }
     }
 
+
+    if (argc <= 2){
+        help_gamsort(argv);
+        exit(11);
+    }
     gamfile = argv[optind];
 
     GAMSorter gs;
@@ -74,20 +97,46 @@ int main_gamsort(int argc, char **argv)
         // Do the sort the old way - write a big ol'
         // RocksDB index of alignments, then dump them
         // from that DB. Loses unmapped reads.
+        string dbname = gamfile + "grai";
+        Index index;
+
+        index.open_for_bulk_load(dbname);
+        int64_t aln_idx = 0;
+        function<void(Alignment&)> lambda_reader = [&index](Alignment& aln) {
+                index.put_alignment(aln);
+        };
+        get_input_file(gamfile, [&](istream& in) {
+            stream::for_each_parallel(in, lambda_reader);
+        });
+
+        vector<Alignment> output_buf;
+        auto lambda_writer = [&output_buf](const Alignment& aln) {
+                output_buf.push_back(aln);
+                stream::write_buffered(cout, output_buf, 100);
+            };
+        index.for_each_alignment(lambda_writer);
+        ofstream outstream;
+        outstream.open(dbname);
+        //stream::write_buffered(outstream, output_buf, 0);
+        index.flush();
+        index.close();
     }
 
     if (dumb_sort)
     {
         gs.dumb_sort(gamfile);
     }
-    else if (index_file.empty() && !dumb_sort)
+    else if (!dumb_sort)
     {
         cerr << "Smart sort not implemented :' ( " << endl;
         exit(11);
     }
 
-    if (!index_file.empty())
+    if (do_index && just_use_rocks)
     {
+    }
+    
+    else if (do_index){
         // Write a super simple index file of format
         // node_d \tab firstAlnLineNumber \tab secondAlnLineNumber ...
         gs.write_index(gamfile, gamfile + ".gai", false);
