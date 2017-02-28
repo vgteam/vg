aaa5e153560535176d28ad4f912de60da4d036d7
Erik Garrison <erik.garrison@gmail.com>
2017-02-28 17:36:17 +0100

    set extra multimaps to 32
    This provides much better performance than 16 in yeast tests.

--- src/main.cpp
+++ src/main.cpp
@@ -5414,7 +5414,7 @@ int main_map(int argc, char** argv) {
     int gap_extend = 1;
     int full_length_bonus = 5;
     bool qual_adjust_alignments = false;
-    int extra_multimaps = 16;
+    int extra_multimaps = 32;
     int max_mapping_quality = 60;
     int method_code = 1;
     string gam_input;
