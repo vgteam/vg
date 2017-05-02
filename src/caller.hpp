9d101bccc2731d0c67a84ede8fcf18bcefb73410
Adam Novak <anovak@soe.ucsc.edu>
2017-05-02 16:27:00 -0700

    Impose a max depth of 60

--- src/caller.hpp
+++ src/caller.hpp
@@ -556,7 +556,7 @@ public:
     Option<size_t> min_mad_for_filter{this, "min-mad", "E", 5,
         "min. minimum allele depth to PASS filter"};
     /// what's the maximum total depth to give a PASS in the filter column
-    Option<size_t> max_dp_for_filter{this, "max-dp", "MmDdAaXxPp", 0,
+    Option<size_t> max_dp_for_filter{this, "max-dp", "MmDdAaXxPp", 60,
         "max depth to PASS filter (0 for unlimited)"};
     /// what's the maximum total depth to give a PASS in the filter column, as a
     /// multiple of the baseline coverage?
