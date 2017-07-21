aedee01a1a094bf6124f78ca62bf8a51df3aa48f
Glenn Hickey <glenn.hickey@gmail.com>
2017-07-21 18:06:44 -0400

    update default toil-vg

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -24,7 +24,7 @@ KEEP_OUTPUT=0
 # Should we show stdout and stderr from tests? If so, set to "-s".
 SHOW_OPT=""
 # What toil-vg should we install?
-TOIL_VG_PACKAGE="git+https://github.com/adamnovak/toil-vg.git@ff8116f466c26711d3f4c84f2a293895bf7f636d"
+TOIL_VG_PACKAGE="git+https://github.com/bd2kgenomics/toil-vg.git@af43e3d23f71b3d011f7d71a6f0906b86a8bb7b8"
 # What tests should we run?
 # Should be something like "jenkins/vgci.py::VGCITest::test_sim_brca2_snp1kg"
 PYTEST_TEST_SPEC="jenkins/vgci.py"
