b271599a4f07caa099c7892268f32331269914cb
Adam Novak <anovak@soe.ucsc.edu>
2017-08-04 16:23:59 -0700

    Add a test that shows us failing to grab enough graph when a MEM starts at a read and ref start

--- src/unittest/mapper.cpp
+++ src/unittest/mapper.cpp
@@ -148,6 +148,152 @@ TEST_CASE( "Mapper can map to a one-node graph", "[mapping][mapper]" ) {
     delete lcpidx;
 }
 
+TEST_CASE( "Mapper finds optimal mapping for read starting with node-border MEM", "[mapping][mapper]" ) {
+    
+    // We have a node 9999 in here to bust some MEM we don't want, to trigger the condition we are trying to test
+    string graph_json = R"(
+    {"node":[{"id":1430,"sequence":"GAGATCGTGCTACCGCACTCCATGCACTCTAG"},
+    {"id":1428,"sequence":"C"},
+    {"id":1429,"sequence":"T"},
+    {"id":1431,"sequence":"CCTGGGCAACAGAACGAGATGCTGTC"},
+    {"id":1427,"sequence":"AGGTTGGAGTGAGC"},
+    {"id":1432,"sequence":"ACA"},
+    {"id":1433,"sequence":"ACAACAACAACAACAA"},
+    {"id":1426,"sequence":"GAGGCAGGAAAATCACTTGAACCGGGAGGCGG"},
+    {"id":1434,"sequence":"T"},
+    {"id":1435,"sequence":"C"},
+    {"id":1424,"sequence":"C"},
+    {"id":1425,"sequence":"T"},
+    {"id":1436,"sequence":"AACAACAACAA"},
+    {"id":1423,"sequence":"TCGGGAGGC"},
+    {"id":1437,"sequence":"T"},
+    {"id":1438,"sequence":"C"},
+    {"id":1421,"sequence":"T"},
+    {"id":1422,"sequence":"C"},
+    {"id":1439,"sequence":"AACAACAACAACAA"},
+    {"id":1420,"sequence":"TA"},
+    {"id":1440,"sequence":"A"},
+    {"id":1441,"sequence":"C"},
+    {"id":1418,"sequence":"A"},
+    {"id":1419,"sequence":"C"},
+    {"id":1442,"sequence":"AA"},
+    {"id":1417,"sequence":"TGCCTGTAATCCCAG"},
+    {"id":1443,"sequence":"C"},
+    {"id":1444,"sequence":"A"},
+    {"id":1416,"sequence":"AAATACAAGTATTAGCCAGGCATTGTGGCAGG"},
+    {"id":1445,"sequence":"TTCTCACATCTAAAACAGAGTTCCTGGTTCCA"},
+    {"id":9999,"sequence":"TGTTGTTGTTGTTGTGTTNCTCATTTCGTTGCCAAT"}
+    ],"edge":[{"to":1431,"from":1430},
+    {"to":1430,"from":1428},
+    {"to":1430,"from":1429},
+    {"to":1432,"from":1431},
+    {"to":1433,"from":1431},
+    {"to":1428,"from":1427},
+    {"to":1429,"from":1427},
+    {"to":1433,"from":1432},
+    {"to":1434,"from":1433},
+    {"to":1435,"from":1433},
+    {"to":1427,"from":1426},
+    {"to":1436,"from":1434},
+    {"to":1436,"from":1435},
+    {"to":1426,"from":1424},
+    {"to":1426,"from":1425},
+    {"to":1437,"from":1436},
+    {"to":1438,"from":1436},
+    {"to":1424,"from":1423},
+    {"to":1425,"from":1423},
+    {"to":1439,"from":1437},
+    {"to":1439,"from":1438},
+    {"to":1423,"from":1421},
+    {"to":1423,"from":1422},
+    {"to":1440,"from":1439},
+    {"to":1441,"from":1439},
+    {"to":1421,"from":1420},
+    {"to":1422,"from":1420},
+    {"to":1442,"from":1440},
+    {"to":1442,"from":1441},
+    {"to":1420,"from":1418},
+    {"to":1420,"from":1419},
+    {"to":1443,"from":1442},
+    {"to":1444,"from":1442},
+    {"to":1418,"from":1417},
+    {"to":1419,"from":1417},
+    {"to":1445,"from":1443},
+    {"to":1445,"from":1444},
+    {"to":1417,"from":1416}],"path":[{"name":"17","mapping":[{"position":{"node_id":1416},"rank":1040},
+    {"position":{"node_id":1417},"rank":1041},
+    {"position":{"node_id":1419},"rank":1042},
+    {"position":{"node_id":1420},"rank":1043},
+    {"position":{"node_id":1422},"rank":1044},
+    {"position":{"node_id":1423},"rank":1045},
+    {"position":{"node_id":1425},"rank":1046},
+    {"position":{"node_id":1426},"rank":1047},
+    {"position":{"node_id":1427},"rank":1048},
+    {"position":{"node_id":1429},"rank":1049},
+    {"position":{"node_id":1430},"rank":1050},
+    {"position":{"node_id":1431},"rank":1051},
+    {"position":{"node_id":1433},"rank":1052},
+    {"position":{"node_id":1435},"rank":1053},
+    {"position":{"node_id":1436},"rank":1054},
+    {"position":{"node_id":1438},"rank":1055},
+    {"position":{"node_id":1439},"rank":1056},
+    {"position":{"node_id":1441},"rank":1057},
+    {"position":{"node_id":1442},"rank":1058},
+    {"position":{"node_id":1444},"rank":1059},
+    {"position":{"node_id":1445},"rank":1060}]}]}
+    )";
+    
+    // Load the JSON
+    Graph proto_graph;
+    json2pb(proto_graph, graph_json.c_str(), graph_json.size());
+    
+    // Make it into a VG
+    VG graph;
+    graph.extend(proto_graph);
+    
+    // Configure GCSA temp directory to the system temp directory
+    gcsa::TempFile::setDirectory(find_temp_dir());
+    // And make it quiet
+    gcsa::Verbosity::set(gcsa::Verbosity::SILENT);
+    
+    // Make pointers to fill in
+    gcsa::GCSA* gcsaidx = nullptr;
+    gcsa::LCPArray* lcpidx = nullptr;
+    
+    // Build the GCSA index
+    graph.build_gcsa_lcp(gcsaidx, lcpidx, 16, false, false, 3);
+    
+    // Build the xg index
+    xg::XG xg_index(proto_graph);
+    
+    // Make a multipath mapper to map against the graph.
+    Mapper mapper(&xg_index, gcsaidx, lcpidx);
+    mapper.set_alignment_scores(1, 4, 6, 1, 5);
+    
+    SECTION( "Mapper can map a read starting with a node-border MEM" ) {
+    
+        Alignment aln;
+        aln.set_sequence("TTGTTGTTGTTGTTGTGTTGTTGGGACAGCCATCTCATTTCGTTGCCAATGCTAGAGTGCATGGAG"
+                         "TGCGGTAGCACGATCTCAGCTCACTCCAACATCCGCCTCTCGGTTCAAGAGATTTTCCTGGTTCAG"
+                         "CCTCCCGAGTAGCTGGAT");
+                         
+        // Map to the graph
+        auto results = mapper.align_multi(aln);
+        
+        // We want one alignment.
+        REQUIRE(results.size() == 1);
+        // We want its first mapping to be to 1436 like the optimal alignment.
+        REQUIRE(results.front().path().mapping_size() >= 1);
+        REQUIRE(results.front().path().mapping(0).position().node_id() == 1436);
+    
+    }
+    
+    // Clean up the GCSA/LCP index
+    delete gcsaidx;
+    delete lcpidx;
+    
+}
+
 }
 
 }
