3b71e49a85c5511a4918d4b4af7226f0f2bf8d33
Adam Novak <anovak@soe.ucsc.edu>
2017-09-05 17:12:02 -0700

    Compare script works

--- scripts/compare-graphs.sh
+++ scripts/compare-graphs.sh
@@ -3,20 +3,42 @@
 
 set -e
 
-# Take the base names for the graphs
-GRAPH_NAMES=( snp1kg primary )
+usage() {
+    # Print usage to stderr
+    exec 1>&2
+    printf "Usage: $0 [Options] OUTPUT_PATH KEYPAIR_NAME REGION_NAME GRAPH [GRAPH [GRAPH ...]] \n"
+    printf "Options:\n\n"
+    exit 1
+}
 
-# And the region to run
-REGION_NAME="BRCA1"
+while getopts "h" o; do
+    case "${o}" in
+        *)
+            usage
+            ;;
+    esac
+done
 
-# What keypair should we set up our cluster with?
-KEYPAIR_NAME="anovak"
+shift $((OPTIND-1))
 
-# Where do we keep our input files
-INPUT_STORE="https://cgl-pipeline-inputs.s3.amazonaws.com/vg_cgl/bakeoff"
+if [[ "$#" -lt "4" ]]; then
+    # Too few arguments
+    usage
+fi
 
-# What path do we want to dump our output in on the local machine?
-OUTPUT_PATH="./out"
+OUTPUT_PATH="${1}"
+shift
+KEYPAIR_NAME="${1}"
+shift
+REGION_NAME="${1}"
+shift
+
+GRAPH_NAMES=( )
+while [[ "$#" -gt "0" ]]; do
+    # Put all the args as graph names
+    GRAPH_NAMES+=("$1")
+    shift
+done
 
 # What's our unique run ID? Should be lower-case and start with a letter for maximum compatibility.
 # See <https://gist.github.com/earthgecko/3089509>
@@ -25,6 +47,9 @@ RUN_ID="run$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1)"
 # What cluster should we use?
 CLUSTER_NAME="${RUN_ID}"
 
+# Where do we keep our input files
+INPUT_STORE="https://cgl-pipeline-inputs.s3.amazonaws.com/vg_cgl/bakeoff"
+
 # Where do we save our results from the various jobs responsible for writing them?
 OUTPUT_STORE="aws:us-west-2:cgl-pipeline-inputs/vg_cgl/comparison-script/runs/${RUN_ID}"
 OUTPUT_STORE_URL="s3://cgl-pipeline-inputs/vg_cgl/comparison-script/runs/${RUN_ID}"
@@ -32,7 +57,7 @@ OUTPUT_STORE_URL="s3://cgl-pipeline-inputs/vg_cgl/comparison-script/runs/${RUN_I
 # Where do we store our jobs?
 JOB_TREE="aws:us-west-2:${RUN_ID}"
 
-echo "Running run ${RUN_ID}"
+echo "Running run ${RUN_ID} as ${KEYPAIR_NAME} to compare ${GRAPH_NAMES[*]} on ${REGION_NAME} into ${OUTPUT_PATH}"
 
 function get_input_url() {
     # Prints the input URL to download for the given file name
@@ -47,6 +72,15 @@ function get_graph_url() {
     shift
     get_input_url "${BASE_GRAPHNAME}-${REGION_NAME}"
 }
+
+# Make sure we don't leave the cluster running on exit.
+function kill_cluster() {
+    set +e
+    aws s3 rm --recursive "${OUTPUT_STORE_URL}"
+    toil clean "${JOB_TREE}"
+    toil destroy-cluster "${CLUSTER_NAME}" -z us-west-2a
+}
+trap kill_cluster EXIT
 
 # Convert just graph stems to full base urls
 GRAPH_URLS=()
@@ -66,7 +100,7 @@ toil ssh-cluster --insecure --zone=us-west-2a "${CLUSTER_NAME}" virtualenv --sys
 toil ssh-cluster --insecure --zone=us-west-2a "${CLUSTER_NAME}" venv/bin/pip install numpy
 toil ssh-cluster --insecure --zone=us-west-2a "${CLUSTER_NAME}" venv/bin/pip install scipy
 toil ssh-cluster --insecure --zone=us-west-2a "${CLUSTER_NAME}" venv/bin/pip install scikit-learn
-toil ssh-cluster --insecure --zone=us-west-2a "${CLUSTER_NAME}" venv/bin/pip install git+https://github.com/adamnovak/toil-vg.git@2fae08f2cdb86e689244e33cf8a6705b01f42a6c
+toil ssh-cluster --insecure --zone=us-west-2a "${CLUSTER_NAME}" venv/bin/pip install git+https://github.com/adamnovak/toil-vg.git@e5dfb2d17b8e3262938b0894847ed5f083a2ccd0
 
 # We need the master's IP to make Mesos go
 MASTER_IP="$(toil ssh-cluster --insecure --zone=us-west-2a "${CLUSTER_NAME}" hostname -i)"
@@ -86,8 +120,5 @@ toil ssh-cluster --insecure --zone=us-west-2a "${CLUSTER_NAME}" venv/bin/toil-vg
     
 mkdir -p ./out
 aws s3 sync "${OUTPUT_STORE_URL}" "${OUTPUT_PATH}"
-aws s3 rm --recursive "${OUTPUT_STORE_URL}"
 
-toil clean "${JOB_TREE}"
-
-toil destroy-cluster "${CLUSTER_NAME}" -z us-west-2a
+# Cluster, tree, and output will get cleaned up by the exit trap
