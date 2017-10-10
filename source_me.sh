93f1f26e5ed854aa9d7fe9a193842c272abe1bbf
Adam Novak <anovak@soe.ucsc.edu>
2017-10-10 14:10:41 -0700

    Actually keep configure.py

--- source_me.sh
+++ source_me.sh
@@ -1,15 +1,10 @@
-# Work out where this script is.
-# See <https://stackoverflow.com/a/246128> and
-# <https://unix.stackexchange.com/q/4650>
-VG_ROOT="$(dirname "$(readlink -f "$_")")"
-
-export LIBRARY_PATH="${VG_ROOT}/lib:${LIBRARY_PATH}"
-export LD_LIBRARY_PATH="${VG_ROOT}/lib:${LD_LIBRARY_PATH}"
-export LD_INCLUDE_PATH="${VG_ROOT}/include:${LD_INCLUDE_PATH}"
-export C_INCLUDE_PATH="${VG_ROOT}/include:${C_INCLUDE_PATH}"
-export CPLUS_INCLUDE_PATH="${VG_ROOT}/include:${CPLUS_INCLUDE_PATH}"
-export INCLUDE_PATH="${VG_ROOT}/include:${INCLUDE_PATH}"
-export PATH="${VG_ROOT}/bin:${VG_ROOT}/scripts:${PATH}"
+export LIBRARY_PATH=`pwd`/lib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
+export LD_INCLUDE_PATH=`pwd`/include:$LD_INCLUDE_PATH
+export C_INCLUDE_PATH=`pwd`/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=`pwd`/include:$CPLUS_INCLUDE_PATH
+export INCLUDE_PATH=`pwd`/include:$INCLUDE_PATH
+export PATH=`pwd`/bin:`pwd`/scripts:$PATH
 export CC=$(which gcc)
 export CXX=$(which g++)
 
