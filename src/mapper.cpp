ae45a39c2d8956764358b34d8e6f3149259ddc23
Erik Garrison <erik.garrison@gmail.com>
2017-10-27 12:09:49 +0200

    this appears to be too aggressive a filter

--- src/mapper.cpp
+++ src/mapper.cpp
@@ -2426,7 +2426,6 @@ pair<vector<Alignment>, vector<Alignment>> Mapper::align_paired_multi(
     double mqmax2 = max_mapping_quality;
     // calculate paired end quality if the model assumptions are not obviously violated
     if (results.first.size() && results.second.size()
-        && maybe_mq1 > 2 && maybe_mq2 > 2
         && pair_consistent(results.first.front(), results.second.front(), 1e-6)) {
         compute_mapping_qualities(results, cluster_mq, mq_cap1, mq_cap2, mqmax1, mqmax2);
     } else {
