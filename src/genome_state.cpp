1bc9956712da6a828fbc47d1252386d7a92eace5
Adam Novak <anovak@soe.ucsc.edu>
2017-11-06 10:54:11 -0800

    Start implementing a state system that might work

--- src/genome_state.cpp
+++ src/genome_state.cpp
@@ -1,1 +1,25 @@
 #include "genome_state.hpp"
+
+namespace vg {
+
+using namespace std;
+
+GenomeState::GenomeState(const SnarlManager& manager, const HandleGraph* graph,
+    const unordered_set<pair<const Snarl*, const Snarl*>> telomeres) : telomeres(telomeres), manager(manager) {
+
+    manager.for_each_snarl_preorder([&](const Snarl* snarl) {
+        // For each snarl
+        
+        // Make an empty state for it
+        state.emplace(snarl, SnarlState());
+        
+        // Make a net graph for it. TODO: we're not considering internal
+        // connectivity, but what we really should do is consider internal
+        // connectivity but only allowing for start to end traversals (but
+        // including in unary snarls)
+        net_graphs.emplace(snarl, manager.net_graph_of(snarl, graph, false));
+    });
+}
+
+
+}
--- src/genome_state.hpp
+++ src/genome_state.hpp
@@ -29,21 +29,6 @@ using namespace std;
  */
 class SnarlState {
 
-public:
-    
-    /**
-     * How many haplotypes traverse this snarl?
-     */
-    size_t size();
-
-    // We can add, remove, and swap haplotypes by rank.
-    // All these operations are meant to be reversible.
-    // TODO: Write real operation descriptions
-    void insert(size_t rank, const vector<handle_t>& haplotype);
-    vector<handle_t> replace(size_t rank, const vector<handle_t>& haplotype);
-    vector<handle_t> erase(size_t rank);
-    void swap(size_t rank1, size_t rank2);
-
 protected:
     /**
      * Store a vector of haplotype traversals of this snarl from start to end.
@@ -52,7 +37,42 @@ protected:
      * start and end nodes are included.
      */
     vector<vector<handle_t>> haplotypes;
+
+public:
     
+    /// How many haplotypes traverse this snarl?
+    size_t size() const;
+    
+    /// We define a way to iterate over the contained haplotypes
+    using const_iterator = decltype(haplotypes)::const_iterator;
+    
+    /// Get an iterator to the first haplotype
+    const_iterator begin() const;
+    
+    /// Get an iterator to the past-the-end haplotype
+    const_iterator end() const;
+    
+    /// Get the haplotype at the given rank.
+    const vector<handle_t>& at(size_t rank) const;
+
+    // We can add, remove, and swap haplotypes by rank.
+    // All these operations are meant to be reversible.
+    
+    /// Insert the given traversal of this snarl from start to end as a
+    /// haplotype with the given overall rank. The first rank is 0. Pushes
+    /// everything with a higher rank 1 rank up.
+    void insert(size_t rank, const vector<handle_t>& haplotype);
+    
+    /// Replace the traversal of this haplotype at the given rank with the given
+    /// new traversal from start to end. Returns the replaced haplotype.
+    vector<handle_t> replace(size_t rank, const vector<handle_t>& haplotype);
+    
+    /// Erase the traversal of this haplotype with the given rank. Shifts
+    /// everything with a higher rank 1 rank down. Returns the erased haplotype.
+    vector<handle_t> erase(size_t rank);
+    
+    /// Swap the traversals of this haplotype with the two given ranks.
+    void swap(size_t rank1, size_t rank2);
     
 };
 
@@ -67,7 +87,8 @@ struct GenomeStateCommand {
     virtual ~GenomeStateCommand() = default;
     
     /// Execute this command on the given state and return the reverse command.
-    virtual GenomeStateCommand* execute(GenomeState& state) const;
+    /// Generally ends up calling a command-type-specific method on the GenomeState that does the actual work.
+    virtual GenomeStateCommand* execute(GenomeState& state) const = 0;
 };
 
 struct InsertHaplotypeCommand : public GenomeStateCommand {
@@ -77,19 +98,25 @@ struct InsertHaplotypeCommand : public GenomeStateCommand {
     /// To be executed in order from left to right (becasue inserting at ranks changes ranks).
     vector<tuple<const Snarl*, size_t, vector<handle_t>>> insertions;
     
+    virtual GenomeStateCommand* execute(GenomeState& state) const;
+    
     virtual ~InsertHaplotypeCommand() = default;
 };
 
 struct DeleteHaplotypeCommand : public GenomeStateCommand {
     /// In each (top level) snarl's state, delete the haplotype at the given rank.
-    
     vector<pair<const Snarl*, size_t>> deletions;
+    
+    virtual GenomeStateCommand* execute(GenomeState& state) const;
 
     virtual ~DeleteHaplotypeCommand() = default;
 };
 
 struct SwapHaplotypesCommand : public GenomeStateCommand {
+    /// Swap the haplotypes at the given ranks
     pair<size_t, size_t> to_swap;
+    
+    virtual GenomeStateCommand* execute(GenomeState& state) const;
     
     virtual ~SwapHaplotypesCommand() = default;
 };
@@ -98,6 +125,8 @@ struct CreateHaplotypeCommand : public GenomeStateCommand {
     /// This is the root snarl to create a new traversal of.
     /// TODO: maybe should be a chain?
     const Snarl* root;
+    
+    virtual GenomeStateCommand* execute(GenomeState& state) const;
     
     virtual ~CreateHaplotypeCommand() = default;
 };
@@ -114,32 +143,46 @@ class GenomeState {
 
 public:
     
+    /// Make a new GenomeState on the given SnarlManager, manageing snarls in
+    /// the given graph, with the given telomere pairs
+    GenomeState(const SnarlManager& manager, const HandleGraph* graph,
+        const unordered_set<pair<const Snarl*, const Snarl*>> telomeres);
+    
     // We execute commands and return the inverse commands
     
+    /// Create a haplotype and return a command to delete it
     DeleteHaplotypeCommand create_haplotype(const CreateHaplotypeCommand& c);
     
+    /// Insert a haplotype and return a command to delete it
     DeleteHaplotypeCommand insert_haplotype(const InsertHaplotypeCommand& c);
     
+    /// Delete a haplotype and return a command to insert it
     InsertHaplotypeCommand delete_haplotype(const DeleteHaplotypeCommand& c);
     
+    /// Swap two haplotypes and return a command to swap them back
     SwapHaplotypesCommand swap_haplotypes(const SwapHaplotypesCommand& c);
     
-    /// Execute a command.
-    /// Return a new heap-allocated command that undoes the command being executed.
-    /// Frees the passed command. TODO: does that make sense?
+    /// Execute a command. Return a new heap-allocated command that undoes the
+    /// command being executed. Frees the passed command. TODO: does that make
+    /// sense?
     GenomeStateCommand* execute(GenomeStateCommand* command);
      
 protected:
-    /// We have a state for every snarl. TODO: What about how top-level snarls
-    /// form chains? Mightn't you need to traverse several? We don't really have
-    /// a root-level state.
+    /// We keep track of pairs of telomere unary snarls. The haplotypes we work
+    /// on connect a left telomere and its corresponding right telomere. We can
+    /// traverse the snarl decomposition from one telomere to the other either
+    /// following chains or pollowing paths inside of some snarl we are in.
+    unordered_set<pair<const Snarl*, const Snarl*>> telomeres;
+
+    /// We have a state for every snarl.
     unordered_map<const Snarl*, SnarlState> state;
     
     /// We precompute all the net graphs and keep them around.
     unordered_map<const Snarl*, NetGraph> net_graphs;
     
     /// We keep a reference to the SnarlManager that knows what children are
-    /// where.
+    /// where and which snarl we should look at next after leaving a previous
+    /// snarl.
     const SnarlManager& manager;
 
 };
