5227cb9bcf462876a8fac494f1354fbbee3f73cd
Jordan Eizenga <joeizeng@gmail.com>
2017-12-05 10:40:51 -0800

    don't override single path mode in jenkins

--- jenkins/vgci.py
+++ jenkins/vgci.py
@@ -501,7 +501,7 @@ class VGCITest(TestCase):
             # toil-vg map options
             # don't waste time sharding reads since we only run on one node
             single_reads_chunk = True,
-            mpmap_opts = ['-B']
+            mpmap_opts = ['-B', '-S']
         )
         
         # Make the context
