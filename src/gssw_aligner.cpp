d41434e572b60da46b6a1324852ccf7c7c3216c3
Jordan Eizenga <joeizeng@gmail.com>
2017-12-11 17:28:04 -0800

    resolve numerical problems in exact mapq algorithm

--- src/gssw_aligner.cpp
+++ src/gssw_aligner.cpp
@@ -342,7 +342,8 @@ double BaseAligner::maximum_mapping_quality_exact(vector<double>& scaled_scores,
             *max_idx_out = i;
         }
     }
-    return scaled_scores[*max_idx_out] > log_sum_exp ? -quality_scale_factor * subtract_log(0.0, scaled_scores[*max_idx_out] - log_sum_exp) : 0.0;
+    return scaled_scores[*max_idx_out] > log_sum_exp ?
+           -quality_scale_factor * subtract_log(0.0, scaled_scores[*max_idx_out] - log_sum_exp) : (double) numeric_limits<int32_t>::max();
 }
 
 // TODO: this algorithm has numerical problems that would be difficult to solve without increasing the
