1258539ce3cd7749c60e0550cd75cc4a8ac21fc0
Jouni Siren <jouni.siren@iki.fi>
2018-03-29 17:06:50 -0700

    Don't use std::move(). It will eat you alive.

--- src/vg_set.cpp
+++ src/vg_set.cpp
@@ -128,7 +128,7 @@ void VGset::to_xg(xg::XG& index, bool store_threads, const regex& paths_to_take,
                         }
                         
                         // Move the mapping into place
-                        mappings[path.name()][mapping.rank()] = move(mapping);
+                        mappings[path.name()][mapping.rank()] = mapping;
                     }
                 }
 
@@ -146,11 +146,11 @@ void VGset::to_xg(xg::XG& index, bool store_threads, const regex& paths_to_take,
                 
                 for(auto& rank_and_mapping : kv.second) {
                     // Put in all the mappings. Ignore the rank since thay're already marked with and sorted by rank.
-                    *path.add_mapping() = move(rank_and_mapping.second);
+                    *path.add_mapping() = rank_and_mapping.second;
                 }
                 
                 // Now the Path is rebuilt; stick it in the big output map.
-                removed_paths[path.name()] = move(path);
+                removed_paths[path.name()] = path;
             }
             
 #ifdef debug
