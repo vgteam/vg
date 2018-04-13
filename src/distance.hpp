7e379c1dcfdcc5b31ba0cecf3e84a8a93ea601b3
Xian Chang <xhchang15@gmail.com>
2018-04-13 16:02:11 -0700

    Wrote distance index

--- src/distance.hpp
+++ src/distance.hpp
@@ -4,8 +4,8 @@ namespace vg {
     
     struct ChainDistances {
         /*Stores distance information for snarls in a chain*/
-        unordered_map<Snarl, size_t> snarl_to_index;
-        vector<int64_t> prefix_sum;//Dist from start of chain to each snarl
+        unordered_map<id_t, size_t>* snarl_to_index;
+        vector<int64_t>* prefix_sum;//Dist from start of chain to each snarl
     };
 
 
@@ -15,16 +15,16 @@ namespace vg {
         visit_to_index maps each visit (node_id, negative if reverse) to an int
         distances stores all the distance bewteen each pair of visits in a snarl
         */
-        unordered_map<int, size_t> visit_to_index;
-        vector<int64_t> distances;
+        unordered_map<int, size_t>* visit_to_index;
+        vector<int64_t>* distances;
     };
 
 
     struct DistanceIndex {
 
         /*The distance index containing snarl and chain information*/
-        unordered_map<id_t, snarlDistances*> sd;
+        unordered_map<id_t, SnarlDistances*> sd;
              //map from node id of first nodein snarl
-        unordered_map<id_t, chainDistances*> cd;
+        unordered_map<id_t, ChainDistances*> cd;
     };
 }
