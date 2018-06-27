a9b67741a1a241eba19019806f435aab4806f940
Adam Novak <anovak@soe.ucsc.edu>
2018-06-27 16:10:52 -0700

    Implement GFA parsing except for paths and non-block segments

--- src/gfa.cpp
+++ src/gfa.cpp
@@ -11,21 +11,23 @@ using namespace gfak;
 class GFAToPinchTranslator {
 private:
     /// Map from string name to numerical name number
-    unordered_map<string, int64_t> name_to_name;
-    /// Track assigned numeric names
-    unordered_set<int64_t> used;
+    unordered_map<string, int64_t> forward;
+    /// Map from numerical name number back to string name
+    unordered_map<int64_t, string> backward;
     /// What is the next unused name we can assign?
     int64_t next_unused = 1;
 public:
     /// Translate from GFA name to pinch thread name
     int64_t translate(const string& name);
+    /// Translate back from pinch thread name to GFA name
+    const string& untranslate(const int64_t& name);
 }
 
 int64_t GFAToPinchTranslator::translate(const string& name) {
     // Look up the name
-    auto found = name_to_name.find(name);
+    auto found = forward.find(name);
     
-    if (found != name_to_name.end()) {
+    if (found != forward.end()) {
         // We have a translation already. Use it.
         return found->second;
     }
@@ -39,7 +41,7 @@ int64_t GFAToPinchTranslator::translate(const string& name) {
         assigned = std::stol(name);
     }
     
-    if (assigned <= 0 || used.count(assigned)) {
+    if (assigned <= 0 || backward.count(assigned)) {
          // We need to find an unused number.
          // next_unused is always guaranteed to be unused.
          assigned = next_unused;
@@ -54,7 +56,74 @@ int64_t GFAToPinchTranslator::translate(const string& name) {
     }
     
     // Save the assigned numeric name
-    name_to_name[name] = assigned;
+    forward[name] = assigned;
+    // Mark it used and record the back translation
+    backward[assigned] = name;
+    // Return it
+    return assigned;
+}
+
+const string& GFAToPinchTranslator::untranslate(const int64_t& name) {
+    // If it was translated, it must have a reverse entry
+    return backward.at(name);
+}
+
+/// Represents a translation from pinch thread block pointer to vg node ID.
+/// Tries to use pinch thread name numbers as IDs for single-thread blocks, and otherwise assigns IDs.
+class PinchToVGTranslator {
+private:
+    /// Map from block pointer to node ID
+    unordered_map<stPinchBlock*, id_t> block_to_id;
+    /// Track assigned numeric names
+    unordered_set<id_t> used;
+    /// What is the next unused name we can assign?
+    id_t next_unused = 1;
+public:
+    /// Translate from pinch thread block to node ID
+    id_t translate(stPinchBlock* block);
+}
+
+id_t PinchToVGTranslator::translate(stPinchBlock* block) {
+    // Look up the block
+    auto found = block_to_id.find(block);
+    
+    if (found != block_to_id.end()) {
+        // We have a translation already. Use it.
+        return found->second;
+    }
+    
+    // Otherwise we need to make a translation.
+    // Try to find an unused number to use.
+    // To start with, we have no clue what to use (0).
+    id_t assigned = 0;
+    
+    // Try to get the first thread in the block
+    auto iter = stPinchBlock_getSegmentIterator(block);
+    stPinchSegment* first_segment = stPinchBlockIt_getNext(&iter);
+    
+    if (first_segment != nullptr) {
+        // We found a segment. Try its name as the ID. If it is the only
+        // segment in the block, and the block the only block for it, then it
+        // is the right hint. Otherwise, it doesn't matter what we hint,
+        // because we get to assign IDs arbitrarily.
+        assigned = (id_t) stPinchSegment_getName(first_segment);
+    }
+    
+    if (assigned <= 0 || used.count(assigned)) {
+         // We need to find an unused number.
+         // next_unused is always guaranteed to be unused.
+         assigned = next_unused;
+         next_unused++;
+    }
+    
+    if (assigned >= next_unused) {
+        // Budge out the assignment cursor past any numbers yet mentioned in the pinch names.
+        // This is guaranteed to be past the largest assigned ID, and therefore unused.
+        next_unused = assigned + 1;
+    }
+    
+    // Save the assigned ID
+    block_to_id[block] = assigned;
     // Mark it used
     used.insert(assigned);
     // Return it
@@ -143,6 +212,11 @@ void gfa_to_graph(istream& in, VG* graph, bool only_perfect_match) {
         stPinchThreadSet_addThread(pinch, pinch_name, 0, record.sequence.size());
     }
     
+    // As we go through the links, we need to remmeber links which are straight abutments of sequences.
+    // These won't be processed by the pinch graph; we need to store them and process them later.
+    // We store them in pinch thread name terms, as source, sink, source reverse, sink reverse
+    vector<tuple<int64_t, int64_t, bool, bool>> abut_links;
+    
     for (auto& name_and_links : gfa_links) {
         // For each set of links, by source node name
         auto& name = name_and_links.first;
@@ -151,6 +225,11 @@ void gfa_to_graph(istream& in, VG* graph, bool only_perfect_match) {
         // Find the source pinch thread
         auto source_pinch_name = gfa_to_pinch.translate(name);
         auto source_thread = stPinchThreadSet_getThread(pinch, source_pinch_name);
+        
+        // And the source sequence string
+        auto& source_string = gfa_sequences[name].sequence;
+        // And sequence length
+        auto source_sequence_length = stPinchThread_getLength(stPinchThreadSet_getThread(pinch, source_pinch_name));
         
         for (auto& link : links) {
             // For each link on this source node
@@ -224,8 +303,18 @@ void gfa_to_graph(istream& in, VG* graph, bool only_perfect_match) {
             bool source_backward = !link.source_orientation_forward;
             bool sink_backward = !link.sink_orientation_backward;
             
-            // Get the sequence lengths
-            auto source_sequence_length = stPinchThread_getLength();
+            if (source_alignment_length == 0 && sing_alignment_length == 0) {
+                // This link is just an end-to-end abutment with no overlap.
+                // It can't be sent through the pinch graph; we store it separately.
+                abut_links.insert(tie(source_pinch_name, sink_pinch_name, source_backward, sink_backward));
+                
+                // Skip the link CIGAR execution
+                continue;
+            }
+            
+            // Find the sink sequence data
+            auto& sink_string = gfa_sequences[link.sink_name].sequence;
+            // And sequence length
             auto sink_sequence_length = stPinchThread_getLength(stPinchThreadSet_getThread(pinch, sink_pinch_name));
             
             // Set up some cursors in each node's sequence that go the right
@@ -244,73 +333,212 @@ void gfa_to_graph(istream& in, VG* graph, bool only_perfect_match) {
             
             for (auto& elem : cigar) {
                 // For each cigar operation
-                auto& length = elem.first;
                 
                 if (length == 0) {
                     // Skip 0-length operations
                     continue;
                 }
                 
-                // Work out the sequence-local start of the region in each sequence that it may apply to, which depends on orientation.
-                int64_t source_region_start = source_backward ? (source_cursor - length + 1) : source_cursor;
-                int64_t sink_region_start = sink_backward ? (sink_cursor - length + 1) : sink_cursor;
+                // Decompose each operation into a series of suboperations.
+                // This gives us an opportunity to replace M operations with =/X
+                vector<pair<size_t, string>> suboperations;
                 
-                assert(!elem.second.empty());
-                switch(elem.second[0]) {
-                case 'M':
-                    if (only_perfect_match) {
-                        // The whole match can be merged
+                if (elem.second == "M" && !only_perfect_match) {
+                    // This is an M operation that needs to be decomposed into = and X
+                    for (size_t i = 0; i < elem.first; i++) {
+                        // For each position along the M operation
+                        
+                        // Find the next character in the source
+                        auto source_char = source_string.at(source_cursor + source_motion * i);
+                        if (source_backward) {
+                            source_char = reverse_complement(source_char);
+                        }
+                        // And the sink
+                        auto sink_char = sink_string.at(sink_cursor + sink_motion * i);
+                        if (sink_backward) {
+                            sink_char = reverse_complement(sink_char);
+                        }
+                        // Work out what kind of operation we need for this pairing of bases.
+                        // TODO: Handle Ns specially?
+                        string opcode = (source_char == sink_char) ? "=" : "X";
+                        
+                        if (!suboperations.empty() && suboperations.back().second == opcode) {
+                            // We can accumulate onto the existing suboperation of this type
+                            suboperations.back().first++;
+                        } else {
+                            // We need a new suboperation of this type
+                            suboperations.push_back(make_pair(1, opcode));
+                        }
+                    }
+                } else {
+                    // This operation can be passed through as-is
+                    suboperations.push_back(elem);
+                }
+               
+                for (auto& subelem : suboperations) {
+                    // For each suboperation, get its length
+                    auto& length = subelem.first;
+                    
+                    // Work out the sequence-local start of the region in each sequence that it may apply to, which depends on orientation.
+                    int64_t source_region_start = source_backward ? (source_cursor - length + 1) : source_cursor;
+                    int64_t sink_region_start = sink_backward ? (sink_cursor - length + 1) : sink_cursor;
+                    
+                    assert(!subelem.second.empty());
+                    switch(subelem.second[0]) {
+                    case 'M':
+                        if (only_perfect_match) {
+                            // The whole match can be merged
+                            stPinchThread_pinch(source_thread, sink_thread, source_region_start, sink_region_start, length, pinch_same_strand);
+                            // Advance both cursors
+                            sink_cursor += sink_motion * length;
+                            source_cursor += source_motion * length;
+                        } else {
+                            // If we aren't in always_perfect_match mode this should have become =/X
+                            throw runtime_error("Encountered unparsed M operation");
+                        }
+                        break;
+                    case '=':
+                        // Always pinch.
+                        // TODO: should we check sequence equality?
                         stPinchThread_pinch(source_thread, sink_thread, source_region_start, sink_region_start, length, pinch_same_strand);
                         // Advance both cursors
                         sink_cursor += sink_motion * length;
                         source_cursor += source_motion * length;
-                    } else {
-                        // TODO: Break up the match into = and X operations according to the sequences
-                        throw runtime_error("Unimplemented");
+                        break;
+                    case 'X':
+                        // Only pinch if we are forcing matches (in which case this was X originally)
+                        if (only_perfect_match) {
+                            stPinchThread_pinch(source_thread, sink_thread, source_region_start, sink_region_start, length, pinch_same_strand);
+                        }
+                        // Advance both cursors
+                        sink_cursor += sink_motion * length;
+                        source_cursor += source_motion * length;
+                        break;
+                    case 'I':
+                        // We don't need to do any pinching, just advance the sink cursor.
+                        sink_cursor += sink_motion * length;
+                        break;
+                    case 'D':
+                        // We don't need to do any pinching, just advance the source cursor.
+                        source_cursor += source_motion * length;
+                        break;
+                    default:
+                        // We should ahve already checked for weird operations.
+                        throw runtime_error("Invalid operation " + subelem.second + " in pre-screened CIGAR");
                     }
-                    break;
-                case '=':
-                    // Always pinch.
-                    // TODO: should we check sequence equality?
-                    stPinchThread_pinch(source_thread, sink_thread, source_region_start, sink_region_start, length, pinch_same_strand);
-                    // Advance both cursors
-                    sink_cursor += sink_motion * length;
-                    source_cursor += source_motion * length;
-                    break;
-                case 'X':
-                    // Only pinch if we are forcing matches
-                    if (only_perfect_match) {
-                        stPinchThread_pinch(source_thread, sink_thread, source_region_start, sink_region_start, length, pinch_same_strand);
-                    }
-                    // Advance both cursors
-                    sink_cursor += sink_motion * length;
-                    source_cursor += source_motion * length;
-                    break;
-                case 'I':
-                    // We don't need to do any pinching, just advance the sink cursor.
-                    sink_cursor += sink_motion * length;
-                    break;
-                case 'D':
-                    // We don't need to do any pinching, just advance the source cursor.
-                    source_cursor += source_motion * length;
-                    break;
-                default:
-                    // We should ahve already checked for weird operations.
-                    throw runtime_error("Invalid operation " + elem.second + " in pre-screened CIGAR");
                 }
             }
+        }
+    }
+
+    // Now all the pinches have been made
+
+    // Convert the pinch blocks into vg nodes
+    
+    // We use this translator to translate block pointers to node IDs
+    PinchToVGTranslator pinch_to_vg;
+    
+    for (auto iter = stPinchThreadSet_getBlockIt(threads), stPinchBlock* block = stPinchThreadSetBlockIt_getNext(&iter);
+        block != nullptr;
+        block = stPinchThreadSetBlockIt_getNext(&iter)) {
+        
+        // For each block in the pinch set
+        
+        // Generate a node ID
+        id_t node_id = pinch_to_vg.translate(block);
+        
+        // Find the first segment
+        auto segment_iter = stPinchBlock_getSegmentIterator(block);
+        stPinchSegment* first_segment = stPinchBlockIt_getNext(&segment_iter);
+        
+        assert(first_segment != nullptr);
+        
+        // Get its thread name, + strand offset, length, and orientation
+        auto segment_thread_name = stPinchSegment_getName(first_segment);
+        auto segment_start = stPinchSegment_getStart(first_segment);
+        auto segment_length = stPinchSegment_getLength(first_segment);
+        auto segment_backward = !stPinchSegment_getBlockOrientation(segment);
+        
+        // Go find the source DNA for the GFA sequence that created the thread
+        const string& thread_sequence = gfa_sequences[gfa_to_pinch.untranslate(segment_thread_name)].sequence;
+        
+        // Compute the sequence for the vg node we will make
+        string node_sequence = thread_sequence.substr(segment_start, segment_length);
+        if (segment_backward) {
+            node_sequence = reverse_complement(node_sequence);
+        }
+        
+        // Make the node in the graph
+        graph->create_node(node_sequence, node_id);
+    }
+    
+    // Add edges from pinch graph adjacencies
+    
+    for (auto iter = stPinchThreadSet_getIt(pinch), stPinchThread* thread = stPinchThreadSetIt_getNext(&iter);
+        thread != nullptr;
+        thread = stPinchThreadSetIt_getNext(&iter)) {
+    
+        // For each thread in the pinch thread set
+        // Start at the beginning
+        stPinchSegment* here = stPinchThread_getFirst(thread);
+        assert(here != nullptr);
+        
+        // Look one segment ahead
+        stPinchSegment* next = stPinchSegment_get3Prime(here);
+        
+        while (next != nullptr) {
+            // For each pinch graph connection from here to next
             
-            // If it is an M
+            // Get the nodes we are connecting
+            id_t here_id = pinch_to_vg.translate(stPinchSegment_getBlock(here));
+            id_t next_id = pinch_to_vg.translate(stPinchSegment_getBlock(next));
             
-            // Grab the two sequences it is trying to match
+            // Get their orientations
+            bool here_backward = !stPinchSegment_getBlockOrientation(here);
+            bool next_backward = !stPinchSegment_getBlockOrientation(next);
             
-            // Pinch each run of equal bases
+            // Make the edge if not present already
+            graph->create_edge(here_id, next_id, here_backward, next_backward); 
+            
+            // Advance right
+            here = next;
+            next = stPinchSegment_get3Prime(here);
         }
     }
     
-    for (edge_elem l : seq_to_edges[(it->second).name]){
-
-
+    // Add edges from abut_links
+    for (auto& abutment : abut_links) {
+        // Unpack each abutment record
+        auto& source_name = abutment.get<0>();
+        auto& sink_name = abutment.get<1>();
+        auto& source_backward = abutment.get<2>();
+        auto& sink_backward = abutment.get<3>();
+        
+        // Get the threads by name
+        stPinchThread* source_thread = stPinchThreadSet_getThread(pinch, source_name);
+        stPinchThread* sink_thread = stPinchThreadSet_getThread(pinch, sink_name);
+        
+        // Find the segment of the source that is relevant.
+        // If the source sequence is forward, it is the last one, but if it is backward, it is the first one.
+        stPinchSegment* source_segment = source_backward ? stPinchThread_getFirst(source_thread) : stPinchThread_getLast(source_thread);
+        // And conversely for the sink
+        stPinchSegment* sink_segment = sink_backward ? stPinchThread_getLast(sink_thread) : stPinchThread_getFirst(sink_thread);
+    
+        // Get the node IDs to connect
+        id_t from_id = pinch_to_vg.translate(stPinchSegment_getBlock(source_segment));
+        id_t to_id = pinch_to_vg.translate(stPinchSegment_getBlock(sink_segment));
+        
+        // Figure out the orientation of each node. We take the node's
+        // orientation relative to the segment, and flip it if the segment
+        // itself is visited backward.
+        bool from_start = (stPinchSegment_getBlockOrientation(source_segment) != source_backward);
+        bool to_end = (stPinchSegment_getBlockOrientation(sink_segment) != sink_backward);
+        
+        // Make the edge
+        graph->create_edge(from_id, to_id, from_start, to_end); 
+    }
+    
+    // Process the GFA paths
 
 
     // Clean up the pinch thread set
