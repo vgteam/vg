3167b39f81285699015870cd344463f461dc4c7f
Eric T. Dawson <eric.t.dawson@gmail.com>
2018-06-28 15:59:52 -0500

    Refactor index_main to use zeroBasedPosition() for variants

--- src/subcommand/index_main.cpp
+++ src/subcommand/index_main.cpp
@@ -616,7 +616,8 @@ int main_index(int argc, char** argv) {
 
                     // Determine the reference nodes for the current variant and create a variant site.
                     // If the variant is not an insertion, there should be a path for the ref allele.
-                    var.position--; // Use a 0-based position to get the correct var_name.
+                    //var.position--; // Use a 0-based position to get the correct var_name.
+                    // UPDATE: Don't use a zero-based position!! 
                     std::string var_name = make_variant_id(var);
                     std::string ref_path_name = "_alt_" + var_name + "_0";
                     auto ref_path_iter = alt_paths.find(ref_path_name);
