b835cf69c542a3d91f856e6f110d4bb8c391c23f
Adam Novak <anovak@soe.ucsc.edu>
2018-07-24 15:22:25 -0700

    Finally get the bin computation working

--- src/gam_index.cpp
+++ src/gam_index.cpp
@@ -1,19 +1,60 @@
 #include "gam_index.hpp"
 
+#include <iostream>
+
 namespace vg {
 
 using namespace std;
 
 auto GAMIndex::bins_of_id(id_t id) -> vector<bin_t> {
-    return vector<bin_t>();
+    vector<bin_t> to_return;
+    
+    // How many bits are in the number? We get a bin per bit.
+    auto number_bits = CHAR_BIT * sizeof(bin_t);
+    
+    // We need to keep in mind that shifting *all* the bits out of a number in
+    // one go is undefined behavior.
+    
+    // Bin number consists of an index which is a prefix of the number
+    bin_t bin_index = ((bin_t)id) >> 1;
+    // And an offset to keep from colliding with other bins.
+    bin_t bin_offset = ((bin_t)~0) >> 1;
+    
+    for (int i = 0; i < number_bits; i++) {
+        
+#ifdef debug
+        cerr << hex << id << " " << i << " " << bin_index << " " << bin_offset << " " << (bin_index + bin_offset) << endl;
+#endif
+    
+        to_return.push_back(bin_index + bin_offset);
+        bin_index = bin_index >> 1;
+        bin_offset = bin_offset >> 1;
+    }
+    
+    return to_return;
 }
     
 auto GAMIndex::common_bin(id_t a, id_t b) -> bin_t {
-    return 0;
+    // Convert to unsigned numbers
+    bin_t a_bin = a;
+    bin_t b_bin = b;
+    
+    // Define the offset for the bin
+    bin_t offset = ((bin_t)~0);
+    
+    // We're just going to pop off bits until we find the common prefix.
+    // Always pop off one bit, even if we are binning a number and itself.
+    // TODO: Find a faster way to do this with the appropriate instruction intrinsics.
+    do {
+        a_bin = a_bin >> 1;
+        b_bin = b_bin >> 1;
+        offset = offset >> 1;
+    } while(a_bin != b_bin);
+    return a_bin + offset;
 }
 
 auto GAMIndex::window_of_id(id_t id) -> window_t  {
-    return 0;
+    return id >> WINDOW_SHIFT;
 }
 
 
--- src/unittest/gam_index.cpp
+++ src/unittest/gam_index.cpp
@@ -22,25 +22,36 @@ TEST_CASE("GAMindex windowing works correctly", "[gam][gamindex]") {
 
 }
 
-TEST_CASE("GAMindex binning works correctly", "[gam][gamindex]") {
+TEST_CASE("GAMindex binning works on a large number", "[gam][gamindex]") {
 
-    auto bins = GAMIndex::bins_of_id(0xACAC);
+    for (id_t to_bin : {0ULL, 1ULL, 10ULL, 0xFFFFFFFFFFFFFFFFULL, 0xFACEDEADCAFEBEEFULL}) {
 
-    // We need one bin per bit for all bits but the last one.
-    REQUIRE(bins.size() == CHAR_BIT * sizeof(id_t) - 1);
+        auto bins = GAMIndex::bins_of_id(to_bin);
 
-    // The bins should end with the least specific bin (0)
-    REQUIRE(bins.back() == 0);
+        // We need one bin per bit for all bits but the last one, plus one top-level 0 bin for everything.
+        REQUIRE(bins.size() == CHAR_BIT * sizeof(id_t));
+
+        // The bins should end with the least specific bin (0)
+        REQUIRE(bins.back() == 0);
+        
+        // Bin levels go from 0 to bits-1.
+        // The first bin should be 2^(bits - 1) - 1 + id >> 1
+        REQUIRE(bins.front() == ((GAMIndex::bin_t)0x1 << (CHAR_BIT * sizeof(id_t) - 1)) - 1 + ((GAMIndex::bin_t)to_bin >> 1));
+        
+        // The first bin is the most specific bin, which is the bin of the number and itself.
+        REQUIRE(bins.front() == GAMIndex::common_bin(to_bin, to_bin));
+    }
+    
+}
+
+TEST_CASE("GAMindex binning works on adjacent numbers", "[gam][gamindex]") {
+    
+    // The common bin of an even and the next odd number should be the two numbers right shifted by one, pulss an offset.
+    // For an odd and the next even, it should be them shifted by 2, pluss a smaller offset.
     
     // what offsets do we expect for bins based on their size?
     auto SIZE_2_OFFSET = ((GAMIndex::bin_t)0x1 << (CHAR_BIT * sizeof(id_t) - 1)) - 1;
     auto SIZE_4_OFFSET = ((GAMIndex::bin_t)0x1 << (CHAR_BIT * sizeof(id_t) - 2)) - 1;
-    
-    // The first bin should be 2^(bits - 1) - 1 + id >> 1
-    REQUIRE(bins.front() == ((GAMIndex::bin_t)0x1 << (CHAR_BIT * sizeof(id_t) - 1)) - 1 + ((GAMIndex::bin_t)0xACAC >> 1));
-    
-    // The common bin of an even and the next odd number should be the two numbers right shifted by one, pulss an offset.
-    // For an odd and the next even, it should be them shifted by 2, pluss a smaller offset.
     
     for (size_t i = -10; i < 10000; i++) {
         auto bin_found = GAMIndex::common_bin(i, i + 1);
