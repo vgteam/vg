fc02a9db53da16b868e063148055b2cb0cbf789b
Adam Novak <anovak@soe.ucsc.edu>
2018-10-05 14:24:54 -0700

    Disable debug prints

--- src/multipath_mapper.cpp
+++ src/multipath_mapper.cpp
@@ -4,7 +4,7 @@
 //
 //
 
-#define debug_multipath_mapper
+//#define debug_multipath_mapper
 //#define debug_multipath_mapper_alignment
 //#define debug_validate_multipath_alignments
 //#define debug_report_startup_training
