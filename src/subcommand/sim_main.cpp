38a54cd727acc5adf15ef875c4986285b6558b09
Jordan Eizenga <joeizeng@gmail.com>
2018-12-10 13:01:31 +0900

    fix opt parsing line in sim

--- src/subcommand/sim_main.cpp
+++ src/subcommand/sim_main.cpp
@@ -127,7 +127,7 @@ int main_sim(int argc, char** argv) {
         };
 
         int option_index = 0;
-        c = getopt_long (argc, argv, "hl:n:s:e:i:fax:Jp:v:Nd:F:P:S:I",
+        c = getopt_long (argc, argv, "hl:n:s:e:i:fax:Jp:v:Nd:F:P:T:S:I",
                 long_options, &option_index);
 
         // Detect the end of the options.
