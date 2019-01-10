cd3e3bc6e7f28da6549ea290b2907814cb1a7f6f
Jordan Eizenga <joeizeng@gmail.com>
2019-01-10 14:34:29 -0800

    add unittests and debug dagify

--- src/algorithms/dagify.cpp
+++ src/algorithms/dagify.cpp
@@ -1,3 +1,5 @@
+//#define debug_dagify
+
 #include "dagify.hpp"
 
 namespace vg {
@@ -9,10 +11,22 @@ using namespace std;
                                      size_t min_preserved_path_length) {
         
         // initialize the translator from the dagified graph back to the original graph
-        unordered_map<id_t, id_t> trans;
+        unordered_map<id_t, id_t> translator;
         
         // generate a canonical orientation across the graph
         vector<handle_t> orientation = single_stranded_orientation(graph);
+        
+        if (orientation.size() < graph->node_size()) {
+            cerr << "error:[dagify] Dagify algorithm only valid on graphs with a single stranded orientation, consider using split_strands first" << endl;
+            exit(1);
+        }
+        
+#ifdef debug_dagify
+        cerr << "got canonical orientation:" << endl;
+        for (const handle_t& h : orientation) {
+            cerr << "\t" << graph->get_id(h) << (graph->get_is_reverse(h) ? "-" : "+") << endl;
+        }
+#endif
         
         // mark the ones that whose canonical orientation is reversed
         unordered_set<id_t> reversed_nodes;
@@ -25,18 +39,39 @@ using namespace std;
         // find the strongly connected components of the original graph
         vector<unordered_set<id_t>> strong_components = strongly_connected_components(graph);
         
-        // create the nodes of the dagified graph
+#ifdef debug_dagify
+        cerr << "got strongly connected components:" << endl;
+        for (size_t i = 0; i < strong_components.size(); i++) {
+            cerr << "\tcomponent " << i << endl;
+            for (id_t nid : strong_components[i]) {
+                cerr << "\t\t" << nid << endl;
+            }
+        }
+#endif
+        
+        // duplicate strongly connected components into the dagified graph in such a way
+        // that paths are preserved
+        
+        // a tracker for which SCC a node belongs to
         unordered_map<id_t, size_t> component_of;
+        // a map from a node in the original graph to all its copies (in order) in the
+        // dagified graph
         unordered_map<handle_t, vector<handle_t>> injector;
         for (size_t i = 0; i < strong_components.size(); i++) {
             
-            // keep track of which nodes are in which component
+#ifdef debug_dagify
+            cerr << "handling component " << i << endl;
+#endif
+            
+            // keep track of which nodes are in which component (for later)
             auto& component = strong_components[i];
             for (id_t node_id : component) {
                 component_of[node_id] = i;
             }
             
             // figure out how many times we need to copy this SCC
+            
+            // wrap the SCC in a handle graph
             SubHandleGraph subgraph(graph);
             for (const id_t& node_id : component) {
                 subgraph.add_handle(graph->get_handle(node_id));
@@ -52,9 +87,19 @@ using namespace std;
                     layout[i] = subgraph.flip(layout[j]);
                     layout[j] = subgraph.flip(tmp);
                 }
+                if (layout.size() % 2) {
+                    layout[layout.size() / 2] = subgraph.flip(layout[layout.size() / 2]);
+                }
             }
             
-            // record the ordering of the layout
+#ifdef debug_dagify
+            cerr << "layout for component:" << endl;
+            for (const handle_t& h : layout) {
+                cerr << "\t" << graph->get_id(h) << (graph->get_is_reverse(h) ? "-" : "+") << endl;
+            }
+#endif
+            
+            // record the ordering of the layout so we can identify backward edges
             unordered_map<handle_t, size_t> ordering;
             for (size_t i = 0; i < layout.size(); i++) {
                 ordering[layout[i]] = i;
@@ -63,11 +108,9 @@ using namespace std;
             // mark the edges as either forward or backward relative to the layout
             vector<vector<size_t>> forward_edges(layout.size());
             vector<pair<size_t, size_t>> backward_edges;
-            // also collect the nodes that are the head of a backward-facing edge
-            unordered_set<handle_t> backward_edge_heads;
             subgraph.for_each_edge([&](const edge_t& edge) {
-                // get the indices of the edge in the layout, according to the canonical
-                // orientation
+                // get the indices of the edge in the layout, making sure to match
+                // the canonical orientation
                 size_t i, j;
                 auto iter = ordering.find(edge.first);
                 if (iter != ordering.end()) {
@@ -85,7 +128,6 @@ using namespace std;
                 }
                 else {
                     backward_edges.emplace_back(i, j);
-                    backward_edge_heads.insert(layout[i]);
                 }
                 
                 // always keep going
@@ -95,23 +137,36 @@ using namespace std;
             // check for each node whether we've duplicated the component enough times
             // to preserve its cycles
             
+            // dynamic progamming structures that represent distances within the current
+            // copy of the SCC and the next copy
             vector<int64_t> distances(layout.size(), numeric_limits<int64_t>::max());
             vector<int64_t> next_distances(layout.size(), numeric_limits<int64_t>::max());
             
-            for (const handle_t& handle : backward_edge_heads) {
+            // init the distances so that we are measuring from the end of the heads of
+            // backward edges (which cross to the next copy of the SCC)
+            for (const pair<size_t, size_t>& bwd_edge : backward_edges) {
+                handle_t handle = layout[bwd_edge.first];
                 distances[ordering[handle]] = -subgraph.get_length(handle);
             }
             
             // init the tracker that we use for the bail-out condition
-            int64_t min_relaxed_dist = 0;
+            int64_t min_relaxed_dist = -1;
             
             // add copies until the minimum distance to the new copy is longer than the distance we're
             // trying to preserve
-            for (size_t copy_num = 0; min_relaxed_dist <= int64_t(min_preserved_path_length); copy_num++) {
+            for (size_t copy_num = 0; min_relaxed_dist < int64_t(min_preserved_path_length); copy_num++) {
+                
+#ifdef debug_dagify
+                cerr << "copy number " << copy_num << endl;
+#endif
                 
                 // do we need a new copy of this SCC to preserve paths?
                 if (copy_num == injector[layout.front()].size()) {
                     // we haven't added this copy of the connected component yet
+                    
+#ifdef debug_dagify
+                    cerr << "adding nodes for this copy" << endl;
+#endif
                     
                     // add the nodes
                     for (const handle_t& original_handle : layout) {
@@ -121,7 +176,12 @@ using namespace std;
                         if (graph->get_is_reverse(original_handle)) {
                             new_handle = into->flip(new_handle);
                         }
-                        trans[into->get_id(new_handle)] = graph->get_id(original_handle);
+#ifdef debug_dagify
+                        cerr << "\t" << graph->get_id(original_handle) << " duplicated to " << into->get_id(new_handle) << endl;
+#endif
+                        
+                        // record the translation between the graphs
+                        translator[into->get_id(new_handle)] = graph->get_id(original_handle);
                         injector[original_handle].push_back(new_handle);
                     }
                     
@@ -130,15 +190,22 @@ using namespace std;
                         handle_t from = injector[layout[i]].back();
                         for (const size_t& j : forward_edges[i]) {
                             into->create_edge(from, injector[layout[j]].back());
+#ifdef debug_dagify
+                            cerr << "\t\tfwd edge " << into->get_id(from) << (into->get_is_reverse(from) ? "-" : "+") << " -> " << into->get_id(injector[layout[j]].back()) << (into->get_is_reverse(injector[layout[j]].back()) ? "-" : "+") << endl;
+#endif
                         }
                     }
                     
+                    // is there a previous copy?
                     if (copy_num > 0) {
                         // add the backward edges between the copies
                         for (const pair<size_t, size_t>& bwd_edge : backward_edges) {
                             const auto& from_copies = injector[layout[bwd_edge.first]];
                             into->create_edge(from_copies[from_copies.size() - 2],
                                               injector[layout[bwd_edge.second]].back());
+#ifdef debug_dagify
+                            cerr << "\t\tbwd edge " << into->get_id(from_copies[from_copies.size() - 2]) << (into->get_is_reverse(from_copies[from_copies.size() - 2]) ? "-" : "+") << " -> " << into->get_id(injector[layout[bwd_edge.second]].back()) << (into->get_is_reverse(injector[layout[bwd_edge.second]].back()) ? "-" : "+") << endl;
+#endif
                         }
                     }
                 }
@@ -146,8 +213,7 @@ using namespace std;
                 // we've finished adding the copy of the SCC that corresponds to this iteration
                 // now we will do the dynamic programming to bound the distance to the next SCC
                 
-                // find the shortest path to the nodes, staying within this copy
-                // of the SCC
+                // find the shortest path to the nodes, staying within this copy of the SCC
                 for (size_t i = 0; i < distances.size(); i++) {
                     // skip infinity to avoid overflow
                     if (distances[i] == numeric_limits<int64_t>::max()) {
@@ -163,8 +229,8 @@ using namespace std;
                 // now find the minimum distance to nodes in the next copy of the SCC (which
                 // may not yet be created in the graph)
                 min_relaxed_dist = numeric_limits<int64_t>::max();
-                vector<int64_t> next_distances(distances.size(), numeric_limits<int64_t>::max());
                 for (const pair<size_t, size_t>& bwd_edge : backward_edges) {
+                    // skip infinity to avoid overflow
                     if (distances[bwd_edge.first] == numeric_limits<int64_t>::max()) {
                         continue;
                     }
@@ -177,34 +243,54 @@ using namespace std;
                     }
                 }
                 
+#ifdef debug_dagify
+                cerr << "distances within component" << endl;
+                for (size_t i = 0; i < distances.size(); i++) {
+                    cerr << "\t" << graph->get_id(layout[i]) << (graph->get_is_reverse(layout[i]) ? "-" : "+") << " " << distances[i] << endl;
+                }
+                cerr << "distances to next component" << endl;
+                for (size_t i = 0; i < next_distances.size(); i++) {
+                    cerr << "\t" << graph->get_id(layout[i]) << (graph->get_is_reverse(layout[i]) ? "-" : "+") << " " << next_distances[i] << endl;
+                }
+#endif
+                
                 // initialize the DP structures for the next iteration
                 distances = move(next_distances);
                 next_distances.assign(distances.size(), numeric_limits<int64_t>::max());
             }
         }
         
+#ifdef debug_dagify
+        cerr << "adding edges between SCCs" << endl;
+#endif
+        
         // add edges between the strongly connected components
         graph->for_each_edge([&](const edge_t& canonical_edge) {
-            // put the edge in the order of the orientation we've imposed on the graph
-            edge_t edge = (graph->get_is_reverse(canonical_edge.first) != reversed_nodes.count(graph->get_id(canonical_edge.first)) ?
-                           edge_t(graph->flip(canonical_edge.second), graph->flip(canonical_edge.first)) :
-                           canonical_edge);
-
-            if (component_of[graph->get_id(edge.first)] != component_of[graph->get_id(edge.second)]) {
+            if (component_of[graph->get_id(canonical_edge.first)] != component_of[graph->get_id(canonical_edge.second)]) {
                 // this edge is between SCCs
+                
+                // put the edge in the order of the orientation we've imposed on the graph so
+                // we can index into the look structures we created
+                edge_t edge = (graph->get_is_reverse(canonical_edge.first) != reversed_nodes.count(graph->get_id(canonical_edge.first)) ?
+                               edge_t(graph->flip(canonical_edge.second), graph->flip(canonical_edge.first)) :
+                               canonical_edge);
                 
                 // connect the last copy of the first node to all copies of the second
                 const handle_t& from = injector[edge.first].back();
                 for (const handle_t& to : injector[edge.second]) {
                     into->create_edge(from, to);
+#ifdef debug_dagify
+                    cerr << "\t" << into->get_id(from) << (into->get_is_reverse(from) ? "-" : "+") << " -> " << into->get_id(to) << (into->get_is_reverse(to) ? "-" : "+") << endl;
+#endif
                 }
             }
+            
             // always keep going
             return true;
         });
         
         // return the ID translator
-        return trans;
+        return translator;
     }
 }
 }
--- src/algorithms/eades_algorithm.cpp
+++ src/algorithms/eades_algorithm.cpp
@@ -1,3 +1,5 @@
+//#define debug_eades
+
 #include "eades_algorithm.hpp"
 
 namespace vg {
@@ -7,6 +9,10 @@ using namespace std;
 
     vector<handle_t> eades_algorithm(const HandleGraph* graph) {
         
+#ifdef debug_eades
+        cerr << "entering Eades algorithm" << endl;
+#endif
+        
         // decide which strand will be "forward" for each node
         vector<handle_t> canonical_orientation = single_stranded_orientation(graph);
         if (canonical_orientation.size() < graph->node_size()) {
@@ -14,44 +20,54 @@ using namespace std;
             exit(1);
         }
         
+#ifdef debug_eades
+        cerr << "got canonical orientation:" << endl;
+        for (const handle_t& h : canonical_orientation) {
+            cerr << "\t" << graph->get_id(h) << (graph->get_is_reverse(h) ? "-" : "+") << endl;
+        }
+#endif
+        
         // maps handles to records of ((in-degree, out-degree), bucket position)
         unordered_map<handle_t, pair<pair<int64_t, int64_t>, list<handle_t>::iterator>> degree_info;
         
         // buckets based on delta(u) among non-source, non-sink nodes (see paper)
-        // buckets are numbered -n + 3, -n + 2, ... , n - 2, n - 3
+        // buckets are numbered -n + 2, -n + 3, ... , n - 3, n - 2
         auto assign_bucket = [&](const int64_t& in_degree, const int64_t& out_degree) {
-            return out_degree - in_degree + int64_t(canonical_orientation.size()) - 3;
+            return out_degree - in_degree + int64_t(canonical_orientation.size()) - 2;
         };
-        vector<list<handle_t>> delta_buckets(2 * canonical_orientation.size() - 5);
+        vector<list<handle_t>> delta_buckets(canonical_orientation.size() > 1 ? 2 * canonical_orientation.size() - 3 : 0);
+        
         
         vector<handle_t> sources;
         vector<handle_t> sinks;
         
         for (const handle_t& handle : canonical_orientation) {
-            
             // compute in- and out-degree
-            int64_t in_degree = 0;
-            int64_t out_degree = 0;
-            graph->follow_edges(handle, true, [&](const handle_t& prev) {
-                in_degree++;
-            });
-            graph->follow_edges(handle, false, [&](const handle_t& next) {
-                out_degree++;
-            });
+            int64_t in_degree = graph->get_degree(handle, true);
+            int64_t out_degree = graph->get_degree(handle, false);
             
             if (in_degree == 0) {
                 // source
                 sources.emplace_back(handle);
+#ifdef debug_eades
+                cerr << "assign " << graph->get_id(handle) << (graph->get_is_reverse(handle) ? "-" : "+") << " to sources" << endl;
+#endif
             }
             else if (out_degree == 0) {
                 // sink
                 sinks.emplace_back(handle);
+#ifdef debug_eades
+                cerr << "assign " << graph->get_id(handle) << (graph->get_is_reverse(handle) ? "-" : "+") << " to sinks" << endl;
+#endif
             }
             else {
                 // non-source, non-sink
                 auto& bucket = delta_buckets[assign_bucket(in_degree, out_degree)];
                 bucket.emplace_front(handle);
                 degree_info[handle] = make_pair(make_pair(in_degree, out_degree), bucket.begin());
+#ifdef debug_eades
+                cerr << "assign " << graph->get_id(handle) << (graph->get_is_reverse(handle) ? "-" : "+") << " to delta bucket " << assign_bucket(in_degree, out_degree) << endl;
+#endif
             }
         }
         
@@ -134,9 +150,42 @@ using namespace std;
         
         while (next_left_idx <= next_right_idx) {
             
+#ifdef debug_eades
+            cerr << "status of layout:" << endl;
+            for (size_t i = 0; i < layout.size(); i++) {
+                if (i >= next_left_idx || i <= next_right_idx) {
+                    cerr << "\t." << endl;
+                }
+                else {
+                    cerr << "\t" << graph->get_id(layout[i]) << (graph->get_is_reverse(layout[i]) ? "-" : "+") << endl;
+                }
+            }
+            cerr << "status of sources:" << endl;
+            for (handle_t h : sources) {
+                cerr << "\t" << graph->get_id(h) << (graph->get_is_reverse(h) ? "-" : "+") << endl;
+            }
+            cerr << "status of sinks:" << endl;
+            for (handle_t h : sinks) {
+                cerr << "\t" << graph->get_id(h) << (graph->get_is_reverse(h) ? "-" : "+") << endl;
+            }
+            cerr << "status of delta buckets:" << endl;
+            for (int64_t i = -layout.size() + 3; i < int(layout.size()) - 3; i++) {
+                cerr << "\t" << i << " ";
+                for (handle_t h : delta_buckets[i + layout.size() - 3]) {
+                    cerr << "\t" << graph->get_id(h) << (graph->get_is_reverse(h) ? "-" : "+") << endl;
+                }
+                cerr << endl;
+            }
+#endif
+            
             if (!sources.empty()) {
                 // add a source to the layout
                 handle_t source = sources.back();
+
+#ifdef debug_eades
+                cerr << "adding next source node " << graph->get_id(source) << (graph->get_is_reverse(source) ? "-" : "+") << endl;
+#endif
+
                 sources.pop_back();
                 layout[next_left_idx] = source;
                 next_left_idx++;
@@ -147,6 +196,11 @@ using namespace std;
             else if (!sinks.empty()) {
                 // add a sink to the layout
                 handle_t sink = sinks.back();
+                
+#ifdef debug_eades
+                cerr << "adding next sink node " << graph->get_id(sink) << (graph->get_is_reverse(sink) ? "-" : "+") << endl;
+#endif
+                
                 sinks.pop_back();
                 layout[next_right_idx] = sink;
                 next_right_idx--;
@@ -158,6 +212,11 @@ using namespace std;
                 // remove a node in the highest delta bucket from the graph
                 auto& bucket = delta_buckets[max_delta_bucket];
                 handle_t next = bucket.back();
+                
+#ifdef debug_eades
+                cerr << "adding node " << graph->get_id(next) << (graph->get_is_reverse(next) ? "-" : "+") << " from delta bucket " << max_delta_bucket << endl;
+#endif
+                
                 bucket.pop_back();
                 degree_info.erase(next);
                 
