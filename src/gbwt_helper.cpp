0edeefa6e6f63f5d5bba04686dc5a5ead02bb7a2
Jouni Siren <jouni.siren@iki.fi>
2019-02-25 13:42:07 -0800

    GBWT-backed HandleGraph

--- src/gbwt_helper.cpp
+++ src/gbwt_helper.cpp
@@ -4,6 +4,213 @@
 
 namespace vg {
 
+//------------------------------------------------------------------------------
+
+GBWTGraph::GBWTGraph(const gbwt::GBWT& gbwt_index, const HandleGraph& sequence_source) :
+    index(gbwt_index), total_nodes(0) {
+
+    // Sanity checks for the GBWT index.
+    assert(this->index.bidirectional());
+
+    // Determine the number of real nodes and the total length of the sequences.
+    // Node n is real, if real_nodes[node_offset(n) / 2] is true.
+    size_t total_length = 0, potential_nodes = this->index.sigma() - this->index.firstNode();
+    this->real_nodes = std::vector<bool>(potential_nodes / 2, false);
+    for (gbwt::node_type node = this->index.firstNode(); node < this->index.sigma(); node += 2) {
+        if (this->index.empty(node)) {
+            continue;
+        }
+        this->real_nodes[this->node_offset(node) / 2] = true;
+        id_t id = gbwt::Node::id(node);
+        assert(sequence_source.has_node(id));
+        this->total_nodes++;
+        handle_t source_handle = sequence_source.get_handle(id, false);
+        total_length += sequence_source.get_length(source_handle);
+    }
+    this->sequences.reserve(2 * total_length);
+    this->offsets = sdsl::int_vector<0>(potential_nodes + 1, 0, gbwt::bit_length(this->sequences.capacity()));
+
+    // Store the concatenated sequences and their offset ranges for both orientations of all nodes.
+    // Given GBWT node n, the sequence is sequences[node_offset(n)] to sequences[node_offset(n + 1) - 1].
+    for (gbwt::node_type node = this->index.firstNode(); node < this->index.sigma(); node += 2) {
+        std::string seq;
+        if (this->is_real(node)) {
+            id_t id = gbwt::Node::id(node);
+            handle_t source_handle = sequence_source.get_handle(id, false);
+            seq = sequence_source.get_sequence(source_handle);
+        }
+        size_t offset = this->node_offset(node);
+        this->sequences.insert(this->sequences.end(), seq.begin(), seq.end());
+        this->offsets[offset + 1] = this->sequences.size();
+        seq = reverse_complement(seq);
+        this->sequences.insert(this->sequences.end(), seq.begin(), seq.end());
+        this->offsets[offset + 2] = this->sequences.size();
+    }
+}
+
+GBWTGraph::GBWTGraph(const GBWTGraph& source) :
+    index(source.index) {
+    this->sequences = source.sequences;
+    this->offsets = source.offsets;
+    this->real_nodes = source.real_nodes;
+    this->total_nodes = source.total_nodes;
+}
+
+GBWTGraph::GBWTGraph(GBWTGraph&& source) :
+    index(source.index) {
+    this->sequences = std::move(source.sequences);
+    this->offsets = std::move(source.offsets);
+    this->real_nodes = std::move(source.real_nodes);
+    this->total_nodes = std::move(total_nodes);
+}
+
+//------------------------------------------------------------------------------
+
+bool GBWTGraph::has_node(id_t node_id) const {
+    gbwt::node_type node = gbwt::Node::encode(node_id, false);
+    return (node != gbwt::ENDMARKER && this->index.contains(node) && this->is_real(node));
+}
+
+handle_t GBWTGraph::get_handle(const id_t& node_id, bool is_reverse) const {
+    return as_handle(gbwt::Node::encode(node_id, is_reverse));
+}
+
+id_t GBWTGraph::get_id(const handle_t& handle) const {
+    return gbwt::Node::id(as_integer(handle));
+}
+
+bool GBWTGraph::get_is_reverse(const handle_t& handle) const {
+    return gbwt::Node::is_reverse(as_integer(handle));
+}
+
+handle_t GBWTGraph::flip(const handle_t& handle) const {
+    return as_handle(gbwt::Node::reverse(as_integer(handle)));
+}
+
+size_t GBWTGraph::get_length(const handle_t& handle) const {
+    size_t offset = this->node_offset(handle);
+    return this->offsets[offset + 1] - this->offsets[offset];
+}
+
+std::string GBWTGraph::get_sequence(const handle_t& handle) const {
+    size_t offset = this->node_offset(handle);
+    return std::string(this->sequences.begin() + this->offsets[offset], this->sequences.begin() + this->offsets[offset + 1]);
+}
+
+// Using undocumented parts of the GBWT interface. --Jouni
+bool GBWTGraph::follow_edges(const handle_t& handle, bool go_left, const std::function<bool(const handle_t&)>& iteratee) const {
+
+    // Incoming edges correspond to the outgoing edges of the reverse node.
+    gbwt::node_type curr = as_integer(handle);
+    if (go_left) {
+        curr = gbwt::Node::reverse(curr);
+    }
+
+    gbwt::CompressedRecord record = this->index.record(curr);
+    for (gbwt::rank_type outrank = 0; outrank < record.outdegree(); outrank++) {
+        gbwt::node_type next = record.successor(outrank);
+        if (next == gbwt::ENDMARKER) {
+            continue;
+        }
+        // If we started from the reverse node, we must reverse the successor nodes to get
+        // the predecessor nodes of the original node.
+        if (go_left) {
+            next = gbwt::Node::reverse(next);
+        }
+        if (!iteratee(as_handle(next))) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void GBWTGraph::for_each_handle(const std::function<bool(const handle_t&)>& iteratee, bool parallel) const {
+    if (parallel) {
+#pragma omp parallel for schedule(dynamic)
+        for (gbwt::node_type node = this->index.firstNode(); node < this->index.sigma(); node += 2) {
+            if (!(this->is_real(node))) {
+                continue;
+            }
+            if (!iteratee(as_handle(node))) {
+                // We should stop early but it's not worth the effort.
+            }
+        }
+    } else {
+        for (gbwt::node_type node = this->index.firstNode(); node < this->index.sigma(); node += 2) {
+            if (!(this->is_real(node))) {
+                continue;
+            }
+            if (!iteratee(as_handle(node))) {
+                return;
+            }
+        }
+    }
+}
+
+size_t GBWTGraph::node_size() const {
+    return total_nodes;
+}
+
+id_t GBWTGraph::min_node_id() const {
+    return gbwt::Node::id(this->index.firstNode());
+}
+
+id_t GBWTGraph::max_node_id() const {
+    id_t next_id = gbwt::Node::id(this->index.sigma());
+    return next_id - 1;
+}
+
+//------------------------------------------------------------------------------
+
+// Using undocumented parts of the GBWT interface. --Jouni
+bool GBWTGraph::follow_edges(gbwt::SearchState state, const std::function<bool(const gbwt::SearchState&)>& iteratee) const {
+    gbwt::CompressedRecord record = this->index.record(state.node);
+    for (gbwt::rank_type outrank = 0; outrank < record.outdegree(); outrank++) {
+        gbwt::node_type next_node = record.successor(outrank);
+        if (next_node == gbwt::ENDMARKER) {
+            continue;
+        }
+        gbwt::SearchState next_state(next_node, record.LF(state.range, next_node));
+        if (!iteratee(next_state)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Using undocumented parts of the GBWT interface. --Jouni
+bool GBWTGraph::follow_edges(gbwt::BidirectionalState state, bool backward, const std::function<bool(const gbwt::BidirectionalState&)>& iteratee) const {
+    if (backward) {
+        state.flip();
+    }
+
+    gbwt::CompressedRecord record = this->index.record(state.forward.node);
+    for (gbwt::rank_type outrank = 0; outrank < record.outdegree(); outrank++) {
+        gbwt::node_type next_node = record.successor(outrank);
+        if (next_node == gbwt::ENDMARKER) {
+            continue;
+        }
+        gbwt::size_type reverse_offset = 0;
+        gbwt::BidirectionalState next_state = state;
+        next_state.forward.node = next_node;
+        next_state.forward.range = record.bdLF(state.forward.range, next_node, reverse_offset);
+        next_state.backward.range.first += reverse_offset;
+        next_state.backward.range.second = next_state.backward.range.first + next_state.forward.size() - 1;
+        if (backward) {
+            next_state.flip();
+        }
+        if (!iteratee(next_state)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+//------------------------------------------------------------------------------
+
 // Stores haplotype-consistent traversal of the graph and the corresponding sequence.
 struct GBWTTraversal {
     // The traversal as a sequence of (begin, length) pairs.
@@ -197,5 +404,7 @@ gbwt::GBWT get_gbwt(const std::vector<gbwt::vector_type>& paths) {
 
     return gbwt_index;
 }
+
+//------------------------------------------------------------------------------
 
 } // namespace vg
--- src/gbwt_helper.hpp
+++ src/gbwt_helper.hpp
@@ -2,16 +2,21 @@
 #define VG_GBWT_HELPER_HPP_INCLUDED
 
 /** \file 
- * Utility functions for working with GBWT.
+ * Utility classes and functions for working with GBWT.
  */
+
+#include <vector>
 
 #include "handle.hpp"
 #include "position.hpp"
 #include "xg.hpp"
 
 #include <gbwt/dynamic_gbwt.h>
+#include <sdsl/int_vector.hpp>
 
 namespace vg {
+
+//------------------------------------------------------------------------------
 
 /// Convert gbwt::node_type to handle_t.
 inline handle_t gbwt_to_handle(const HandleGraph& graph, gbwt::node_type node) {
@@ -48,6 +53,120 @@ inline gbwt::node_type xg_path_to_gbwt(const xg::XGPath& path, size_t i) {
     return gbwt::Node::encode(path.node(i), path.is_reverse(i));
 }
 
+//------------------------------------------------------------------------------
+
+/**
+ * A runtime-only HandleGraph implementation that uses GBWT for graph topology and
+ * extracts sequences from another HandleGraph. Faster sequence access but slower
+ * graph navigation than in XG. Also supports a version of follow_edges() that
+ * takes only paths supported by the indexed haplotypes.
+ */
+class GBWTGraph : public HandleGraph {
+public:
+    /// Create a graph backed by the GBWT index and extract the sequences from the
+    /// given HandleGraph.
+    GBWTGraph(const gbwt::GBWT& gbwt_index, const HandleGraph& sequence_source);
+
+    /// Copy constructor.
+    GBWTGraph(const GBWTGraph& source);
+
+    /// Move constructor.
+    GBWTGraph(GBWTGraph&& source);
+
+    const gbwt::GBWT&   index;
+    std::vector<char>   sequences;
+    sdsl::int_vector<0> offsets;
+    std::vector<bool>   real_nodes;
+    size_t              total_nodes;
+
+//------------------------------------------------------------------------------
+
+    // Standard HandleGraph interface.
+
+    // Method to check if a node exists by ID
+    virtual bool has_node(id_t node_id) const;
+
+    /// Look up the handle for the node with the given ID in the given orientation.
+    virtual handle_t get_handle(const id_t& node_id, bool is_reverse = false) const;
+
+    // Copy over the visit version which would otherwise be shadowed.
+    using HandleGraph::get_handle;
+
+    /// Get the ID from a handle.
+    virtual id_t get_id(const handle_t& handle) const;
+
+    /// Get the orientation of a handle.
+    virtual bool get_is_reverse(const handle_t& handle) const;
+
+    /// Invert the orientation of a handle (potentially without getting its ID).
+    virtual handle_t flip(const handle_t& handle) const;
+
+    /// Get the length of a node.
+    virtual size_t get_length(const handle_t& handle) const;
+
+    /// Get the sequence of a node, presented in the handle's local forward
+    /// orientation.
+    virtual std::string get_sequence(const handle_t& handle) const;
+
+    /// Loop over all the handles to next/previous (right/left) nodes. Passes
+    /// them to a callback which returns false to stop iterating and true to
+    /// continue. Returns false if we stopped early.
+    virtual bool follow_edges(const handle_t& handle, bool go_left, const std::function<bool(const handle_t&)>& iteratee) const;
+
+    // Copy over the template version.
+    using HandleGraph::follow_edges;
+
+    /// Loop over all the nodes in the graph in their local forward
+    /// orientations, in their internal stored order. Stop if the iteratee returns false.
+    virtual void for_each_handle(const std::function<bool(const handle_t&)>& iteratee, bool parallel = false) const;
+
+    // Copy over the template version.
+    using HandleGraph::for_each_handle;
+
+    /// Return the number of nodes in the graph.
+    virtual size_t node_size() const;
+
+    /// Get the minimum node ID used in the graph, if any are used.
+    virtual id_t min_node_id() const;
+
+    /// Get the maximum node ID used in the graph, if any are used.
+    virtual id_t max_node_id() const;
+
+//------------------------------------------------------------------------------
+
+    /// Convert handle_t to gbwt::SearchState.
+    gbwt::SearchState get_state(const handle_t& handle) const { return this->index.find(as_integer(handle)); }
+
+    /// Convert handle_t to gbwt::BidirectionalState.
+    gbwt::BidirectionalState get_bd_state(const handle_t& handle) const { return this->index.bdFind(as_integer(handle)); }
+
+    /// Convert gbwt::node_type to handle_t.
+    static handle_t node_to_handle(gbwt::node_type node) { return as_handle(node); }
+
+    /// Convert handle_t to gbwt::node_type.
+    static gbwt::node_type handle_to_node(const handle_t& handle) { return as_integer(handle); }
+
+    /// Visit all successor states of this state and call iteratee for the state.
+    /// Stop and return false if the iteratee returns false.
+    /// Note that the state may be empty if no path continues to that node.
+    bool follow_edges(gbwt::SearchState state, const std::function<bool(const gbwt::SearchState&)>& iteratee) const;
+
+    /// Visit all predecessor/successor states of this state and call iteratee for the state.
+    /// Stop and return false if the iteratee returns false.
+    /// Note that the state may be empty if no path continues to that node.
+    /// Each state corresponds to a path. Going backward extends the path left, while going
+    /// extends it right.
+    bool follow_edges(gbwt::BidirectionalState state, bool backward,
+                      const std::function<bool(const gbwt::BidirectionalState&)>& iteratee) const;
+
+private:
+    size_t node_offset(gbwt::node_type node) const { return node - this->index.firstNode(); }
+    size_t node_offset(const handle_t& handle) const { return this->node_offset(as_integer(handle)); }
+    bool is_real(gbwt::node_type node) const { return this->real_nodes[this->node_offset(node) / 2]; }
+};
+
+//------------------------------------------------------------------------------
+
 /// Traverse all haplotype-consistent kmers in the graph and call lambda() for each kmer.
 /// Uses multiple threads, so the lambda should be thread-safe.
 void for_each_kmer(const HandleGraph& graph, const gbwt::GBWT& haplotypes, size_t k,
@@ -70,6 +189,8 @@ void for_each_window(const HandleGraph& graph, size_t window_size,
 
 /// Transform the paths into a GBWT index. Primarily for testing.
 gbwt::GBWT get_gbwt(const std::vector<gbwt::vector_type>& paths);
+
+//------------------------------------------------------------------------------
 
 } // namespace vg
 
