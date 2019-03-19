4fcbdbd8e7771226127f5a6c924925fcd06807ff
Adam Novak <anovak@soe.ucsc.edu>
2019-03-19 15:16:03 -0700

    Hook up the extender to the GAFFE driver

--- src/subcommand/gaffe_main.cpp
+++ src/subcommand/gaffe_main.cpp
@@ -20,6 +20,7 @@
 #include "../stream/vpkg.hpp"
 #include "../stream/stream.hpp"
 #include "../alignment_emitter.hpp"
+#include "../gapless_extender.hpp"
 
 using namespace std;
 using namespace vg;
@@ -32,6 +33,7 @@ void help_gaffe(char** argv) {
     << endl
     << "basic options:" << endl
     << "  -x, --xg-name FILE            use this xg index (required)" << endl
+    << "  -H, --gbwt-name FILE          use this GBWT index (required)" << endl
     << "  -m, --minimizer-name FILE     use this minimizer index (required)" << endl
     << "  -s, --snarls FILE             cluster using these snarls (required)" << endl
     << "  -d, --dist-name FILE          cluster using this distance index (required)" << endl
@@ -56,7 +58,7 @@ int main_gaffe(int argc, char** argv) {
 
     // initialize parameters with their default options
     string xg_name;
-    string gcsa_name;
+    string gbwt_name;
     string minimizer_name;
     string snarls_name;
     string distance_name;
@@ -82,6 +84,7 @@ int main_gaffe(int argc, char** argv) {
         {
             {"help", no_argument, 0, 'h'},
             {"xg-name", required_argument, 0, 'x'},
+            {"gbwt-name", required_argument, 0, 'H'},
             {"minimizer-name", required_argument, 0, 'm'},
             {"snarls", required_argument, 0, 's'},
             {"dist-name", required_argument, 0, 'd'},
@@ -96,7 +99,7 @@ int main_gaffe(int argc, char** argv) {
         };
 
         int option_index = 0;
-        c = getopt_long (argc, argv, "hx:m:s:d:c:G:f:M:t:",
+        c = getopt_long (argc, argv, "hx:H:m:s:d:c:G:f:M:t:",
                          long_options, &option_index);
 
 
@@ -110,6 +113,14 @@ int main_gaffe(int argc, char** argv) {
                 xg_name = optarg;
                 if (xg_name.empty()) {
                     cerr << "error:[vg gaffe] Must provide XG file with -x." << endl;
+                    exit(1);
+                }
+                break;
+                
+            case 'H':
+                gbwt_name = optarg;
+                if (gbwt_name.empty()) {
+                    cerr << "error:[vg gaffe] Must provide GBWT file with -H." << endl;
                     exit(1);
                 }
                 break;
@@ -188,6 +199,11 @@ int main_gaffe(int argc, char** argv) {
         exit(1);
     }
     
+    if (gbwt_name.empty()) {
+        cerr << "error:[vg gaffe] Mapping requires a GBWT index (-H)" << endl;
+        exit(1);
+    }
+    
     if (minimizer_name.empty()) {
         cerr << "error:[vg gaffe] Mapping requires a minimizer index (-m)" << endl;
         exit(1);
@@ -205,6 +221,7 @@ int main_gaffe(int argc, char** argv) {
     
     // create in-memory objects
     unique_ptr<xg::XG> xg_index = stream::VPKG::load_one<xg::XG>(xg_name);
+    unique_ptr<gbwt::GBWT> gbwt_index = stream::VPKG::load_one<gbwt::GBWT>(gbwt_name);
     unique_ptr<MinimizerIndex> minimizer_index = stream::VPKG::load_one<MinimizerIndex>(minimizer_name);
     unique_ptr<SnarlManager> snarl_manager = stream::VPKG::load_one<SnarlManager>(snarls_name);
     unique_ptr<DistanceIndex> distance_index = stream::VPKG::load_one<DistanceIndex>(distance_name);
@@ -212,6 +229,12 @@ int main_gaffe(int argc, char** argv) {
     // Connect the DistanceIndex to the other things it needs to work.
     distance_index->setGraph(xg_index.get());
     distance_index->setSnarlManager(snarl_manager.get());
+    
+    // Make a GBWTGraph over the GBWT and the XG
+    GBWTGraph gbwt_graph(*gbwt_index, *xg_index);
+    
+    // Make a gapless extender to extend seed hits in haplotype space.
+    GaplessExtender extender(gbwt_graph);
     
     // Make the clusterer
     SnarlSeedClusterer clusterer;
@@ -305,19 +328,73 @@ int main_gaffe(int argc, char** argv) {
             aln.set_read_group(read_group);
         }
         
-        for (size_t i = 0; i < min(cluster_indexes_in_order.size(), max_multimaps); i++) {
-            // For each cluster, produce an Alignment
+        // How many multimaps will we produce?
+        size_t actual_multimaps = min(cluster_indexes_in_order.size(), max_multimaps);
+        
+        for (size_t i = 0; i < actual_multimaps; i++) {
+            // For each cluster
+            hash_set<size_t>& cluster = clusters[i];
+            
+            // Pack the seeds into (read position, graph position) pairs.
+            vector<pair<size_t, pos_t>> seed_matchings;
+            seed_matchings.reserve(cluster.size());
+            for (auto& seed_index : cluster) {
+                // For each seed in the cluster, generate its matching pair
+                seed_matchings.emplace_back(minimizers[seed_to_source[seed_index]].second, seeds[seed_index]);
+            }
+            
+            // Extend seed hits in the cluster into a real alignment path and mismatch count.
+            std::pair<Path, size_t> extended = extender.extend_seeds(seed_matchings, aln.sequence());
+            auto& path = extended.first;
+            auto& mismatch_count = extended.second;
+            
+            // Produce an output Alignment
             aligned.emplace_back(aln);
             Alignment& out = aligned.back();
             
-            // TODO: Call Jouni's extender here
-            out.set_score(0);
-            out.clear_path();
-            out.clear_refpos();
+            if (path.mapping_size() != 0) {
+                // We have a mapping
+                
+                // Compute a score based on the sequence length and mismatch count.
+                // Alignments will only contain matches and mismatches.
+                int alignment_score = default_match * (aln.sequence().size() - mismatch_count) - default_mismatch * extended.second;
+                // Compute identity from mismatch count.
+                double identity = aln.sequence().size() == 0 ? 0.0 : (aln.sequence().size() - mismatch_count) / (double) aln.sequence().size();
+                
+                // Fill in the extension info
+                *out.mutable_path() = path;
+                out.set_score(alignment_score);
+                out.set_identity(identity);
+                
+            } else {
+                // Read was not able to be mapped; mismatch_count is not true.
+                // Make an un-aligned alignment.
+                
+                out.clear_path();
+                out.set_score(0);
+                out.set_identity(0);
+            }
             
-            out.set_is_secondary(i > 0);
+            // Set other fields
+            out.clear_refpos();
         }
         
+        // Sort again by actual score instead of cluster coverage
+        std::sort(aligned.begin(), aligned.end(), [](const Alignment& a, const Alignment& b) -> bool {
+            // Return true if a must come before b (i.e. it has a larger score)
+            return a.score() > b.score();
+        });
+        
+        for (size_t i = 0; i < aligned.size(); i++) {
+            // For each output alignment in score order
+            auto& out = aligned[i];
+            
+            // Assign primary and secondary status
+            out.set_is_secondary(i > 0);
+            out.set_mapping_quality(0);
+        }
+        
+        // Ship out all the aligned alignments
         alignment_emitter->emit_mapped_single(std::move(aligned));
     };
     
