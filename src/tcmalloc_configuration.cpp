04eafe48e05ffd0cf4c5bf9b1a2f585985f8a03a
Adam Novak <anovak@soe.ucsc.edu>
2019-06-12 15:16:58 -0700

    Slightly shrink tcmalloc cache size in hopes of slightly betetr performance

--- src/tcmalloc_configuration.cpp
+++ src/tcmalloc_configuration.cpp
@@ -25,9 +25,9 @@ public:
         // set a higher value for tcmalloc warnings
         setenv("TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD", "1000000000000000", 1);
         
-        // Allow 16 MB of memory per core (i.e. per default OMP thread) for tcmalloc thread caches.
+        // Allow 8 MB of memory per core (i.e. per default OMP thread) for tcmalloc thread caches.
         // The default is 16 MB total, which is way too small.
-        size_t tcmalloc_thread_cache_bytes = get_thread_count() * 16 * 1024 * 1024;
+        size_t tcmalloc_thread_cache_bytes = get_thread_count() * 8 * 1024 * 1024;
         // Ask tcmalloc to up our thread cache size.
         // Don't stop if this fails; it doesn't work under callgrind for example.
         MallocExtension::instance()->SetNumericProperty("tcmalloc.max_total_thread_cache_bytes", tcmalloc_thread_cache_bytes);
