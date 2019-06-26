7bcaf96f58be632075b1e0fe47afa00cfddd21d9
Adam Novak <anovak@soe.ucsc.edu>
2019-06-26 16:35:47 -0700

    Start on a tree-based subgraph to represent haplotype embeddings of tails

---
+++ src/tree_subgraph.cpp
@@ -0,0 +1,18 @@
+/**
+ * \file tree_subgraph.cpp
+ * Contains the implementation of the TreeSubgraph.
+ */
+
+
+#include "path_subgraph.hpp"
+#include "path.hpp"
+#include <handlegraph/util.hpp>
+#include <iostream>
+
+namespace vg {
+
+using namespace std;
+
+
+}
+
