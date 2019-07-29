1affff6da4d63abab0cb606878cc1458b83a7572
Adam Novak <anovak@soe.ucsc.edu>
2019-07-29 16:33:14 -0700

    Rip out slower code paths and settle on xdrop without connectivity

--- scripts/giraffe-wrangler.sh
+++ scripts/giraffe-wrangler.sh
@@ -45,7 +45,7 @@ REAL_FASTQ="${1}"
 shift
 
 # Define the Giraffe parameters
-GIRAFFE_OPTS=(-s75 -u 0.1 -v 25 -w 5 -C 600 --skip-connectivity)
+GIRAFFE_OPTS=(-s75 -u 0.1 -v 25 -w 5 -C 600)
 # And the thread count for everyone
 THREAD_COUNT=32
 
