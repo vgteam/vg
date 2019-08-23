c0559e79f601e13162adf675ad40253a8760c8f8
Jouni Siren <jouni.siren@iki.fi>
2019-08-23 15:28:39 -0700

    Build minimizer index from any HandleGraph; require GBWT

--- src/subcommand/minimizer_main.cpp
+++ src/subcommand/minimizer_main.cpp
@@ -11,12 +11,12 @@
  * --max-occs can be used to specify the maximum number of occurrences for
  * a kmer. Kmers more frequent than that will be removed from the index.
  *
- * The index contains either all minimizers or haplotype-consistent minimizers
- * (option --gbwt-name). Indexing all minimizers from complex graph regions
- * can take a long time (e.g. 65 hours vs 30 minutes for 1000GP), because many
- * windows have the same minimizer. As the total number of minimizers is
- * manageable (e.g. 2.1 billion vs. 1.4 billion for 1000GP), it should be
- * possible to develop a better algorithm for finding the minimizers.
+ * The index contains either all haplotype-consistent minimizers. Indexing al
+ *  minimizers from complex graph regions can take a long time (e.g. 65 hours
+ * vs 30 minutes for 1000GP), because many windows have the same minimizer.
+ * As the total number of minimizers is manageable (e.g. 2.1 billion vs.
+ * 1.4 billion for 1000GP), it should be possible to develop a better
+ * algorithm for finding the minimizers.
  *
  * A quick idea:
  * - For each node v, extract the subgraph for the windows starting in v.
@@ -30,7 +30,6 @@
 #include "subcommand.hpp"
 
 #include <vg/io/vpkg.hpp>
-#include <vg/io/stream.hpp>
 
 #include <iostream>
 #include <vector>
@@ -43,21 +42,22 @@ using namespace vg::subcommand;
 
 
 void help_minimizer(char** argv) {
-    std::cerr << "usage: " << argv[0] << " minimizer [options] graph.xg" << std::endl;
+    std::cerr << "usage: " << argv[0] << " minimizer [options] graph" << std::endl;
     std::cerr << "Builds a minimizer index of the graph in the XG index." << std::endl;
     std::cerr << "    -k, --kmer-length N    length of the kmers in the index (default: " << MinimizerIndex::KMER_LENGTH << ")" << std::endl;
     std::cerr << "    -w, --window-length N  index the smallest kmer in a window of N kmers (default: " << MinimizerIndex::WINDOW_LENGTH << ")" << std::endl;
     std::cerr << "    -i, --index-name X     store the index to file X (required)" << std::endl;
     std::cerr << "    -l, --load-index X     load the index from file X and insert the new kmers into it" << std::endl;
     std::cerr << "                           (overrides --kmer-length, --window-length, and --max-occs)" << std::endl;
-    std::cerr << "    -g, --gbwt-name X      index only haplotype-consistent kmers using the GBWT index in file X" << std::endl;
+    std::cerr << "    -g, --gbwt-name X      use the GBWT index in file X (required)" << std::endl;
+    std::cerr << "    -G, --gbwt-graph X     the input graph is a GBWTGraph" << std::endl;
     std::cerr << "    -p, --progress         show progress information" << std::endl;
     std::cerr << "    -t, --threads N        use N threads for index construction (default: " << omp_get_max_threads() << ")" << std::endl;
 }
 
 int main_minimizer(int argc, char** argv) {
 
-    if (argc == 2) {
+    if (argc <= 2) {
         help_minimizer(argv);
         return 1;
     }
@@ -65,7 +65,8 @@ int main_minimizer(int argc, char** argv) {
     // Command-line options.
     size_t kmer_length = MinimizerIndex::KMER_LENGTH;
     size_t window_length = MinimizerIndex::WINDOW_LENGTH;
-    std::string index_name, load_index, gbwt_name, xg_name;
+    std::string index_name, load_index, gbwt_name, graph_name;
+    bool is_gbwt_graph = false;
     bool progress = false;
     int threads = omp_get_max_threads();
 
@@ -79,13 +80,14 @@ int main_minimizer(int argc, char** argv) {
             { "index-name", required_argument, 0, 'i' },
             { "load-index", required_argument, 0, 'l' },
             { "gbwt-name", required_argument, 0, 'g' },
+            { "gbwt-graph", no_argument, 0, 'G' },
             { "progress", no_argument, 0, 'p' },
             { "threads", required_argument, 0, 't' },
             { 0, 0, 0, 0 }
         };
 
         int option_index = 0;
-        c = getopt_long(argc, argv, "k:w:i:l:g:pt:h", long_options, &option_index);
+        c = getopt_long(argc, argv, "k:w:i:l:g:Gpt:h", long_options, &option_index);
         if (c == -1) { break; } // End of options.
 
         switch (c)
@@ -105,6 +107,9 @@ int main_minimizer(int argc, char** argv) {
         case 'g':
             gbwt_name = optarg;
             break;
+        case 'G':
+            is_gbwt_graph = true;
+            break;
         case 'p':
             progress = true;
             break;
@@ -123,47 +128,50 @@ int main_minimizer(int argc, char** argv) {
             std::abort();
         }
     }
-    if (index_name.empty()) {
-        std::cerr << "[vg minimizer]: option --index-name is required" << std::endl;
+    if (index_name.empty() || gbwt_name.empty()) {
+        std::cerr << "[vg minimizer]: options --index-name and --gbwt-name are required" << std::endl;
         return 1;
     }
     if (optind + 1 != argc) {
         help_minimizer(argv);
         return 1;
     }
-    xg_name = argv[optind];
+    graph_name = argv[optind];
 
     double start = gbwt::readTimer();
 
-    // Input graph.
+    // GBWT index.
     if (progress) {
-        std::cerr << "Loading XG index " << xg_name << std::endl;
+        std::cerr << "Loading GBWT index " << gbwt_name << std::endl;
     }
-    std::unique_ptr<PathPositionHandleGraph> xg_index;
-    xg_index = vg::io::VPKG::load_one<PathPositionHandleGraph>(xg_name);
+    std::unique_ptr<gbwt::GBWT> gbwt_index(vg::io::VPKG::load_one<gbwt::GBWT>(gbwt_name));
+
+    // GBWTGraph.
+    std::unique_ptr<GBWTGraph> gbwt_graph;
+    if (is_gbwt_graph) {
+        if (progress) {
+            std::cerr << "Loading GBWTGraph " << graph_name << std::endl;
+        }
+        gbwt_graph = vg::io::VPKG::load_one<GBWTGraph>(graph_name);
+        gbwt_graph->set_gbwt(*gbwt_index);
+    } else {
+        if (progress) {
+            std::cerr << "Loading input graph " << graph_name << std::endl;
+        }
+        std::unique_ptr<HandleGraph> input_graph(vg::io::VPKG::load_one<HandleGraph>(graph_name));
+        if (progress) {
+            std::cerr << "Building GBWTGraph" << std::endl;
+        }
+        gbwt_graph.reset(new GBWTGraph(*gbwt_index, *input_graph));
+    }
 
     // Minimizer index.
     std::unique_ptr<MinimizerIndex> index(new MinimizerIndex(kmer_length, window_length));
     if (!load_index.empty()) {
         if (progress) {
-            std::cerr << "Loading minimizer index " << load_index << std::endl;
+            std::cerr << "Loading MinimizerIndex " << load_index << std::endl;
         }
         index = vg::io::VPKG::load_one<MinimizerIndex>(load_index);
-    }
-
-    // GBWT-backed graph.
-    std::unique_ptr<gbwt::GBWT> gbwt_index;
-    std::unique_ptr<GBWTGraph> gbwt_graph;
-    if (!gbwt_name.empty()) {
-        if (progress) {
-            std::cerr << "Loading GBWT index " << gbwt_name << std::endl;
-        }
-        gbwt_index = vg::io::VPKG::load_one<gbwt::GBWT>(gbwt_name);
-        if (progress) {
-            std::cerr << "Building GBWT-backed graph" << std::endl;
-        }
-        gbwt_graph.reset(new GBWTGraph(*gbwt_index, *xg_index));
-        xg_index.reset(nullptr); // The XG index is no longer needed.
     }
 
     // Minimizer caching.
@@ -182,11 +190,8 @@ int main_minimizer(int argc, char** argv) {
 
     // Build the index.
     if (progress) {
-        std::cerr << "Building the index" << std::endl;
+        std::cerr << "Building MinimizerIndex" << std::endl;
     }
-    const HandleGraph& graph = *(gbwt_name.empty() ?
-                                 static_cast<const HandleGraph*>(xg_index.get()) :
-                                 static_cast<const HandleGraph*>(gbwt_graph.get()));
     auto lambda = [&](const std::vector<handle_t>& traversal, const std::string& seq) {
         std::vector<MinimizerIndex::minimizer_type> minimizers = index->minimizers(seq);
         auto iter = traversal.begin();
@@ -197,13 +202,13 @@ int main_minimizer(int argc, char** argv) {
                 continue;
             }
             // Find the node covering minimizer starting position.
-            size_t node_length = graph.get_length(*iter);
+            size_t node_length = gbwt_graph->get_length(*iter);
             while (node_start + node_length <= minimizer.offset) {
                 node_start += node_length;
                 ++iter;
-                node_length = graph.get_length(*iter);
+                node_length = gbwt_graph->get_length(*iter);
             }
-            pos_t pos { graph.get_id(*iter), graph.get_is_reverse(*iter), minimizer.offset - node_start };
+            pos_t pos { gbwt_graph->get_id(*iter), gbwt_graph->get_is_reverse(*iter), minimizer.offset - node_start };
             if (minimizer.is_reverse) {
                 pos = reverse_base_pos(pos, node_length);
             }
@@ -224,15 +229,10 @@ int main_minimizer(int argc, char** argv) {
     // in each orientation. If we consider only the windows starting in forward (reverse) orientation,
     // we may skip windows that cross from a reverse node to a forward node (from a forward node to a
     // reverse node).
-    if (gbwt_name.empty()) {
-        for_each_window(*xg_index, index->k() + index->w() - 1, lambda, (threads > 1));
-    } else {
-        for_each_haplotype_window(*gbwt_graph, index->k() + index->w() - 1, lambda, (threads > 1));
-    }
+    for_each_haplotype_window(*gbwt_graph, index->k() + index->w() - 1, lambda, (threads > 1));
     for (int thread_id = 0; thread_id < threads; thread_id++) {
         flush_cache(thread_id);
     }
-    xg_index.reset(nullptr);
     gbwt_graph.reset(nullptr);
     gbwt_index.reset(nullptr);
 
