1b2e5fb0ec54f1b65ccd72d4d6ed427dff471c25
Jouni Siren <jouni.siren@iki.fi>
2019-09-30 22:40:36 -0700

    Use hash sets as buffers

--- src/prune.cpp
+++ src/prune.cpp
@@ -9,7 +9,7 @@ pair_hash_set<edge_t> find_edges_to_prune(const HandleGraph& graph, size_t k, si
     // Each thread collects edges to be deleted into a separate buffer. When the buffer grows
     // large enough, flush it into a shared hash set.
     pair_hash_set<edge_t> result;
-    auto flush_buffer = [&result](std::vector<edge_t>& buffer) {
+    auto flush_buffer = [&result](pair_hash_set<edge_t>& buffer) {
         #pragma omp critical (prune_flush)
         {
             for (const edge_t& edge : buffer) {
@@ -20,7 +20,7 @@ pair_hash_set<edge_t> find_edges_to_prune(const HandleGraph& graph, size_t k, si
     };
 
     // for each position on the forward and reverse of the graph
-    std::vector<std::vector<edge_t>> buffers(get_thread_count());
+    std::vector<pair_hash_set<edge_t>> buffers(get_thread_count());
     graph.for_each_handle([&](const handle_t& h) {
         // for the forward and reverse of this handle
         // walk k bases from the end, so that any kmer starting on the node will be represented in the tree we build
@@ -44,7 +44,7 @@ pair_hash_set<edge_t> find_edges_to_prune(const HandleGraph& graph, size_t k, si
                     graph.follow_edges(walk.curr, false, [&](const handle_t& next) {
                             if (next_count > 1 && edge_max == walk.forks) { // our next step takes us over the max
                                 int tid = omp_get_thread_num();
-                                buffers[tid].push_back(graph.edge_handle(walk.curr, next));
+                                buffers[tid].insert(graph.edge_handle(walk.curr, next));
                                 if (buffers[tid].size() >= PRUNE_THREAD_BUFFER_SIZE) {
                                     flush_buffer(buffers[tid]);
                                 }
@@ -84,7 +84,7 @@ pair_hash_set<edge_t> find_edges_to_prune(const HandleGraph& graph, size_t k, si
                             graph.follow_edges(walk.curr, false, [&](const handle_t& next) {
                                 if (next_count > 1 && edge_max == walk.forks) { // our next step takes us over the max
                                     int tid = omp_get_thread_num();
-                                    buffers[tid].push_back(graph.edge_handle(walk.curr, next));
+                                    buffers[tid].insert(graph.edge_handle(walk.curr, next));
                                     if (buffers[tid].size() >= PRUNE_THREAD_BUFFER_SIZE) {
                                         flush_buffer(buffers[tid]);
                                     }
@@ -108,7 +108,7 @@ pair_hash_set<edge_t> find_edges_to_prune(const HandleGraph& graph, size_t k, si
     }, true);
 
     // Flush the buffers and return the result.
-    for (std::vector<edge_t>& buffer : buffers) {
+    for (pair_hash_set<edge_t>& buffer : buffers) {
         flush_buffer(buffer);
     }
     return result;
