85b34316c16c38ba68f7299efffcd941ff0a064a
Susanna Morin <susanna.morin@gmail.com>
2020-01-29 16:44:48 -0800

    Finished mcmc_caller

--- mcmc.sh
+++ mcmc.sh
@@ -1,8 +1,10 @@
 set -ex
 
 # Index a couple of nearly identical contigs
-./bin/vg construct -m 1000 -a -r test/small/x.fa -v test/small/x.vcf.gz >x.vg
-./bin/vg  index -x x.xg -g x.gcsa -v test/small/x.vcf.gz --gbwt-name x.gbwt -k 16 x.vg
+# ./bin/vg construct -m 1000 -a -r test/small/x.fa -v test/small/x.vcf.gz >x.vg
+# ./bin/vg  index -x x.xg -g x.gcsa -v test/small/x.vcf.gz --gbwt-name x.gbwt -k 16 x.vg
+./bin/vg construct -m 1000 -a -r test/1mb1kgp/z.fa -v test/1mb1kgp/z.vcf.gz >x.vg
+./bin/vg  index -x x.xg -g x.gcsa -v test/1mb1kgp/z.vcf.gz --gbwt-name x.gbwt -k 16 x.vg
 
 # simulate reads
 ./bin/vg sim -l 100 -n 100 -e 0.1 -i 0.1 -a -x x.xg > reads.gam
@@ -17,4 +19,4 @@ set -ex
 ./bin/vg mcmc reads.mgam x.vg sites.snarls > graph_with_paths.vg
 
 # call view on output
-./bin/vg view -d -w graph_with_paths.vg | dot -Tpdf -o /home/susanna/Desktop/img.pdf
+# ./bin/vg view -d -w graph_with_paths.vg | dot -Tpdf -o /home/susanna/Desktop/img.pdf
