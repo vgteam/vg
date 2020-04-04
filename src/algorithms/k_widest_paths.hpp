debb6406ba62f2416c52a6a981200f8b0c743116
Glenn Hickey <glenn.hickey@gmail.com>
2020-04-04 17:01:26 -0400

    some small optimizations

--- src/algorithms/k_widest_paths.hpp
+++ src/algorithms/k_widest_paths.hpp
@@ -22,12 +22,14 @@ namespace algorithms {
 ///  -- looks for the "widest" path (maximum minimum weight) instead of shortest
 ///  -- counts node and edge weights (via callbakcs)
 ///  -- returns the path as well as the score
-///  -- option for ignoring certain nodes and edges in search (required by Yen's algorithm) 
+///  -- option for ignoring certain nodes and edges in search (required by Yen's algorithm)
+///  -- stop if score dips below cutoff
 pair<double, vector<handle_t>> widest_dijkstra(const HandleGraph* g, handle_t source, handle_t sink,
                                                function<double(const handle_t&)> node_weight_callback,
                                                function<double(const edge_t&)> edge_weight_callback,
                                                function<bool(const handle_t&)> is_node_ignored_callback,
-                                               function<bool(const edge_t&)> is_edge_ignored_callbback);
+                                               function<bool(const edge_t&)> is_edge_ignored_callbback,
+                                               double cutoff = 0.0);
 
 /// Find the k widest paths
 vector<pair<double, vector<handle_t>>> yens_k_widest_paths(const HandleGraph* g, handle_t source, handle_t sink,
