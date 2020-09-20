9aa88fecf1142c275988258cba814e76e787f822
Jordan Eizenga <joeizeng@gmail.com>
2020-09-19 19:49:34 -0700

    make mp aln emitter inherit from hts emitter

--- src/multipath_alignment_emitter.hpp
+++ src/multipath_alignment_emitter.hpp
@@ -15,6 +15,7 @@
 #include <vg/io/protobuf_emitter.hpp>
 #include <vg/io/stream_multiplexer.hpp>
 #include "multipath_alignment.hpp"
+#include "hts_alignment_emitter.hpp"
 #include "alignment.hpp"
 
 namespace vg {
@@ -23,14 +24,20 @@ using namespace std;
 /*
  * Class that handles multithreaded output for multipath alignments
  */
-class MultipathAlignmentEmitter {
+class MultipathAlignmentEmitter : public HTSWriter {
 public:
     
     /// Initialize with the intended output stream and the maximum number of threads that
-    /// will be outputting. Optionally converts to single path alignments in either "gam" or "gaf"
-    /// format.
-    MultipathAlignmentEmitter(ostream& out, int num_threads, const HandleGraph& graph,
-                              const string out_format = "gamp");
+    /// will be outputting.
+    /// Allowed formats:
+    /// - "GAMP"
+    /// - "GAM", involves conversion to single path
+    /// - "GAF", involves conversion to single path, requires a  graph
+    /// - "SAM", "BAM", "CRAM:" requires path length map, and all input alignments must
+    ///  already be surjected. If alignments have connections, requires a graph
+    MultipathAlignmentEmitter(const string& filename, size_t num_threads, const string out_format = "GAMP",
+                              const PathPositionHandleGraph* graph = nullptr,
+                              const map<string, int64_t>* path_length = nullptr);
     ~MultipathAlignmentEmitter();
     
     /// Choose a read group to apply to all emitted alignments
@@ -41,25 +48,35 @@ public:
     
     /// Emit paired read mappings as interleaved protobuf messages
     void emit_pairs(const string& name_1, const string& name_2,
-                    vector<pair<multipath_alignment_t, multipath_alignment_t>>&& mp_aln_pairs);
+                    vector<pair<multipath_alignment_t, multipath_alignment_t>>&& mp_aln_pairs,
+                    vector<pair<tuple<string, bool, int64_t>, tuple<string, bool, int64_t>>>* path_positions = nullptr,
+                    vector<int64_t>* tlen_limits = nullptr);
     
     /// Emit read mappings as protobuf messages
-    void emit_singles(const string& name, vector<multipath_alignment_t>&& mp_alns);
+    void emit_singles(const string& name, vector<multipath_alignment_t>&& mp_alns,
+                      vector<tuple<string, bool, int64_t>>* path_positions = nullptr);
     
 private:
     
-    void convert_multipath_alignment(const multipath_alignment_t& mp_aln, Alignment& aln,
-                                     const string* prev_name = nullptr,
-                                     const string* next_name = nullptr) const;
+    enum output_format_t {GAMP, GAM, GAF, BAM, SAM, CRAM};
+    output_format_t format;
     
-    /// the graph we're aligning against
-    const HandleGraph& graph;
+    void convert_to_alignment(const multipath_alignment_t& mp_aln, Alignment& aln,
+                              const string* prev_name = nullptr,
+                              const string* next_name = nullptr) const;
     
-    /// the stream that everything is emitted into
-    ostream& out;
+    void convert_to_hts_unpaired(const string& name, const multipath_alignment_t& mp_aln,
+                                 const string& ref_name, bool ref_rev, int64_t ref_pos,
+                                 bam_hdr_t* header, vector<bam1_t*>& dest) const;
     
-    /// shared multiplexer across streams
-    vg::io::StreamMultiplexer multiplexer;
+    void convert_to_hts_paired(const string& name_1, const string& name_2,
+                               const multipath_alignment_t& mp_aln_1,
+                               const multipath_alignment_t& mp_aln_2,
+                               const string& ref_name_1, bool ref_rev_1, int64_t ref_pos_1,
+                               const string& ref_name_2, bool ref_rev_2, int64_t ref_pos_2,
+                               int64_t tlen_limit, bam_hdr_t* header, vector<bam1_t*>& dest) const;
+    
+    const PathPositionHandleGraph* graph;
     
     /// an Alignment emitter for each thread
     vector<unique_ptr<vg::io::ProtobufEmitter<Alignment>>> aln_emitters;
