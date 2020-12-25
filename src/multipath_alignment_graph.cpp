2e7f8a78f687ab5451615f351a583517f3376feb
Jordan Eizenga <joeizeng@gmail.com>
2020-12-25 12:36:54 -0800

    turn off debug

--- src/multipath_alignment_graph.cpp
+++ src/multipath_alignment_graph.cpp
@@ -5,7 +5,7 @@
 #include "multipath_alignment_graph.hpp"
 #include "sequence_complexity.hpp"
 
-#define debug_multipath_alignment
+//#define debug_multipath_alignment
 
 using namespace std;
 namespace vg {
