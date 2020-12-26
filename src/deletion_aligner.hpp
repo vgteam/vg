7846638a46e57d272fcb383c9b48fd5a40d6c337
Jordan Eizenga <joeizeng@gmail.com>
2020-12-25 21:53:13 -0800

    hook deletion aligner into banded global

--- src/deletion_aligner.hpp
+++ src/deletion_aligner.hpp
@@ -4,8 +4,8 @@
  * Defines an aligner for global deletions
  *
  */
-#ifndef VG_DINUCLEOTIDE_MACHINE_GRAPH_HPP_INCLUDED
-#define VG_DINUCLEOTIDE_MACHINE_GRAPH_HPP_INCLUDED
+#ifndef VG_DELETION_ALIGNER_GRAPH_HPP_INCLUDED
+#define VG_DELETION_ALIGNER_GRAPH_HPP_INCLUDED
 
 #include <cstdint>
 #include <vg/vg.pb.h>
@@ -25,24 +25,33 @@ using namespace std;
 class DeletionAligner {
 public:
     DeletionAligner(int8_t gap_open, int8_t gap_extension);
-    DeletionAligner() = delete;
+    DeletionAligner() = default;
     ~DeletionAligner() = default;
     
+    // store a global alignment of an empty sequence to this graph in the alignment
+    // crashes if alignment sequence is not empty
     void align(Alignment& aln, const HandleGraph& graph) const;
     
+    // store a global alignment of an empty sequence to this graph in the alignment
+    // also store the next highest-scoring alignments in the vector
+    // crashes if alignment sequence is not empty
     void align_multi(Alignment& aln, vector<Alignment>& alt_alns,
                      const HandleGraph& graph, int32_t max_alt_alns) const;
     
     
 private:
     
+    // do the dynamic programming and return the trace backs
     vector<vector<handle_t>> run_dp(const HandleGraph& graph,
                                     int32_t max_tracebacks) const;
     
+    // calculate min distance with DP. returns the DP table and
+    // the sink nodes in the graph, with their full DP value
     pair<vector<size_t>, vector<pair<size_t, size_t>>> min_dists(const vector<handle_t>& order,
                                                                  const unordered_map<handle_t, size_t>& index_of,
                                                                  const HandleGraph& graph) const;
     
+    // compute tracebacks using the DP table
     vector<vector<handle_t>> traceback(const vector<handle_t>& order,
                                        const unordered_map<handle_t, size_t>& index_of,
                                        const HandleGraph& graph,
@@ -50,6 +59,7 @@ private:
                                        const vector<pair<size_t, size_t>>& sinks,
                                        size_t max_tracebacks) const;
     
+    // convert a trace into an alignment path
     void trace_to_alignment(Alignment& aln, const vector<handle_t>& trace,
                             const HandleGraph& graph) const;
     
