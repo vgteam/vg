74fccf645796badeaf3b5a478928df5dc33c0edb
Susanna Morin <susanna.morin@gmail.com>
2021-01-26 16:44:50 -0800

    sparse_union_find builds now

--- src/sparse_union_find.cpp
+++ src/sparse_union_find.cpp
@@ -1,26 +1,23 @@
 
-#include "structures/sparse_union_find.hpp"
+#include "sparse_union_find.hpp"
 
-namespace structures {
+namespace vg {
 
     using namespace std;
 
-    SparseUnionFind::SparseUnionFind(){size_t size, bool include_children, vector<size_t> node_ids) :
-        IDs(IDs), include_children(include_children), node_ids(node_ids){
-        
-        uf_nodes.reserve(size);
-        for (size_t i = 0; i < size; i++) {
-            uf_nodes.emplace_back(i);
+    SparseUnionFind::SparseUnionFind(bool include_children, vector<size_t> node_ids) :
+        include_children(include_children), node_ids(node_ids), UnionFind(node_ids.size(), include_children) {
+        for (size_t i = 0; i < node_ids.size(); i++) {
             sparse_to_dense[node_ids[i]] = i; 
             dense_to_sparse[i]= node_ids[i]; 
-        }
-        
-        
+        }        
+    }
+    SparseUnionFind::~SparseUnionFind(){
+        //nothing to do
     }
 
-
-    SparseUnionFind::find_group(size_t i){
-       size_t dense_group_id = UnionFind::find_group(size_t i);
+    size_t SparseUnionFind::find_group(size_t i){
+       size_t dense_group_id = UnionFind::find_group(i);
        
        size_t sparse_group_id = dense_to_sparse[dense_group_id];
 
@@ -28,22 +25,10 @@ namespace structures {
 
     }
 
-    vector<size_t> UnionFind::group(size_t i) {
-        assert(include_children);
-        vector<size_t> to_return;//contains dense node_ids
-        // go to head of group
-        vector<size_t> stack{find_group(i)};
-        // traverse tree downwards to find all indices in group
-        while (!stack.empty()) {
-            size_t curr = stack.back();
-            stack.pop_back();
-            to_return.push_back(curr);
-            unordered_set<size_t>& children = uf_nodes[curr].children;
-            for (size_t child : children) {
-                stack.push_back(child);
-            }
-        }
-
+    vector<size_t> SparseUnionFind::group(size_t i) {
+        
+        vector<size_t> to_return = UnionFind::group(sparse_to_dense.at(i));
+       
         vector<size_t> sparse_to_return;
         //traverse to_return and retrieve ids from dense_to_sparse
         for(size_t i =0; i < to_return.size(); i++){
@@ -57,15 +42,9 @@ namespace structures {
         return sparse_to_return;
     }
 
-    vector<vector<size_t>> UnionFind::all_groups() {
-        vector<vector<size_t>> to_return(uf_nodes.size());
-        for (size_t i = 0; i < uf_nodes.size(); i++) {
-            to_return[find_group(i)].push_back(i);
-        }
-        auto new_end = std::remove_if(to_return.begin(), to_return.end(),
-                                    [](const vector<size_t>& grp) { return grp.empty(); });
-        to_return.resize(new_end - to_return.begin());
-
+    vector<vector<size_t>> SparseUnionFind::all_groups() {
+        vector<vector<size_t>> to_return = UnionFind::all_groups();
+        
         vector<vector<size_t>> sparse_to_return;
 
         //translate from dense to sparse
@@ -85,4 +64,4 @@ namespace structures {
     }
 
 
-};
+}
--- src/sparse_union_find.hpp
+++ src/sparse_union_find.hpp
@@ -3,34 +3,53 @@
 
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
 #include <cstdint>
 #include <algorithm>
 #include <cassert>
-#include "structures/union_find.hpp"
+#include "deps/structures/src/include/structures/union_find.hpp"
 
-namespace structures {
+namespace vg {
 
-using namespace std;
+    using namespace std;
+    using namespace structures;
 
-class SparseUnionFind : public UnionFind{
+    class SparseUnionFind : public UnionFind{
 
-    public:
-    vector<size_t> IDs;
+        public:
+        vector<size_t> node_ids;
+        unordered_map<size_t, size_t> sparse_to_dense;//incoming 
+        unordered_map<size_t, size_t> dense_to_sparse;//outgoing
+        
 
-    SparseUnionFind(size_t size, bool include_children, vector<size_t> node_ids);
+        SparseUnionFind(bool include_children, vector<size_t> node_ids);
 
-    find_group(size_t i);
+        /// Destructor
+        ~SparseUnionFind();
+        
+        /// Returns the number of indices in the UnionFind
+        size_t size();
+        
+        /// Returns the group ID that index i belongs to (can change after calling union)
+        size_t find_group(size_t i);
+        
+        /// Merges the group containing index i with the group containing index j
+        void union_groups(size_t i, size_t j);
+        
+        /// Returns the size of the group containing index i
+        size_t group_size(size_t i);
+        
+        /// Returns a vector of the indices in the same group as index i
+        vector<size_t> group(size_t i);
+        
+        /// Returns all of the groups, each in a separate vector
+        vector<vector<size_t>> all_groups();
 
-    private:
-    struct UFNode;
-    vector<UFNode> uf_nodes;
-    bool include_children;
-    unordered_map<size_t, size_t> sparse_to_dense;//incoming 
-    unordered_map<size_t, size_t> dense_to_sparse;//outgoing
+        private:
+        bool include_children;
 
-
-
-};
+    };
+  
 
 }
 #endif
