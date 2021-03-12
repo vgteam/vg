c998d375148e15e877d42256b7ebcca10a4ed97c
Adam Novak <anovak@soe.ucsc.edu>
2021-03-12 12:03:08 -0800

    Count up instructions used on mapping across threads

--- src/subcommand/giraffe_main.cpp
+++ src/subcommand/giraffe_main.cpp
@@ -1206,8 +1206,8 @@ int main_giraffe(int argc, char** argv) {
         // We also time in terms of CPU time
         clock_t cpu_time_before;
         
-        // We may also have access to perf stats
-        int perf_fd = 0;
+        // We may also have access to perf stats.
+        vector<int> perf_fds;
         
 #ifdef __linux__
         // Set up a counter for executed instructions.
@@ -1217,18 +1217,42 @@ int main_giraffe(int argc, char** argv) {
         perf_config.type = PERF_TYPE_HARDWARE;
         perf_config.size = sizeof(struct perf_event_attr);
         perf_config.config = PERF_COUNT_HW_INSTRUCTIONS;
-        // Start disabled
-        perf_config.disabled = 1;
         perf_config.exclude_kernel = 1;
         perf_config.exclude_hv = 1;
-        perf_fd = perf_event_open(&perf_config, 0, -1, -1, 0);
-        // TODO: check errno/strerror here. May not work if not root!
+        
+        perf_fds.resize(thread_count);
+        
+        perf_fds[omp_get_thread_num()] = perf_event_open(&perf_config, 0, -1, -1, 0);
+        if (show_progress && perf_fds[omp_get_thread_num()] == -1) {
+            int problem = errno;
+            cerr << "Not counting CPU instructions because perf events are unavailable: " << strerror(problem) << endl;
+            perf_fds.clear();
+        }
+        
+        // Each OMP thread will call this to make sure perf is on.
+        auto ensure_perf_for_thread = [&]() {
+            if (!perf_fds.empty() && perf_fds[omp_get_thread_num()] == 0) {
+                perf_fds[omp_get_thread_num()] = perf_event_open(&perf_config, 0, -1, -1, 0);
+            }
+        };
+        
+        // Main thread will call this to turn it off
+        auto stop_perf_for_thread = [&]() {
+            if (!perf_fds.empty() && perf_fds[omp_get_thread_num()] != 0) {
+                ioctl(perf_fds[omp_get_thread_num()], PERF_EVENT_IOC_DISABLE, 0);
+            }
+        };
+        
+        // Main thread will call this when mapping starts to reset the counter.
+        auto reset_perf_for_thread = [&]() {
+            if (!perf_fds.empty() && perf_fds[omp_get_thread_num()] != 0) {
+                ioctl(perf_fds[omp_get_thread_num()], PERF_EVENT_IOC_RESET, 0);
+            }
+        };
+        
+        // TODO: we won't count the output thread, but it will appear in CPU time!
 #endif
 
-
-        cerr << "Profiling with fd " << perf_fd << endl;
-        assert(perf_fd != 0 && paef_fd != -1);
-        
         {
         
             // Look up all the paths we might need to surject to.
@@ -1256,10 +1280,7 @@ int main_giraffe(int argc, char** argv) {
             cpu_time_before = clock();
             
 #ifdef __linux__
-            if (perf_fd) {
-                ioctl(perf_fd, PERF_EVENT_IOC_RESET, 0);
-                ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, 0);
-            }
+            reset_perf_for_thread();
 #endif
 
             if (interleaved || !fastq_filename_2.empty()) {
@@ -1300,6 +1321,9 @@ int main_giraffe(int argc, char** argv) {
                 
                 // Define how to align and output a read pair, in a thread.
                 auto map_read_pair = [&](Alignment& aln1, Alignment& aln2) {
+#ifdef __linux__
+                    ensure_perf_for_thread();
+#endif
                     
                     pair<vector<Alignment>, vector<Alignment>> mapped_pairs = minimizer_mapper.map_paired(aln1, aln2, ambiguous_pair_buffer);
                     if (!mapped_pairs.first.empty() && !mapped_pairs.second.empty()) {
@@ -1372,6 +1396,10 @@ int main_giraffe(int argc, char** argv) {
             
                 // Define how to align and output a read, in a thread.
                 auto map_read = [&](Alignment& aln) {
+#ifdef __linux__
+                    ensure_perf_for_thread();
+#endif
+                
                     // Map the read with the MinimizerMapper.
                     minimizer_mapper.map(aln, *alignment_emitter);
                     // Record that we mapped a read.
@@ -1398,9 +1426,7 @@ int main_giraffe(int argc, char** argv) {
         std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
         clock_t cpu_time_after = clock();
 #ifdef __linux__
-        if (perf_fd) {
-            ioctl(perf_fd, PERF_EVENT_IOC_DISABLE, 0);
-        }
+        stop_perf_for_thread();
 #endif
         
         // Compute wall clock elapsed
@@ -1412,16 +1438,21 @@ int main_giraffe(int argc, char** argv) {
         
         // Compute instructions used
         long long total_instructions = 0;
-        if (perf_fd) {
-            if (read(perf_fd, &total_instructions, sizeof(long long)) != sizeof(long long)) {
-                // Read failed for some reason.
-                cerr << "Could not count instructions" << endl;
-                total_instructions = 0;
+        for (auto& perf_fd : perf_fds) {
+            if (perf_fd > 0) {
+                long long thread_instructions;
+                if (read(perf_fd, &thread_instructions, sizeof(long long)) != sizeof(long long)) {
+                    // Read failed for some reason.
+                    cerr << "warning:[vg giraffe] Could not count CPU instructions executed" << endl;
+                    thread_instructions = 0;
+                }
+                if (close(perf_fd)) {
+                    int problem = errno;
+                    cerr << "warning:[vg giraffe] Error closing perf event instruction counter: " << strerror(problem) << endl;
+                }
+                total_instructions += thread_instructions;
             }
-            close(perf_fd);
         }
-        
-        cerr << "Total instructions: " << total_instructions << endl;
         
         // How many reads did we map?
         size_t total_reads_mapped = 0;
@@ -1433,8 +1464,8 @@ int main_giraffe(int argc, char** argv) {
         double reads_per_second_per_thread = total_reads_mapped / (all_threads_seconds.count() * thread_count + first_thread_additional_seconds.count());
         // And per CPU second (including any IO threads)
         double reads_per_cpu_second = total_reads_mapped / cpu_seconds;
-        double instructions_per_read = total_instructions / (double)total_reads_mapped;
-        double instructions_per_second = total_instructions / cpu_seconds;
+        double mega_instructions_per_read = total_instructions / (double)total_reads_mapped / 1E6;
+        double mega_instructions_per_second = total_instructions / cpu_seconds / 1E6;
         
         if (show_progress) {
             // Log to standard error
@@ -1442,16 +1473,18 @@ int main_giraffe(int argc, char** argv) {
                 << thread_count << " threads in "
                 << all_threads_seconds.count() << " seconds with " 
                 << first_thread_additional_seconds.count() << " additional single-threaded seconds." << endl;
-            cerr << "Used " << cpu_seconds << " CPU-seconds." << endl;
-            
             cerr << "Mapping speed: " << reads_per_second_per_thread
                 << " reads per second per thread" << endl;
+            
+            cerr << "Used " << cpu_seconds << " CPU-seconds (including output)." << endl;
             cerr << "Mapping speed: " << reads_per_cpu_second
-                << " reads per CPU-second" << endl;
+                << " reads per CPU-second (including output)" << endl;
+            
             if (total_instructions != 0) {
-                cerr << "Mapping slowness: " << instructions_per_read
-                    << " instructions per read at " << instructions_per_second
-                    << " instructions per CPU-second" << endl;
+                cerr << "Used " << total_instructions << " CPU instructions (not including output)." << endl;
+                cerr << "Mapping slowness: " << mega_instructions_per_read
+                    << " M instructions per read at " << mega_instructions_per_second
+                    << " M mapping instructions per inclusive CPU-second" << endl;
             }
 
             cerr << "Memory footprint: " << gbwt::inGigabytes(gbwt::memoryUsage()) << " GB" << endl;
