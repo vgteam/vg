54635984b97bc75b75670f6445e8e1eeb98bef68
Adam Novak <anovak@soe.ucsc.edu>
2021-03-12 13:28:23 -0800

    Add speed test script

---
+++ scripts/giraffe-speed.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# giraffe-speed.sh: evaluate Giraffe mapping speed.
+# Suggested to run on an AWS
+# Partially based on https://github.com/vgteam/giraffe-sv-paper/blob/566573b708878d8854acb088a0a8f7c920b120eb/scripts/giraffe/speed_giraffe.sh
+set -e
+
+THREAD_COUNT=16
+
+#2million real reads
+READS=s3://vg-k8s/profiling/reads/real/NA19239/novaseq6000-ERR3239454-shuffled-1m.fq.gz
+
+# The HGSVC graph is smaller and faster to load so we use that here.
+GRAPH=hgsvc
+GRAPH_BASE=s3://vg-k8s/profiling/graphs/v2/for-NA19240/hgsvc/hs38d1/HGSVC_hs38d1
+# sampled.64 isn't much bigger than the base GBWT for HGSVC
+GBWT="sampled.64"
+
+function fetch() {
+    # Download a URL to a file, if it isn't there already
+    if [ ! -e "${2}" ] ; then
+        # For now we only do S3 URLs
+        aws s3 cp "${1}" "${2}"
+    fi
+}
+
+fetch "${READS}" "./reads.fq.gz"
+fetch "${GRAPH_BASE}.xg" "./${GRAPH}.xg"
+fetch "${GRAPH_BASE}.dist" "./${GRAPH}.dist"
+fetch "${GRAPH_BASE}.${GBWT}.gbwt" "./${GRAPH}.${GBWT}.gbwt"
+fetch "${GRAPH_BASE}.${GBWT}.gg" "./${GRAPH}.${GBWT}.gg"
+fetch "${GRAPH_BASE}.${GBWT}.min" "./${GRAPH}.${GBWT}.min"
+
+/usr/bin/time -v timeout -k1 1h bash -c "vg giraffe -x '${GRAPH}.xg' -H '${GRAPH}.${GBWT}.gbwt' -g '${GRAPH}.${GBWT}.gg' -m '${GRAPH}.${GBWT}.min' -d '${GRAPH}.dist' -f '${READS}.fq.gz' -i -t '${THREAD_COUNT}' -p 2>log.txt >mapped.gam" 2> time-log.txt || true
+
