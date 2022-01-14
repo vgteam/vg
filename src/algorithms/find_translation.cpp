5dd7ac2e765a03a2f5050e7ce96bb4c7cbf0db25
Adam Novak <anovak@soe.ucsc.edu>
2022-01-14 16:58:17 -0500

    Account for graphs that are translations

--- src/algorithms/find_translation.cpp
+++ src/algorithms/find_translation.cpp
@@ -14,6 +14,10 @@ const NamedNodeBackTranslation* find_translation(const HandleGraph* graph) {
         // No graph means no translation.
         return nullptr;
     }
+    if (dynamic_cast<const NamedNodeBackTranslation*>(graph)) {
+        // Some graph implementations (like GBWTGraph) just are a NamedNodeBackTranslation already.
+        return dynamic_cast<const NamedNodeBackTranslation*>(graph);
+    }
     if (dynamic_cast<const GFAHandleGraph*>(graph)) {
         // If we loaded the graph fropm a GFA we would have attached this translation.
         return &dynamic_cast<const GFAHandleGraph*>(graph)->gfa_id_space;
