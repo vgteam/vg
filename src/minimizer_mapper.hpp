f35c391d5b1cc3dc1fcf28986dcdbf8d50f75fb7
Xian <xhchang@ucsc.edu>
2022-05-29 12:55:12 -0400

    Annotate old and new seeds properly in giraffe

--- src/minimizer_mapper.hpp
+++ src/minimizer_mapper.hpp
@@ -376,6 +376,7 @@ protected:
     /**
      * Add annotations to an Alignment with statistics about the minimizers.
      */
+    void old_annotate_with_minimizer_statistics(Alignment& target, const std::vector<Minimizer>& minimizers, const std::vector<OldSeed>& seeds, const Funnel& funnel) const;
     void annotate_with_minimizer_statistics(Alignment& target, const std::vector<Minimizer>& minimizers, const std::vector<Seed>& seeds, const Funnel& funnel) const;
 
 //-----------------------------------------------------------------------------
