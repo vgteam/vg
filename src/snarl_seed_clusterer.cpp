e6c44fdf85631e05521b10c1812d3d97621e4c05
Xian Chang <xhchang@plaza.gi.ucsc.edu>
2022-08-11 20:59:08 -0700

    Save depth

--- src/snarl_seed_clusterer.cpp
+++ src/snarl_seed_clusterer.cpp
@@ -280,6 +280,9 @@ void NewSnarlSeedClusterer::get_nodes( TreeState& tree_state, vector<ParentToChi
 cerr << "Add all seeds to nodes: " << endl;
 #endif
 
+    //Map node id to the depth of its parent
+    hash_map<id_t, size_t> id_to_depth;
+
 
     // Assign each seed to a node.
 
@@ -379,8 +382,11 @@ cerr << "Add all seeds to nodes: " << endl;
                     depth = 2;
                 } else if (std::get<7>(old_cache)) {
                     depth = 1;
-                } else {
+                } else if (id_to_depth.count(id) == 0) {
                     depth = distance_index.get_depth(parent);
+                    id_to_depth[id] = depth;
+                } else {
+                    depth = id_to_depth[id];
                 }
 #ifdef DEBUG_CLUSTE
                 assert(depth == distance_index.get_depth(parent));R
