1b9dc1b7855ce429a5f3ebc8a3801bad4b80b9a9
Xian Chang <xhchang@plaza.gi.ucsc.edu>
2023-01-05 15:40:36 -0800

    Increase snarl size limit

--- src/subcommand/index_main.cpp
+++ src/subcommand/index_main.cpp
@@ -134,7 +134,7 @@ int main_index(int argc, char** argv) {
     bool xg_alts = false;
 
     //Distance index
-    size_t snarl_limit = 3000;
+    size_t snarl_limit = 5000;
 
     int c;
     optind = 2; // force optind past command positional argument
