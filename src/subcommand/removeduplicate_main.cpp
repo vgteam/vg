249a97ed8a6bf076ef08b94ad49cc94894fbbf50
parsaeskandar <parsa.eskandar@gmail.com>
2023-02-17 12:36:20 -0800

    make_coalesced_sorted_intervals function

--- src/subcommand/removeduplicate_main.cpp
+++ src/subcommand/removeduplicate_main.cpp
@@ -27,6 +27,41 @@ using namespace std;
 using namespace vg;
 using namespace vg::subcommand;
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+vector <pair<long long, long long>> make_coalesced_sorted_intervals(const Alignment &aln) {
+    /// return a vector of pairs of id-ts of the node ids that are themselves sorted and properly coalesced
+    vector<long long> sorted_node_ids;
+    vector <pair<long long, long long>> intervals;
+    for (const Mapping mapping: aln.path().mapping()) {
+        sorted_node_ids.push_back(mapping.position().node_id());
+    }
+
+    if (sorted_node_ids.empty()) {
+        return intervals;
+    }
+    // We have a sorted vector of node_ids
+    sort(sorted_node_ids.begin(), sorted_node_ids.end());
+
+
+    long long start = sorted_node_ids[0];
+    long long end = sorted_node_ids[0];
+    for (long long i = 1; i < sorted_node_ids.size(); i++) {
+        if (sorted_node_ids[i] == end + 1) {
+            end = sorted_node_ids[i];
+        } else {
+            intervals.push_back(make_pair(start, end));
+            start = end = sorted_node_ids[i];
+        }
+    }
+
+    intervals.push_back(make_pair(start, end));
+    return intervals;
+}
+
+
 vector <pos_t> alignment_position(const Alignment &aln) {
     /// return a vector of pos_t variables which the index of the vector points to the read offset and the value points to the node pos_t
     vector <pos_t> positions;
@@ -78,9 +113,8 @@ bool check_duplicate(const Alignment aln1, const Alignment aln2) {
 
 }
 
-void print_gam(const Alignment aln){
-    cout << aln.sequence() << endl;
-}
+
+
 
 void help_rmvdup(char **argv) {
 // TODO: add whatever option is needed to this list. Change long_option and getopt_long if want to add an option.
@@ -141,57 +175,59 @@ int main_rmvdup(int argc, char *argv[]) {
         exit(1);
     }
 
-//
-//    // This is the function that is going to find the position of each PCR in the graph
-
-//    function<void(Alignment & )> test = [&](const Alignment &aln) {
-//        vector <pos_t> temp = alignment_position(aln);
-//        cout << aln.sequence() << endl;
-//        for (auto it = 0; it < temp.size(); it++)
-//            cout << "Read Offset " << it << "Node id " << id(temp[it]) << " Node Offset " << offset(temp[it]) << endl;
-//        cout << endl;
-//        cout << check_duplicate(aln, aln);
-//        cout << temp.size() << temp.
-
-//    };
-//
-//    string file_name = get_input_file_name(optind, argc, argv);
-//    ifstream file_in(file_name);
-//    vg::io::for_each_parallel(file_in, test);
-
-
-//    get_input_file(optind, argc, argv, [&](istream& gam_in) {
-//        vg::io::for_each_parallel(gam_in, record_truth);
-//        GAMSorter gs(show_progress);
-//        unique_ptr<GAMIndex> index;
-//        index = unique_ptr<GAMIndex>(new GAMIndex());
-//        gs.stream_sort(gam_in, cout, index.get());
-//    });
 
     string sorted_gam_name = get_input_file_name(optind, argc, argv);
-    unique_ptr<GAMIndex> gam_index;
-    unique_ptr<vg::io::ProtobufIterator<Alignment>> gam_cursor;
+    unique_ptr <GAMIndex> gam_index;
     if (!sorted_gam_name.empty()) {
         // Load the GAM index
         gam_index = unique_ptr<GAMIndex>(new GAMIndex());
-        get_input_file(sorted_gam_name + ".gai", [&](istream& in) {
+        get_input_file(sorted_gam_name + ".gai", [&](istream &in) {
             // We get it form the appropriate .gai, which must exist
             gam_index->load(in);
         });
     }
+
+    function<void(Alignment & )> test = [&](const Alignment &aln) {
+        if (gam_index.get() != nullptr) {
+
+            // This is a schema of what I am going to do
+            // I mark all the reads that have to get deleted as duplicates. This means one read from each duplicate set remains unmarked.
+            // This way we can remove all reads with duplicate flag and not worry about deleting them all
+            // TODO: check if the above algorithm is logical
+            if (aln not in
+            checked
+            duplicates){ // TODO: handle this with hash
+                // make the alignment nodes list that can be use as input if .find function of the gam_index
+                vector <pair<long long, long long>> intervals = make_coalesced_sorted_intervals(aln);
+                // Find all alignments that share at least one node with the current working alignment
+                get_input_file(sorted_gam_name, [&](istream &input_gam) {
+                    vg::io::ProtobufIterator<Alignment> gam_cursor(input_gam);
+                    // find all sharing nodes alignments and call the function to handle the result
+                    gam_index->find(gam_cursor, intervals, print_gam);
+                });
+
+
+            }
+
+            for (auto interval: intervals) {
+                std::cout << "[" << interval.first << ", " << interval.second << "]" << std::endl;
+            }
+            cout << "============================================" << endl;
+
+
+        }
+    };
+
+
     if (gam_index.get() != nullptr) {
-        // Find in sorted GAM
+        get_input_file(sorted_gam_name, [&](istream &in) {
+            vg::io::for_each(in, test);
+//            vg::io::for_each_parallel(in, test);
 
-        get_input_file(sorted_gam_name, [&](istream& in) {
-            // Make a cursor for input
-            vg::io::ProtobufIterator<Alignment> cursor(in);
-
-
-            // find the alignment for the specific node
-            gam_index->find(cursor, 0, 100, print_gam);
         });
-
     }
+
+
     return 0;
 
 }
