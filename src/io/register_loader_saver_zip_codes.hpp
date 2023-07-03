78655b03bb19e86a60f51fb32609448542e08829
Xian <xhchang@ucsc.edu>
2023-07-03 13:51:47 +0200

    Serialize zipcodes properly (maybe)

---
+++ src/io/register_loader_saver_zip_codes.hpp
@@ -0,0 +1,21 @@
+#ifndef VG_IO_REGISTER_LOADER_SAVER_ZIP_CODES_HPP_INCLUDED
+#define VG_IO_REGISTER_LOADER_SAVER_ZIP_CODES_HPP_INCLUDED
+
+/**
+ * \file register_loader_saver_zip_codes.hpp
+ * Defines IO for a ZipCodeCollection from stream files.
+ */
+
+namespace vg {
+
+namespace io {
+
+using namespace std;
+
+void register_loader_saver_zip_codes();
+
+}
+
+}
+
+#endif
