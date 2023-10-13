5bd82077232091053022f00995cd95c4077aecc0
Adam Novak <anovak@soe.ucsc.edu>
2023-10-13 12:53:01 -0700

    Set min chains to 4 from search

--- src/subcommand/giraffe_main.cpp
+++ src/subcommand/giraffe_main.cpp
@@ -636,7 +636,8 @@ int main_giraffe(int argc, char** argv) {
         .add_entry<double>("mapq-score-scale", 0.0004)
         .add_entry<size_t>("min-to-fragment", 2)
         .add_entry<size_t>("max-to-fragment", 10)
-        .add_entry<double>("fragment-score-fraction", 0.3);
+        .add_entry<double>("fragment-score-fraction", 0.3)
+        .add_entry<size_t>("min-chains", 4);
         
    
     std::vector<struct option> long_options =
