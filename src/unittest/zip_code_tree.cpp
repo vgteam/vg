dd468e80b5243b6b4b46a7468c36a5c92b0a9831
Xian <xhchang@ucsc.edu>
2024-07-09 14:49:57 +0200

    Turn off random tests

--- src/unittest/zip_code_tree.cpp
+++ src/unittest/zip_code_tree.cpp
@@ -2842,7 +2842,7 @@ namespace unittest {
 
     TEST_CASE("Random graphs zip tree", "[zip_tree][zip_tree_random]"){
     
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 0; i++) {
             // For each random graph
     
             default_random_engine generator(time(NULL));
