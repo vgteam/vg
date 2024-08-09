4c208454d3a340cd30619a2c8ce3e928b137eeb2
Jordan Eizenga <joeizeng@gmail.com>
2024-08-08 20:09:32 -0700

    cap the band padding

--- src/algorithms/pad_band.cpp
+++ src/algorithms/pad_band.cpp
@@ -12,7 +12,7 @@ namespace vg {
 namespace algorithms {
 
 
-std::function<size_t(const Alignment&, const HandleGraph&)> pad_band_random_walk_internal(double band_padding_multiplier, size_t band_padding_memo_size,
+std::function<size_t(const Alignment&, const HandleGraph&)> pad_band_random_walk_internal(double band_padding_multiplier, size_t band_padding_memo_size, size_t max_padding,
                                                                                           const std::function<size_t(const Alignment&, const HandleGraph&)> size_function) {
     // We're goign to capture this vector by value into the closure
     std::vector<size_t> band_padding_memo;
@@ -20,29 +20,29 @@ std::function<size_t(const Alignment&, const HandleGraph&)> pad_band_random_walk
     // Fill it in to initialize
     band_padding_memo.resize(band_padding_memo_size);
     for (size_t i = 0; i < band_padding_memo.size(); i++) {
-        band_padding_memo[i] = size_t(band_padding_multiplier * sqrt(i)) + 1;
+        band_padding_memo[i] = std::min<size_t>(max_padding, size_t(band_padding_multiplier * sqrt(i)) + 1);
     }
     
     function<size_t(const Alignment&, const HandleGraph&)> choose_band_padding =
-        [band_padding_multiplier, band_padding_memo, size_function](const Alignment& seq, const HandleGraph& graph) {
+        [band_padding_multiplier, band_padding_memo, size_function, max_padding](const Alignment& seq, const HandleGraph& graph) {
             size_t size = size_function(seq, graph);
             return size < band_padding_memo.size() ? band_padding_memo.at(size)
-                                                   : size_t(band_padding_multiplier * sqrt(size)) + 1;
+                                                   : std::min<size_t>(max_padding, size_t(band_padding_multiplier * sqrt(size)) + 1);
     };
     
     // And return the closure which now owns the memo table.
     return choose_band_padding;
 }
 
-std::function<size_t(const Alignment&, const HandleGraph&)> pad_band_random_walk(double band_padding_multiplier, size_t band_padding_memo_size) {
-    return pad_band_random_walk_internal(band_padding_multiplier, band_padding_memo_size,
+std::function<size_t(const Alignment&, const HandleGraph&)> pad_band_random_walk(double band_padding_multiplier, size_t band_padding_memo_size, size_t max_padding) {
+    return pad_band_random_walk_internal(band_padding_multiplier, band_padding_memo_size, max_padding,
                                         [](const Alignment& seq, const HandleGraph&) {
         return seq.sequence().size();
     });
 }
 
-std::function<size_t(const Alignment&, const HandleGraph&)> pad_band_min_random_walk(double band_padding_multiplier, size_t band_padding_memo_size) {
-    return pad_band_random_walk_internal(band_padding_multiplier, band_padding_memo_size,
+std::function<size_t(const Alignment&, const HandleGraph&)> pad_band_min_random_walk(double band_padding_multiplier, size_t band_padding_memo_size, size_t max_padding) {
+    return pad_band_random_walk_internal(band_padding_multiplier, band_padding_memo_size, max_padding,
                                          [](const Alignment& seq, const HandleGraph& graph) {
         return std::min(seq.sequence().size(), graph.get_total_length());
     });
