b1281b509a9c5ff0feda37ba5c5f2ed2b109cd1b
Jouni Siren <jouni.siren@iki.fi>
2025-03-12 11:58:38 -0700

    More consistent serialize/load errors with GCSA structures

--- src/io/register_loader_saver_gcsa.cpp
+++ src/io/register_loader_saver_gcsa.cpp
@@ -24,15 +24,15 @@ void register_loader_saver_gcsa() {
         gcsa::GCSA* index = new gcsa::GCSA();
         
         // Load it. In case of a failure, this will:
-        // * Print an error message if sanity checks fail.
+        // * Throw std::runtime_error if sanity checks fail.
         // * Fail silently if reading the input fails.
-        // The exceptions are derived from std::runtime_error.
         index->load(input);
         
         // Return it so the caller owns it.
         return (void*) index;
     }, [](const void* index_void, ostream& output) {
         // Cast to GCSA and serialize to the stream.
+        // This will fail silently if writing to the output stream fails.
         assert(index_void != nullptr);
         ((const gcsa::GCSA*) index_void)->serialize(output);
     });
