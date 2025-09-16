0a7c6d4f4355c76b2c31e5b28126f022264c9696
faithokamoto <faith.purpleflower@gmail.com>
2025-09-16 14:53:47 -0700

    better explanation

--- src/log.hpp
+++ src/log.hpp
@@ -6,13 +6,11 @@
  * Shipped along with utility.hpp
  *
  * There are three options here:
- * - emit_with_indent(): assuming that the first line is already indented,
- *   emit a message with a given indent, wrapping lines at 80 columns.
+ * - basic_log(): do nothing but append a context string to the front of
+ *   the log message. This is useful for logging progress messages.
  *   This is a generic logging utility.
- * - emit_warning(): emit a warning message with a given context, optionally
- *   pretty-printed with emit_with_indent().
- * - error_and_exit(): emit an error message with a given context, optionally
- *   pretty-printed with emit_with_indent(), and then exit with EXIT_FAILURE.
+ * - warning(): emit a warning message with a given context
+ * - fatal_error(): emit an error message with a given context
  * 
  * Errors/warnings are output to std::cerr as:
  * error<context>: message
@@ -22,24 +20,18 @@
  * The error/warning functions exist to have a standardized error/warning format
  * across the vg subcommands. You don't *have* to use them elsewhere, but
  * it's a good idea to have consistent formatting.
+ * 
+ * These functions return a cerrWrapper object that behaves like a stream.
+ * You can stream things to it with operator<<, and when the object goes out
+ * of scope, it will exit the program with a failure code (in the case of
+ * fatal_error) or do nothing (in the case of warning and basic_log).
+ * 
+ * Technically you can get the same effect by streaming to cerr, but this
+ * way you don't have to remember to exit after logging an error,
+ * and we can change format in a centralized place.
  */
 
 namespace vg {
-
-void error_and_exit(const std::string& context, const std::string& message, bool pretty_print = true);
-/// Warn the user with a standard format
-/// "context" is the context in which the warning occurs, e.g. "[vg inject]"
-/// If pretty_print is true, will reformat the message to fit within 80 columns
-/// via emit_with_indent(): honors "\n" and "\t", adding extra newlines
-void emit_warning(const std::string& context, const std::string& message, bool pretty_print = true);
-
-/// Write a message to an output stream with indentation
-/// Inserts "indent" spaces on the beginning of each line
-/// Honors existing newlines, but otherwise adds newlines between words
-/// such that everything gets printed with width no more than 80
-/// Assumes that the first line is already indented
-/// (because you put something there and you want others to line up)
-void emit_with_indent(std::ostream& outstream, const std::string& message, std::size_t indent);
 
 // https://stackoverflow.com/a/25615354/
 class cerrWrapper {
