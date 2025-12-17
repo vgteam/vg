e32aaf3723989f21d90a5dc90311e5e8790ae710
Adam Novak <anovak@soe.ucsc.edu>
2025-12-17 16:26:21 -0500

    Use toil-vg with all schema 2+ Dockers and CI image with sufficiently new Python

--- vgci/vgci.sh
+++ vgci/vgci.sh
@@ -32,7 +32,7 @@ KEEP_INTERMEDIATE_FILES=0
 # Should we show stdout and stderr from tests? If so, set to "-s".
 SHOW_OPT=""
 # What toil-vg should we install?
-TOIL_VG_PACKAGE="git+https://github.com/vgteam/toil-vg.git@88365694a895df8b00afccd20613032c2fe94b44"
+TOIL_VG_PACKAGE="git+https://github.com/vgteam/toil-vg.git@028d4176e2c30c8aeba0b05241e0212eca64ecb9"
 # What toil should we install?
 # Could be something like "toil[aws,mesos]==3.20.0"
 # or "toil[wdl,aws,mesos]@git+https://github.com/DataBiosphere/toil.git@52aa2979f23c24001837b2808552973e7bb263e6"
