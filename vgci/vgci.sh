4dec887c326c667af4f0d6a5786b455466995589
Adam Novak <anovak@soe.ucsc.edu>
2026-02-27 11:10:54 -0500

    Use toil-vg that asks for PNGs, and install fewer R packages for plotting

--- vgci/vgci.sh
+++ vgci/vgci.sh
@@ -32,7 +32,7 @@ KEEP_INTERMEDIATE_FILES=0
 # Should we show stdout and stderr from tests? If so, set to "-s".
 SHOW_OPT=""
 # What toil-vg should we install?
-TOIL_VG_PACKAGE="git+https://github.com/vgteam/toil-vg.git@2cb0553e0b39af1602c0a535580d8c25ab9a209c"
+TOIL_VG_PACKAGE="git+https://github.com/vgteam/toil-vg.git@a56a860ad31583ce4b1616c9fa2bfb4bfe06d635"
 # What toil should we install?
 # Could be something like "toil[aws,mesos]==3.20.0"
 # or "toil[wdl,aws,mesos]@git+https://github.com/DataBiosphere/toil.git@52aa2979f23c24001837b2808552973e7bb263e6"
