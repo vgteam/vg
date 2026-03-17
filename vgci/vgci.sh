32eab3843dbf2144ad74fdef76e3993ba1afb2c8
Faith Okamoto <52177356+faithokamoto@users.noreply.github.com>
2026-03-17 16:17:37 -0700

    bump again

--- vgci/vgci.sh
+++ vgci/vgci.sh
@@ -32,7 +32,7 @@ KEEP_INTERMEDIATE_FILES=0
 # Should we show stdout and stderr from tests? If so, set to "-s".
 SHOW_OPT=""
 # What toil-vg should we install?
-TOIL_VG_PACKAGE="git+https://github.com/vgteam/toil-vg.git@b18709c8dea3d01edf05376625124b70bedc522e"
+TOIL_VG_PACKAGE="git+https://github.com/vgteam/toil-vg.git@a4f4e12bc8f14c65e511d88073ed50d9cf52e60b"
 # What toil should we install?
 # Could be something like "toil[aws,mesos]==3.20.0"
 # or "toil[wdl,aws,mesos]@git+https://github.com/DataBiosphere/toil.git@52aa2979f23c24001837b2808552973e7bb263e6"
