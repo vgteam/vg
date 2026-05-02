986c5fd0899dff9e8921148e539c198d8aea6548
Zia <194475824+electricEpilith@users.noreply.github.com>
2026-05-01 17:53:23 -0700

    sonLib, pinchesAndCacti, 3edge now CMake deps

--- src/cactus.hpp
+++ src/cactus.hpp
@@ -6,17 +6,17 @@
  * cactus.hpp: Wrapper utility functions for for pinchesAndCacti
  */
 
-#include <vector>
 #include <map>
+#include <vector>
 
+#include "nodeside.hpp"
 #include "types.hpp"
 #include "utility.hpp"
-#include "nodeside.hpp"
 #include "vg.hpp"
 
 extern "C" {
-#include <sonLib/sonLib.h>
-#include <sonLib/stCactusGraphs.h>
+#include <sonLib.h>
+#include <stCactusGraphs.h>
 }
 
 using namespace std;
@@ -25,30 +25,31 @@ namespace vg {
 
 // We use this when talking to Cactus.
 struct CactusSide {
-    int64_t node;
-    bool is_end;
+  int64_t node;
+  bool is_end;
 };
 
 // Convert VG to Cactus Graph. Takes a list of path names to use to find
 // telomeres if present in a connected component.
-// If we know the graph is a single weakly connected component, single_component can
-// be set to ture to avoid recomputing components.
-// Notes:
+// If we know the graph is a single weakly connected component, single_component
+// can be set to ture to avoid recomputing components. Notes:
 //  - returned cactus graph needs to be freed by stCactusGraph_destruct
-//  - returns a Cactus graph, and a list of stCactusEdgeEnd* telomeres, in pairs of adjacent items.
-pair<stCactusGraph*, stList*> handle_graph_to_cactus(const PathHandleGraph& graph, const unordered_set<string>& hint_paths,
-                                                     bool single_component = false);
+//  - returns a Cactus graph, and a list of stCactusEdgeEnd* telomeres, in pairs
+//  of adjacent items.
+pair<stCactusGraph *, stList *>
+handle_graph_to_cactus(const PathHandleGraph &graph,
+                       const unordered_set<string> &hint_paths,
+                       bool single_component = false);
 
 // Convert back from Cactus to VG
 // (to, for example, display using vg view)
 // todo: also provide mapping info to get nodes embedded in cactus components
-VG cactus_to_vg(stCactusGraph* cactus_graph);
+VG cactus_to_vg(stCactusGraph *cactus_graph);
 
 // Convert vg into vg formatted cactus representation
 // Input graph must be sorted!
-VG cactusify(const PathHandleGraph& graph);
+VG cactusify(const PathHandleGraph &graph);
 
-}
-
+} // namespace vg
 
 #endif
