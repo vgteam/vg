372513c53218250edb31ef12d420a3c6fca45a0b
faithokamoto <faith.purpleflower@gmail.com>
2026-07-16 12:44:19 -0700

    new test case (that we fail)

--- src/unittest/chain_items.cpp
+++ src/unittest/chain_items.cpp
@@ -144,6 +144,38 @@ TEST_CASE("find_best_chain is willing to leave the main diagonal if the items su
     REQUIRE(result.second == std::vector<size_t>{0, 1, 2, 3});
 }
 
+TEST_CASE("find_best_chains tries all possibilities for X", "[chain_items][find_best_chains]") {
+    // Set up graph fixture
+    HashGraph graph = make_disconnected_graph(5, 10);
+    // 1-3-5 diagonal
+    graph.create_edge(graph.get_handle(1, false), graph.get_handle(3, false));
+    graph.create_edge(graph.get_handle(3, false), graph.get_handle(5, false));
+    // 2-3-4 diagonal
+    graph.create_edge(graph.get_handle(2, false), graph.get_handle(3, false));
+    graph.create_edge(graph.get_handle(3, false), graph.get_handle(4, false));
+    auto h = get_handles(graph);
+    
+    IntegratedSnarlFinder snarl_finder(graph);
+    SnarlDistanceIndex distance_index;
+    fill_in_distance_index(&distance_index, &graph, &snarl_finder);
+
+    // One anchor on each node
+    // read start, graph handle and offset, length, and score
+    auto to_score = make_anchors({{1, h[1], 0, 10, 10},
+                                  {1, h[2], 0, 10, 10},
+                                  {11, h[3], 0, 10, 10},
+                                  {21, h[4], 0, 10, 10},
+                                  {21, h[5], 0, 10, 10}}, graph);
+    
+    // Actually run the chaining and test
+    auto result = algorithms::find_best_chains(to_score, distance_index, graph, algorithms::ChainScoringScheme(), 4);
+    // We should take all possible paths through the X
+    REQUIRE(result.chains.size() == 4);
+    for (const auto& chain : result.chains) {
+        REQUIRE(chain.scored_chain.second.size() == 3);
+    }
+}
+
 }
 
 }
